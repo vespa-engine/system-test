17510c5d8c705ba58ad6fe75c655d01e9705187e
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2019-07-15 15:40:09 +0200

    Enable TLS for fbench in system tests

--- lib/nodetypes/query_loader.rb
+++ lib/nodetypes/query_loader.rb
@@ -23,8 +23,7 @@ module QueryLoader
     fbenchcmd += "-r #{params[:reuse]} " if params[:reuse]
     fbenchcmd += "-o #{params[:output]} " if params[:output]
     fbenchcmd += "-xy " if params[:get_extended]
-    # TODO Re-enable HTTPS once performance issues are resolved
-    # fbenchcmd += "-D " unless params[:disable_tls]
+    fbenchcmd += "-D " unless params[:disable_tls]
     fbenchcmd += "#{qrserver.name} #{qrserver.http_port} "
     qrserver2 = params[:qrserver2]
     fbenchcmd += "#{qrserver2.name} #{qrserver2.http_port}" if params[:multiple_qrs]
