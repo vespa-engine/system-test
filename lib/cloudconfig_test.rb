eddeec146286cb1f105b5cdff36d03c696ef8553
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2019-07-23 17:13:12 +0200

    Rest API is hardcoded to use http as scheme in uris

--- lib/cloudconfig_test.rb
+++ lib/cloudconfig_test.rb
@@ -75,7 +75,7 @@ class CloudConfigTest < TestCase
   def validate_create_session_response(json_response, hostname, tenant, expected_session_id)
     assert_json_contains_field(json_response, "prepared")
     if expected_session_id > 0
-      expected_prepare_url = "#{https_client.scheme}://#{hostname}:#{DEFAULT_SERVER_HTTPPORT}#{create_session_v2_path(tenant)}/#{expected_session_id}/prepared"
+      expected_prepare_url = "http://#{hostname}:#{DEFAULT_SERVER_HTTPPORT}#{create_session_v2_path(tenant)}/#{expected_session_id}/prepared"
       assert_equal(expected_prepare_url, json_response["prepared"])
     end
   end
