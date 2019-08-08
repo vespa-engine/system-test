9823800694984c2189a0ba5891577d1b66a8fcea
Jo Kristian Bergum <bergum@oath.com>
2019-08-08 11:17:07 +0200

    New system test testing xgboost integration and scoring with toy data sets from sklearn

---
+++ tests/search/xgboost/train.py
@@ -0,0 +1,51 @@
+from sklearn import datasets
+import xgboost as xgb
+import json
+import sys
+
+featureMapFile = sys.argv[1]
+modelDir = sys.argv[2]
+feedDir = sys.argv[3]
+
+def makeVespaFeed(dataset, data):
+  datapoints = []
+  for i in range(0,len(data)):
+    x = data[i]
+    doc = {
+        "put": "id:%s:x::%i" % (dataset,i), 
+        "fields": {
+            "id" : i, 
+            "features": x.tolist(),
+            "dataset": dataset
+        }
+    }
+    datapoints.append(doc)
+  return datapoints    
+  
+
+diabetes = datasets.load_diabetes()
+breast_cancer = datasets.load_breast_cancer()
+
+json.dump(makeVespaFeed("diabetes",diabetes.data), open(feedDir + "diabetes-feed.json","w"))
+json.dump(makeVespaFeed("breast_cancer",breast_cancer.data), open(feedDir + "breast_cancer-feed.json","w"))
+
+d = xgb.XGBRegressor(n_estimators=20, objective="reg:squarederror", base_score=0.0)
+d .fit(diabetes.data,diabetes.target)
+d.get_booster().dump_model(modelDir + "regression_diabetes.json", fmap=featureMapFile, dump_format='json')
+
+b = xgb.XGBRegressor(n_estimators=20, objective="reg:logistic", base_score=0.5)
+b.fit(breast_cancer.data,breast_cancer.target)
+b.get_booster().dump_model(modelDir + "regression_breast_cancer.json", fmap=featureMapFile, dump_format='json')
+
+c = xgb.XGBClassifier(n_estimators=20, objective='binary:logistic')
+c.fit(breast_cancer.data,breast_cancer.target) 
+c.get_booster().dump_model(modelDir + "binary_breast_cancer.json", fmap=featureMapFile, dump_format='json')
+
+#predictions
+predictions = {
+    "regression_diabetes" :  d.predict(diabetes.data).tolist(),
+    "regression_breast_cancer" : b.predict(breast_cancer.data).tolist(),
+    "binary_breast_cancer" : c.predict_proba(breast_cancer.data)[:,1].tolist()
+}
+json.dump(predictions,sys.stdout)
+
---
+++ tests/search/xgboost/xgboost.rb
@@ -0,0 +1,70 @@
+# Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+require 'indexed_search_test'
+require 'json'
+
+class XGBoostServing < IndexedSearchTest
+
+  def setup
+    @valgrid = false
+    set_owner("jobergum")
+    set_description("Test XGBoost model representation in Vespa")
+
+    @node = @vespa.nodeproxies.values.first
+    @node.execute("pip3 install xgboost sklearn")
+
+    @node.execute("python3 #{selfdir}/train.py #{selfdir}/feature-map.txt  #{selfdir}/app/models/ #{selfdir}  > #{selfdir}/predictions.json")
+
+    preds = File.read("#{selfdir}/predictions.json")
+    @predictions = JSON.parse(preds) 
+  end
+
+  def compare(vespa_prediction,dataset)
+    xgboost_prediction = @predictions[dataset]
+    print("Comparing for #{dataset}")
+    for i in (0...xgboost_prediction.length) 
+      assert_in_delta(xgboost_prediction[i], vespa_prediction[i],0.0001, message="Vespa prediction for data id #{i} does not match")
+    end
+  end
+
+  def test_xgboost
+    deploy("#{selfdir}/app")
+    start
+
+    #Feed files generated from setup/train.py 
+    feed(:file => "#{selfdir}/diabetes-feed.json")
+    feed(:file => "#{selfdir}/breast_cancer-feed.json")
+    wait_for_hitcount("query=sddocname:x", 569 + 442)
+
+    regression_diabetes = getVespaPrediction("diabetes", "regression-diabetes")
+    compare(regression_diabetes, "regression_diabetes")
+
+    regression_breast_cancer = getVespaPrediction("breast_cancer", "regression-breast_cancer")
+    compare(regression_breast_cancer, "regression_breast_cancer")
+
+    binary_breast_cancer = getVespaPrediction("breast_cancer", "binary-probability-breast_cancer")
+    compare(binary_breast_cancer, "binary_breast_cancer")
+  end
+
+  def getVespaPrediction(dataset,rankProfile)
+    result = search("/search/?query=dataset:#{dataset}&format=json&hits=1000&ranking=#{rankProfile}")
+    tree = JSON.parse(result.xmldata)
+    hits = tree["root"]["children"]
+    predictions = {}
+    hits.each do |hit|
+      score = hit['relevance']
+      id = hit['fields']['id']
+      predictions[id] = score
+    end
+    predictions_array = []
+    predictions.keys().sort().each do |id| 
+      predictions_array.push(predictions[id])
+    end
+    return predictions_array
+  end
+
+  def teardown
+    stop
+  end
+
+end
