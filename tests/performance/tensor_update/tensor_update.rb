921ea1265700bca8a9dc1ea98a06fbc0d98099a8
Harald Musum <musum@verizonmedia.com>
2019-09-09 12:17:19 +0200

    Better performance

--- tests/performance/tensor_update/tensor_update.rb
+++ tests/performance/tensor_update/tensor_update.rb
@@ -62,7 +62,7 @@ class TensorUpdatePerfTest < PerformanceTest
 
   def get_graphs
     [
-      get_feed_throughput_graph("assign", 10, 27500, 32000),
+      get_feed_throughput_graph("assign", 10, 27500, 32500),
       get_feed_throughput_graph("assign", 100, 6400, 7300),
       get_feed_throughput_graph("assign", 1000, 680, 800),
       get_feed_throughput_graph("modify", 10, 28000, 34000),
