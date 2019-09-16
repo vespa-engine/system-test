66dffd3aa52f02925b242d16f59c5bb458e55919
Harald Musum <musum@verizonmedia.com>
2019-09-16 07:21:16 +0000

    Add test of ZooKeeper version used

---
+++ tests/cloudconfig/zookeeper/zookeeper.rb
@@ -0,0 +1,23 @@
+# Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+require 'cloudconfig_test'
+
+class ZooKeeper < CloudConfigTest
+
+  def setup
+    set_description("Tests that ZooKeeper version used is the wanted one")
+    set_owner("musum")
+  end
+
+  def test_zookeeper_version
+    deploy_app(CloudconfigApp.new)
+
+    output = vespa.nodeproxies.values.first.execute("echo srvr|nc localhost 2181|head -n 1")
+    expected_version = "3.4"
+    assert(output.start_with?("Zookeeper version: #{expected_version}"),
+           "Expected ZooKeeper version to be #{expected_version}, another version found in output: #{output}")
+  end
+
+  def teardown
+    stop
+  end
+end
