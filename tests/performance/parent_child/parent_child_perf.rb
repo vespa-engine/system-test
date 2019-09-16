eaaec5257b9d975623d851198173293f6c2e7bd2
Harald Musum <musum@verizonmedia.com>
2019-09-16 08:31:06 +0200

    Better performance

--- tests/performance/parent_child/parent_child_perf.rb
+++ tests/performance/parent_child/parent_child_perf.rb
@@ -142,7 +142,7 @@ class ParentChildPerfTest < PerformanceTest
       get_matching_latency_graphs(FLATTENED, SLOW, CAMPAIGN_AD_RATIO),
       get_matching_latency_graphs(FLATTENED, FAST, CAMPAIGN_AD_RATIO),
       get_matching_latency_graphs(IMPORTED_NESTED, FAST, ADVERTISER_CAMPAIGN_RATIO),
-      get_feeding_graph(FEEDING_PUT, 23000, 27000),
+      get_feeding_graph(FEEDING_PUT, 23000, 27500),
       get_feeding_graph(FEEDING_UPDATE, 26000, 32000),
       get_latency_graph(RANKING, SLOW, 88, 106),
       get_latency_graph(MATCHING, SLOW, 1.8, 2.1),
