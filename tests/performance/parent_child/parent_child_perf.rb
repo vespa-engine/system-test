f603077654fc64c98ac98509879c90ea00859285
Harald Musum <musum@verizonmedia.com>
2019-10-02 08:29:55 +0200

    Allow lower latency

--- tests/performance/parent_child/parent_child_perf.rb
+++ tests/performance/parent_child/parent_child_perf.rb
@@ -146,7 +146,7 @@ class ParentChildPerfTest < PerformanceTest
       get_feeding_graph(FEEDING_UPDATE, 26000, 32000),
       get_latency_graph(RANKING, SLOW, 88, 106),
       get_latency_graph(MATCHING, SLOW, 1.8, 2.1),
-      get_latency_graph(MATCHING, FAST, 1.15, 1.37)
+      get_latency_graph(MATCHING, FAST, 1.13, 1.37)
     ]
   end
 
