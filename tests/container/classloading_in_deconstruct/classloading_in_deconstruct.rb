9c49b9e24d695db232194e692d4c05fe10250f5a
gjoranv <gv@verizonmedia.com>
2019-10-24 15:15:47 +0200

    Add new test with a searcher that loads a new class in deconstruct

---
+++ tests/container/classloading_in_deconstruct/classloading_in_deconstruct.rb
@@ -0,0 +1,49 @@
+# Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+require 'container_test'
+require 'app_generator/container_app'
+
+class ClassloadingInDeconstruct < ContainerTest
+
+  def setup
+    set_owner("gjoranv")
+    set_description("Deploys a component that loads a new class in its deconstruct method, " +
+                        "to verify that the bundle classloader is still functioning.")
+  end
+
+  def test_classloading_in_deconstruct
+    searcher = add_bundle_dir(selfdir, "com.yahoo.vespatest.DeconstructSearcher", :name => 'searcher')
+    compile_bundles(@vespa.nodeproxies.values.first)
+
+    start(create_application('Hello, World!'), :bundles => [searcher])
+    verify_response('Hello, World!')
+
+    # Redeploy with same bundle, but modified configured message
+    deploy(create_application('Hello again!'), :bundles => [searcher])
+    verify_response('Hello again!')
+
+    sleep_period = 70
+    puts "Sleeping #{sleep_period} seconds for old searcher to be deconstructed."
+    sleep(sleep_period)
+  end
+
+  def verify_response(expected)
+    result = @container.search("/search/")
+    assert_match(/#{expected}/, result.xmldata, "Did not get expected response.")
+  end
+
+  def create_application(message)
+    config = ConfigOverride.new(:"com.yahoo.vespatest.response").
+        add("response", message)
+
+    ContainerApp.new(false).
+        container(Container.new.search(Searching.new.
+            chain(Chain.new.add(
+                Searcher.new("com.yahoo.vespatest.DeconstructSearcher").
+                    config(config)))))
+  end
+
+  def teardown
+    stop
+  end
+
+end
