607cae573d934ce468bf01a0844f628cb3eb5cb3
gjoranv <gv@verizonmedia.com>
2019-10-28 21:06:51 +0100

    Use updated bundle instead of updated config.
    - The searcher's deconstruct loads a class from a bundle that
      has been uninstalled.

---
+++ tests/container/classloading_in_deconstruct/exporter/src/main/java/com/yahoo/exporter/Exporter.java
@@ -0,0 +1,19 @@
+// Copyright 2019 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.exporter;
+
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author gjoranv
+ */
+public class Exporter implements Callable<String> {
+    private static final Logger log = Logger.getLogger(Exporter.class.getName());
+
+    @Override
+    public String call() {
+        return "Successfully called!";
+    }
+
+}
