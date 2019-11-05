d873c81e76a85c20963319551e1399ea5f52563b
Harald Musum <musum@verizonmedia.com>
2019-11-05 13:03:16 +0100

    Allow some more slack

--- tests/performance/tensor_eval/tensor_eval_normal.rb
+++ tests/performance/tensor_eval/tensor_eval_normal.rb
@@ -30,7 +30,7 @@ class TensorEvalNormalPerfTest < TensorEvalPerfTest
       get_latency_graphs_for_rank_profile(DENSE_FLOAT_TENSOR_DOT_PRODUCT),
       get_latency_graphs_for_rank_profile(SPARSE_TENSOR_DOT_PRODUCT),
       get_latency_graph_for_rank_profile(FEATURE_DOT_PRODUCT,              250, 40.0, 45.0),
-      get_latency_graph_for_rank_profile(FEATURE_DOT_PRODUCT_ARRAY,        500, 15.5, 16.9),
+      get_latency_graph_for_rank_profile(FEATURE_DOT_PRODUCT_ARRAY,        500, 15.5, 17.2),
       get_latency_graph_for_rank_profile(DENSE_TENSOR_DOT_PRODUCT,         500, 16.9, 18.5),
       get_latency_graph_for_rank_profile(SPARSE_TENSOR_DOT_PRODUCT,        250, 690, 820)
     ]
