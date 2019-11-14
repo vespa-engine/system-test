962d7239953a1579afc3f3a6781c9ac3e36d2def
Harald Musum <musum@verizonmedia.com>
2019-11-14 12:17:05 +0100

    We are now using ZooKeeper 3.5

--- tests/cloudconfig/zookeeper/zookeeper.rb
+++ tests/cloudconfig/zookeeper/zookeeper.rb
@@ -12,7 +12,7 @@ class ZooKeeper < CloudConfigTest
     deploy_app(CloudconfigApp.new)
 
     output = vespa.nodeproxies.values.first.execute("echo srvr|nc localhost 2181|head -n 1")
-    expected_version = "3.4"
+    expected_version = "3.5"
     assert(output.start_with?("Zookeeper version: #{expected_version}"),
            "Expected ZooKeeper version to be #{expected_version}, another version found in output: #{output}")
   end
