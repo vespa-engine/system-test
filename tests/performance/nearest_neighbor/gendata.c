7a3c4419f6ce8a7a4c1d6a2b35654700768d559e
Arne Juul <arnej@verizonmedia.com>
2019-11-28 12:28:17 +0000

    add performance test for NearestNeighbor search operator

---
+++ tests/performance/nearest_neighbor/gendata.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NUMDOCS 123456
+#define NUMDIMS 512
+
+int pct() {
+	return random() % 100;
+}
+
+void header(int num) {
+	printf("{\"id\":\"id:test:foobar::%d\", \"fields\":{", num);
+	printf("\"title\":\"doc %d ", num);
+	if (pct() < 10) printf(" ten");
+	if (pct() < 50) printf(" fifty");
+	if (pct() < 90) printf(" ninety");
+	printf(" here\", ");
+}
+
+void vector() {
+	int i;
+	printf("\"dvector\": { \"values\": [ ");
+	for (i = 0; i < NUMDIMS; ++i) {
+		double dv = (random() % 1000000) * 0.0001;
+		if (i != 0) printf(", ");
+		printf("%.3f", dv);
+	}
+	printf("] }, ");
+}
+
+void footer(int num) {
+	printf("\"order\":%d", num);
+	printf("}},\n");
+}
+
+int main(int argc, char **argv) {
+	int i;
+	srandom(42);
+	printf("[\n");
+	for (i = 0; i < NUMDOCS; i++) {
+		header(i);
+		vector();
+		footer(i);
+	}
+	printf("{\"id\":\"id:test:foobar::0\",\"fields\":{\"title\":\"0\", \"order\":0}}\n");
+	printf("]\n");
+	return 0;
+}
