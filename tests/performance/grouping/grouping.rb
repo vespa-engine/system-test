009c2e7ba4c426979249ec28b79949e91b5f1bfc
Harald Musum <musum@verizonmedia.com>
2019-12-04 10:34:27 +0100

    Allow lower latency

--- tests/performance/grouping/grouping.rb
+++ tests/performance/grouping/grouping.rb
@@ -67,7 +67,7 @@ class GroupingTest < PerformanceTest
     @graphs = [{ :x => 'legend',
                  :y => 'latency',
                  :filter => { 'clients' => [ 1 ], 'legend' => [ 'multilevel' ] },
-                 :y_min => 240,
+                 :y_min => 235,
                  :y_max => 295,
                  :historic => true
                },
