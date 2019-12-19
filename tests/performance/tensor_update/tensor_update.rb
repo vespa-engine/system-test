03f29a143077f527d8ea8b2a418760962289d3a7
Harald Musum <musum@verizonmedia.com>
2019-12-19 08:08:54 +0100

    Allow some more slack

--- tests/performance/tensor_update/tensor_update.rb
+++ tests/performance/tensor_update/tensor_update.rb
@@ -62,9 +62,9 @@ class TensorUpdatePerfTest < PerformanceTest
 
   def get_graphs
     [
-      get_feed_throughput_graph("assign",   10, 31000, 34500),
+      get_feed_throughput_graph("assign",   10, 31000, 35000),
       get_feed_throughput_graph("assign",  100,  9500, 11500),
-      get_feed_throughput_graph("assign", 1000,  1250,  1400),
+      get_feed_throughput_graph("assign", 1000,  1200,  1400),
       get_feed_throughput_graph("modify",   10, 31500, 37000),
       get_feed_throughput_graph("modify",  100, 31500, 37000),
       get_feed_throughput_graph("modify", 1000, 32000, 37000)
