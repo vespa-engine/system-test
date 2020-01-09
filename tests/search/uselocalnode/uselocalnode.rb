492237d5bb71fb1d586cd6f550c36775b094b5f9
Jon Bratseth <bratseth@verizonmedia.com>
2020-01-09 14:20:46 +0100

    No explicit use-local-node

--- tests/search/uselocalnode/uselocalnode.rb
+++ tests/search/uselocalnode/uselocalnode.rb
@@ -11,7 +11,7 @@ class UseLocalNodeTest < SearchTest
   def setup
     set_owner("balder")
     deploy_app(SearchApp.new.cluster(
-        SearchCluster.new.sd(SEARCH_DATA + "music.sd").use_local_node(true).redundancy(2).ready_copies(2).
+        SearchCluster.new.sd(SEARCH_DATA + "music.sd").redundancy(2).ready_copies(2).
                group(NodeGroup.new(0, "topgroup").
                      distribution("1|*").
                      group(NodeGroup.new(0, "group0").node(NodeSpec.new("node1", 0))).
@@ -24,22 +24,16 @@ class UseLocalNodeTest < SearchTest
   end
 
   def test_uselocalnode_javadispatch
-    run_test("&dispatch.internal=true")
-  end
-
-  def run_test(query_append)
     feed_and_wait_for_docs("music", 1, :file => SEARCH_DATA+"music.1.xml")
     vespa.search["search"].searchnode[0].stop
     vespa.search["search"].searchnode[1].stop
     wait_for_hitcount("music", 0, 10)
     vespa.qrserver.each_value do |container|
       host= container.name
-      # Excplicitly go through dispatch so that it generates the log messages we test for
-      r=container.search("/search/?query=sddocname:music&nocache&hits=0&dispatch.direct=false#{query_append}")
+      r=container.search("/search/?query=sddocname:music&nocache&hits=0")
     end
     matches = wait_for_atleast_log_matches(/.*Coverage of group 0 is only 0\/1 [(]requires 1[)]/, 2, 120, {:multinode => true})
     assert(matches <= 6, "The test should see 2 to 6 reports for group 0 coverage, saw #{matches}")
-    assert_log_not_matches(/.*Coverage of group 1 is only 0\/1 [(]requires 1[)]/, {:multinode => true})
   end
 
   def teardown
