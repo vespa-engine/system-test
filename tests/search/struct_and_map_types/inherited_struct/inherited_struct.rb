05329dca3057be99b80ac8709787fcdabc1feed2
Henning Baldersheim <balder@yahoo-inc.com>
2020-01-28 07:22:08 +0000

    Add a test that uses an inherited struct in another nested struct.

---
+++ tests/search/struct_and_map_types/inherited_struct/inherited_struct.rb
@@ -0,0 +1,24 @@
+# Copyright 2020 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+require 'indexed_search_test'
+
+class InheritedStruct < IndexedSearchTest
+
+  def setup
+    set_owner("balder")
+    set_description("Test that structs can be used in inherited types.")
+  end
+
+  # Multiple clusters inherit from same base sd
+  def test_structs_are_inherited
+    add_bundle_dir(File.expand_path(selfdir + "/concretedocs"), "concretedocs")
+    deploy(selfdir + "app", [selfdir + "concretedocs/base.sd", selfdir + "concretedocs/usebase.sd"])
+    start
+    feed(:file => selfdir + "docs.json")
+    assert_hitcount("query=sddocname:usebase", 1)
+  end
+
+  def teardown
+    stop
+  end
+
+end
