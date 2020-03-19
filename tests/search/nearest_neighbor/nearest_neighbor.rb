decba78d7ae92312c65776ee797914ee9f9b648a
Arne Juul <arnej@verizonmedia.com>
2020-03-19 12:07:06 +0000

    refactor to use a hash with query properties instead of lots and lots of parameters

--- tests/search/nearest_neighbor/nearest_neighbor.rb
+++ tests/search/nearest_neighbor/nearest_neighbor.rb
@@ -13,48 +13,52 @@ class NearestNeighborTest < IndexedSearchTest
     start
     feed_docs
 
-    run_brute_force_tests("pos", "qpos_double")
-    run_brute_force_tests("pos", "qpos_float")
-    run_hnsw_tests("pos", "qpos_double")
+    run_brute_force_tests({:query_tensor => "qpos_double"})
+    run_brute_force_tests({:query_tensor => "qpos_float"})
+    run_hnsw_tests({:query_tensor => "qpos_double"})
   end
 
-  def run_common_tests(doc_tensor, query_tensor, approx)
-    assert_nearest_docs(-2, 10, doc_tensor, query_tensor, approx, nil, [[0,2],[1,3],[2,4],[3,5],[4,6],[5,7],[6,8],[7,9],[8,10],[9,11]])
-    assert_nearest_docs(-2, 5,  doc_tensor, query_tensor, approx, nil, [[0,2],[1,3],[2,4],[3,5],[4,6]])
-    assert_nearest_docs(-2, 3,  doc_tensor, query_tensor, approx, nil, [[0,2],[1,3],[2,4]])
-    assert_nearest_docs(-2, 1,  doc_tensor, query_tensor, approx, nil, [[0,2]])
+  def run_common_tests(query_props)
+    query_props[:x_0] = -2
+    assert_nearest_docs(query_props, 10, [[0,2],[1,3],[2,4],[3,5],[4,6],[5,7],[6,8],[7,9],[8,10],[9,11]])
+    assert_nearest_docs(query_props,  5, [[0,2],[1,3],[2,4],[3,5],[4,6]])
+    assert_nearest_docs(query_props,  3, [[0,2],[1,3],[2,4]])
+    assert_nearest_docs(query_props,  1, [[0,2]])
   end
 
-  def run_brute_force_tests(doc_tensor, query_tensor)
-    run_common_tests(doc_tensor, query_tensor, "false")
+  def run_brute_force_tests(query_props)
+    query_props[:approx] = "false"
+    run_common_tests(query_props)
 
     # In this case we always find a nearer document when evaluating the next,
     # so all documents go through the heap used by the backend search iterator.
     # This means all documents are returned in the result as well.
-    assert_nearest_docs(11, 3,  doc_tensor, query_tensor, "false", nil, [[9,2],[8,3],[7,4],[6,5],[5,6],[4,7],[3,8],[2,9],[1,10],[0,11]])
+    assert_nearest_docs(query_props, 3, [[9,2],[8,3],[7,4],[6,5],[5,6],[4,7],[3,8],[2,9],[1,10],[0,11]], {:x_0 => 11})
 
     # With additional query filter
-    assert_nearest_docs(-2, 3, doc_tensor, query_tensor, "false", 0, [[0,2],[2,4],[4,6]])
-    assert_nearest_docs(-2, 3, doc_tensor, query_tensor, "false", 1, [[1,3],[3,5],[5,7]])
+    assert_nearest_docs(query_props, 3, [[0,2],[2,4],[4,6]], {:x_0 => -2, :filter => "0"})
+    assert_nearest_docs(query_props, 3, [[1,3],[3,5],[5,7]], {:x_0 => -2, :filter => "1"})
   end
 
-  def run_hnsw_tests(doc_tensor, query_tensor)
-    run_common_tests(doc_tensor, query_tensor, "true")
-    run_common_tests(doc_tensor, query_tensor, nil)
+  def run_hnsw_tests(query_props)
+    run_common_tests(query_props)
+    query_props[:approx] = "true"
+    run_common_tests(query_props)
 
     # This is different from the brute force test as we search the hnsw index and find the top k hits up front and can return only those.
-    assert_nearest_docs(11, 3,  doc_tensor, query_tensor, nil, nil, [[9,2],[8,3],[7,4]])
-    assert_nearest_docs(7,  1,  doc_tensor, query_tensor, nil, nil, [[7,0]])
+    assert_nearest_docs(query_props, 3, [[9,2],[8,3],[7,4]], {:x_0 => 11})
+    assert_nearest_docs(query_props, 1, [[7,0]], {:x_0 => 7})
 
     # With additional query filter.
     # HNSW will be used when the iterator is strict (k is less than 5
     # since filter produces 5 hits). HNSW produces 4 hits of which 2 are
     # fitered out:
-    assert_nearest_docs(-2, 4, doc_tensor, query_tensor, nil, 0, [[0,2],[2,4]])
-    assert_nearest_docs(-2, 4, doc_tensor, query_tensor, nil, 1, [[1,3],[3,5]])
-    # Bruteforce is used when iterator is NOT strict:
-    assert_nearest_docs(-2, 6, doc_tensor, query_tensor, nil, 0, [[0,2],[2,4],[4,6],[6,8],[8,10]])
-    assert_nearest_docs(-2, 6, doc_tensor, query_tensor, nil, 1, [[1,3],[3,5],[5,7],[7,9],[9,11]])
+    query_props[:x_0] = -2
+    assert_nearest_docs(query_props, 4, [[0,2],[2,4]], {:filter => "0"})
+    assert_nearest_docs(query_props, 4, [[1,3],[3,5]], {:filter => "1"})
+    # Bruteforce is used always when iterator is NOT strict:
+    assert_nearest_docs(query_props, 6, [[0,2],[2,4],[4,6],[6,8],[8,10]], {:filter => "0"})
+    assert_nearest_docs(query_props, 6, [[1,3],[3,5],[5,7],[7,9],[9,11]], {:filter => "1"})
   end
 
   def get_docid(i)
@@ -70,19 +74,31 @@ class NearestNeighborTest < IndexedSearchTest
       doc = Document.new("test", get_docid(i)).
         # TODO: Collapse back to 'pos' when we can choose which algorithm to run in the query.
         add_field("pos", { "values" => [i, X_1_POS] }).
+        add_field("text", "txt #{i} with #{i % 2} and #{i % 3} plus #{i+1000}").
         add_field("filter", "#{i % 2}")
       vespa.document_api_v1.put(doc)
     end
   end
 
-  def assert_nearest_docs(x_0, target_num_hits, doc_tensor, query_tensor, approx, filter, exp_results)
-    query = get_query(x_0, X_1_POS, target_num_hits, doc_tensor, query_tensor, approx, filter)
+  def assert_nearest_docs(setup, target_num_hits, exp_results, overrides = {})
+    query_props = setup.merge(overrides)
+    query_props[:target_num_hits] = target_num_hits
+    query_props[:query_tensor] ||= 'qpos_double'
+    query_props[:doc_tensor] ||= 'pos'
+    query = get_query(query_props)
     puts "assert_nearest_docs(): query='#{query}'"
     result = search(query)
     assert_hitcount(result, exp_results.length)
     for i in 0...exp_results.length do
-      exp_docid = exp_results[i][0]
-      exp_distance = exp_results[i][1]
+      assert_single_doc(exp_results[i], result, i, query_props)
+    end
+  end
+
+  def assert_single_doc(exp_result, result, i, qp)
+      query_tensor = qp[:query_tensor]
+      doc_tensor = qp[:doc_tensor]
+      exp_docid = exp_result[0]
+      exp_distance = exp_result[1]
       exp_score = 15 - exp_distance
       exp_closeness = 1.0 / (1.0 + exp_distance)
       exp_features = { "rankingExpression(euclidean_distance_#{query_tensor})" => exp_distance,
@@ -96,14 +112,23 @@ class NearestNeighborTest < IndexedSearchTest
       assert_field_value(result, "documentid", get_docid(exp_docid), i)
       assert_relevancy(result, exp_score, i)
       assert_features(exp_features, JSON.parse(result.hit[i].field["summaryfeatures"]))
-    end
   end
 
-  def get_query(x_0, x_1, target_num_hits, doc_tensor, query_tensor, approx = nil, filter = nil)
+  def get_query(qprops)
+    x_0 = qprops[:x_0] || 0
+    x_1 = qprops[:x_1] || X_1_POS
+    target_num_hits = qprops[:target_num_hits] || 10
+    query_tensor = qprops[:query_tensor]
+    doc_tensor = qprops[:doc_tensor]
+    approx = qprops[:approx]
+    filter = qprops[:filter]
+    text = qprops[:text]
+
     result = "yql=select * from sources * where [{\"targetNumHits\": #{target_num_hits},"
     result += "\"approximate\": #{approx}," if approx
     result += "\"label\": \"nns\"}] nearestNeighbor(#{doc_tensor},#{query_tensor})"
     result += " and filter contains \"#{filter}\"" if filter
+    result += " or text contains \"#{text}\"" if text
     result += ";&ranking.features.query(#{query_tensor})={{x:0}:#{x_0},{x:1}:#{x_1}}"
     return result
   end
