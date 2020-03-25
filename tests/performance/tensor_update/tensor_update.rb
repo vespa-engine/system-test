ad5deab95ec670aa0a0cbd7ce0908f59ccc24165
Harald Musum <musum@verizonmedia.com>
2020-03-25 06:51:04 +0100

    Recenter limits

--- tests/performance/tensor_update/tensor_update.rb
+++ tests/performance/tensor_update/tensor_update.rb
@@ -65,8 +65,8 @@ class TensorUpdatePerfTest < PerformanceTest
       get_feed_throughput_graph("assign",   10, 32000, 36500),
       get_feed_throughput_graph("assign",  100,  9500, 12000),
       get_feed_throughput_graph("assign", 1000,  1190,  1450),
-      get_feed_throughput_graph("modify",   10, 32500, 38000),
-      get_feed_throughput_graph("modify",  100, 32500, 38500),
+      get_feed_throughput_graph("modify",   10, 32500, 38500),
+      get_feed_throughput_graph("modify",  100, 32500, 39000),
       get_feed_throughput_graph("modify", 1000, 32500, 39000)
     ]
   end
