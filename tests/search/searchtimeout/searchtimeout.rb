4752238f7a457e178e202ca110816b9df2234039
Henning Baldersheim <balder@yahoo-inc.com>
2020-05-18 22:07:02 +0000

    Since initial timeout is 1/2 the given timeout until it has adjusted the timeout factor up from 0.5, we need to give it more time.

--- tests/search/searchtimeout/searchtimeout.rb
+++ tests/search/searchtimeout/searchtimeout.rb
@@ -13,11 +13,11 @@ class SearchTimeoutTest < IndexedSearchTest
 
   def test_timeout_long_firstphase
     feed_and_wait_for_docs("banana", 20, :file => selfdir+"docs.xml")
-    assert_result_withtimeout(20.0, "query=sddocname:banana&hits=1&nocache", selfdir + "result.1.xml")
+    assert_result_withtimeout(40.0, "query=sddocname:banana&hits=1&nocache", selfdir + "result.1.xml")
     # TODO This will start failing once timeout are best effort.
     assert_query_errors_withouttimeout("query=sddocname:banana&hits=1&nocache&timeout=5.0&ranking.softtimeout.enable=false",
                                     ["Timeout while waiting for sc0.num0|Query timed out in sc0.num0"])
-    assert_hitcount_withouttimeout("query=sddocname:banana&hits=1&nocache&timeout=20.0", 20)
+    assert_hitcount_withouttimeout("query=sddocname:banana&hits=1&nocache&timeout=40.0", 20)
     assert_hitcount_withouttimeout("query=sddocname:banana&hits=1&nocache&timeout=2.5&ranking.softtimeout.enable=true", 3)
     assert_hitcount_withouttimeout("query=sddocname:banana&hits=1&nocache&timeout=2.5&ranking.softtimeout.enable=true&ranking.softtimeout.factor=0.70", 4)
     for i in 0..40 do
