7a6f13141a81865a9856996914c908014f102760
Harald Musum <musum@verizonmedia.com>
2020-09-21 09:01:46 +0000

    Use higher timeout

--- tests/search/mappedcharencoding/mappedcharencoding.rb
+++ tests/search/mappedcharencoding/mappedcharencoding.rb
@@ -14,34 +14,37 @@ class MappedCharEncoding < IndexedSearchTest
   def test_mapped_char_encoding
     feed_and_wait_for_docs("music", 10000, :file => SEARCH_DATA+"music.10000.xml")
 
-    field = "surl"
     puts "Query: Searching for 'walkuere'"
-    assert_field("query=walkuere", selfdir+"walkure_u_mapped.result", field, true)
+    assert_surl_field("query=walkuere", selfdir+"walkure_u_mapped.result")
 
     puts "Query: Searching for 'walk?re'"
-    assert_field("query=walk%c3%bcre", selfdir+"walkure_u_mapped.result", field, true)
+    assert_surl_field("query=walk%c3%bcre", selfdir+"walkure_u_mapped.result")
 
     puts "Query: Searching for 'WALK?RE'"
-    assert_field("query=WALK%C3%9CRE", selfdir+"walkure_u_mapped.result", field, true)
+    assert_surl_field("query=WALK%C3%9CRE", selfdir+"walkure_u_mapped.result")
 
     puts "Query: Searching for 'espana'"
-    assert_field("query=espana&hits=15", selfdir+"espana_n_mapped.result", field, true)
+    assert_surl_field("query=espana&hits=15", selfdir+"espana_n_mapped.result")
 
     puts "Query: Searching for 'espa?a'"
-    assert_field("query=espa%c3%b1a&hits=15", selfdir+"espana_n_mapped.result", field, true)
+    assert_surl_field("query=espa%c3%b1a&hits=15", selfdir+"espana_n_mapped.result")
 
     puts "Query: Searching for 'ESPA?A'"
-    assert_field("query=ESPA%C3%91A&hits=15", selfdir+"espana_n_mapped.result", field, true)
+    assert_surl_field("query=ESPA%C3%91A&hits=15", selfdir+"espana_n_mapped.result")
 
     puts "Query: Searching for 'francois'"
-    assert_field("query=francois&hits=15", selfdir+"francois_c_mapped.result", field, true)
+    assert_surl_field("query=francois&hits=15", selfdir+"francois_c_mapped.result")
 
     puts "Query: Searching for 'fran?ois'"
-    assert_field("query=fran%c3%a7ois&hits=15", selfdir+"francois_c_mapped.result", field, true)
+    assert_surl_field("query=fran%c3%a7ois&hits=15", selfdir+"francois_c_mapped.result")
 
     puts "Query: Searching for 'FRAN?OIS'"
-    assert_field("query=FRAN%C3%87OIS&hits=15", selfdir+"francois_c_mapped.result", field, true)
+    assert_surl_field("query=FRAN%C3%87OIS&hits=15", selfdir+"francois_c_mapped.result")
+  end
 
+  def assert_surl_field(query, file)
+    field = "surl"    
+    assert_field(query, file, field, true, 10)
   end
 
   def teardown
