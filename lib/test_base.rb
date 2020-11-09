fe639d9350782333afb47a0bb620217a74d28a2e
Harald Musum <musum@verizonmedia.com>
2020-11-09 08:45:40 +0100

    Print before doing delete

--- lib/test_base.rb
+++ lib/test_base.rb
@@ -1425,13 +1425,13 @@ module TestBase
     status_code = 0
     iteration = 0
     until (status_code == 200 || iteration > 5) do
+      puts "Deleting application, iteration #{iteration}"
       result = delete_tenant_application(tenant, app, hostname)
       status_code = result.code.to_i
       if status_code != 200
         sleep 1
       end
       iteration = iteration + 1
-      puts "Deleting application, iteration #{iteration}"
     end
     if status_code != 200
       raise "Unable to delete application #{app}, got status code #{result.code}"
