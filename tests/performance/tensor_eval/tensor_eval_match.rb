65fa719bf25642e752633b1ca1a640ec2c1c1bf5
Arne Juul <arnej@verizonmedia.com>
2020-11-17 07:05:14 +0000

    split ruby file for expensive tensor evaluations

---
+++ tests/performance/tensor_eval/tensor_eval_match.rb
@@ -0,0 +1,43 @@
+# Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+require 'performance/tensor_eval/tensor_eval'
+
+class TensorEvalExpensivePerfTest < TensorEvalPerfTest
+
+  def setup
+    super
+    set_owner("geirst")
+  end
+
+  def test_tensor_evaluation_match
+    set_description("Test performance of various expensive tensor matching use cases")
+    @graphs = get_graphs_match
+    deploy_and_feed(5000)
+
+    [5,10,25].each do |dim_size|
+      run_fbench_helper(MATCH, TENSOR_MATCH_25X25, dim_size, "queries.tensor.sparse.y.#{dim_size}.txt")
+    end
+
+    [5,10,25,50].each do |dim_size|
+      run_fbench_helper(MATCH, TENSOR_MATCH_50X50, dim_size, "queries.tensor.sparse.y.#{dim_size}.txt")
+    end
+
+    [5,10,25,50,100].each do |dim_size|
+      run_fbench_helper(MATCH, TENSOR_MATCH_100X100, dim_size, "queries.tensor.sparse.y.#{dim_size}.txt")
+    end
+  end
+
+  def get_graphs_match
+    [
+      get_latency_graphs_for_rank_profile(TENSOR_MATCH_25X25),
+      get_latency_graphs_for_rank_profile(TENSOR_MATCH_50X50),
+      get_latency_graphs_for_rank_profile(TENSOR_MATCH_100X100),
+      get_latency_graph_for_rank_profile(TENSOR_MATCH_50X50, 50, 380, 450)
+    ]
+  end
+
+  def teardown
+    super
+  end
+
+end
---
+++ tests/performance/tensor_eval/tensor_eval_matrix.rb
@@ -0,0 +1,35 @@
+# Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+require 'performance/tensor_eval/tensor_eval'
+
+class TensorEvalExpensivePerfTest < TensorEvalPerfTest
+
+  def setup
+    super
+    set_owner("geirst")
+  end
+
+  def test_tensor_evaluation_matrix
+    set_description("Test performance of tensor matrix product")
+    @graphs = get_graphs_matrix
+    deploy_and_feed(5000)
+
+    [10,25,50,100].each do |dim_size|
+      rank_profile = "tensor_matrix_product_#{dim_size}x#{dim_size}"
+      query_file = "queries.tensor.dense.#{dim_size}.txt"
+      run_fbench_helper(MATRIX_PRODUCT, rank_profile, dim_size, query_file)
+    end
+  end
+
+  def get_graphs_matrix
+    [
+      get_latency_graphs_for_eval_type(MATRIX_PRODUCT),
+      get_latency_graph_for_rank_profile("tensor_matrix_product_25x25", 25, 1.70, 2.20)
+    ]
+  end
+
+  def teardown
+    super
+  end
+
+end
