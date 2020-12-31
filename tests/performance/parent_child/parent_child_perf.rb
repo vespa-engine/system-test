b393b37253c5e3ddbece8cb0d0c6df4faf5b92e7
Harald Musum <musum@verizonmedia.com>
2020-12-31 13:35:38 +0100

    Recenter limits

--- tests/performance/parent_child/parent_child_perf.rb
+++ tests/performance/parent_child/parent_child_perf.rb
@@ -147,11 +147,11 @@ class ParentChildPerfTest < PerformanceTest
       get_matching_latency_graphs(FLATTENED, SLOW, CAMPAIGN_AD_RATIO),
       get_matching_latency_graphs(FLATTENED, FAST, CAMPAIGN_AD_RATIO),
       get_matching_latency_graphs(IMPORTED_NESTED, FAST, ADVERTISER_CAMPAIGN_RATIO),
-      get_feeding_graph(FEEDING_PUT, 24000, 36000),
-      get_feeding_graph(FEEDING_UPDATE, 28000, 38500),
+      get_feeding_graph(FEEDING_PUT, 27000, 37000),
+      get_feeding_graph(FEEDING_UPDATE, 29000, 39000),
       get_latency_graph(RANKING, SLOW, 85, 105),
       get_latency_graph(MATCHING, SLOW, 1.68, 2.00),
-      get_latency_graph(MATCHING, FAST, 1.04, 1.30)
+      get_latency_graph(MATCHING, FAST, 1.03, 1.30)
     ]
   end
 
