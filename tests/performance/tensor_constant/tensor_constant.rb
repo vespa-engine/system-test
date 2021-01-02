88d536e8aff94d178c8fb88b25b3dd66b533fa7b
Harald Musum <musum@verizonmedia.com>
2021-01-02 09:16:20 +0100

    Allow some more slack

--- tests/performance/tensor_constant/tensor_constant.rb
+++ tests/performance/tensor_constant/tensor_constant.rb
@@ -70,7 +70,7 @@ class TensorConstantPerfTest < PerformanceTest
   def get_graphs_lz4
     [
       get_deploy_graph(TOTAL_TIME, nil, nil),
-      get_deploy_graph(PREPARE_TIME, 2.2, 3.8),
+      get_deploy_graph(PREPARE_TIME, 2.2, 4.0),
       get_deploy_graph(ACTIVATE_TIME, nil, nil),
       get_deploy_graph(FILE_DISTRIBUTION_TIME, 4, 16)
     ]
