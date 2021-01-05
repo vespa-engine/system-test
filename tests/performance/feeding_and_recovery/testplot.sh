ca1e8aa82d6c50f3cfaafa3e1337e68f6872c6f0
Henning Baldersheim <balder@yahoo-inc.com>
2021-01-05 11:17:08 +0000

    GC unused file.

--- tests/performance/feeding_and_recovery/testplot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-lastdir=$(ls /home/y/logs/systemtests/command-line/ | sort | tail -1)
-echo /home/y/logs/systemtests/command-line/$lastdir/*/*/results/performance
-./myplot.rb /home/y/logs/systemtests/command-line/$lastdir/*/*/results/performance
