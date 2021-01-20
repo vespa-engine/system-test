cf7d576ab9958609f360c1c36204e1ddbf03e392
Harald Musum <musum@verizonmedia.com>
2021-01-20 18:45:42 +0100

    Recenter limits
    Better performance after changes in https://github.com/vespa-engine/vespa/pull/16111

--- tests/performance/mixed_tensors/mixed_tensor_eval.rb
+++ tests/performance/mixed_tensors/mixed_tensor_eval.rb
@@ -48,9 +48,9 @@ class MixedTensorPerfTest < MixedTensorPerfTestBase
 
   def get_graphs
     [
-      get_latency_graph(SINGLE_MODEL, 4.0, 4.5),
-      get_latency_graph(MULTI_MODEL_EARLY_REDUCE, 7.5, 8.5),
-      get_latency_graph(MULTI_MODEL_LATE_REDUCE, 4.9, 5.6)
+      get_latency_graph(SINGLE_MODEL, 3.0, 3.5),
+      get_latency_graph(MULTI_MODEL_EARLY_REDUCE, 6.7, 7.5),
+      get_latency_graph(MULTI_MODEL_LATE_REDUCE, 4.0, 5.0)
     ]
   end
 
