4afb0ff0194140f6917c8773cd384c002d2b04e6
Harald Musum <musum@verizonmedia.com>
2021-02-04 07:20:27 +0100

    Allow some more slack

--- tests/performance/reindexing/reindexing_and_feeding.rb
+++ tests/performance/reindexing/reindexing_and_feeding.rb
@@ -174,18 +174,18 @@ class ReindexingAndFeedingTest < PerformanceTest
 	'reindexing.throughput' =>   { :y_min =>  3500, :y_max =>  5100 },
       },
       { :legend => 'feed' } => {
-	'feeder.throughput'     =>   { :y_min =>  8600, :y_max =>  9400 },	
+	'feeder.throughput'     =>   { :y_min =>  8500, :y_max =>  9400 },	
       },
       { :legend => 'reindex_feed' } => {
-	'reindexing.throughput' =>   { :y_min =>  3000, :y_max =>  3700 },
-	'feeder.throughput'     =>   { :y_min =>  8500, :y_max =>  9500 },
+	'reindexing.throughput' =>   { :y_min =>  2900, :y_max =>  3700 },
+	'feeder.throughput'     =>   { :y_min =>  8400, :y_max =>  9500 },
       },
       { :legend => 'update' } => {
-	'feeder.throughput'     =>   { :y_min => 38000, :y_max => 47000 },
+	'feeder.throughput'     =>   { :y_min => 38000, :y_max => 47500 },
       },
       { :legend => 'reindex_update' } => {
-	'reindexing.throughput' =>   { :y_min =>  3300, :y_max =>  4400 },
-	'feeder.throughput'     =>   { :y_min => 34000, :y_max => 41000 },
+	'reindexing.throughput' =>   { :y_min =>  3200, :y_max =>  4400 },
+	'feeder.throughput'     =>   { :y_min => 34000, :y_max => 41500 },
       }
     }
   end
