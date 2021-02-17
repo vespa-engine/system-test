815b0f9b3eb6e4b7f64efdd1afeaff4e37642e5e
Harald Musum <musum@verizonmedia.com>
2021-02-17 10:36:19 +0100

    Take ownership of test

--- tests/search/stop/stopvespa.rb
+++ tests/search/stop/stopvespa.rb
@@ -1,13 +1,14 @@
-# Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+# Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 require 'indexed_search_test'
 
 class StopVespa < IndexedSearchTest
+
   def can_share_configservers?(method_name=nil)
     false
   end
 
   def setup
-    set_owner("nobody")
+    set_owner("musum")
     set_description("Test that stopping vespa works well")
     deploy_app(SearchApp.new.sd(selfdir+"simple.sd"))
     start
