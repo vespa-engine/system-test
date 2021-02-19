a82937774494a99416d57cdaaf8cdd4f6ba3c6e2
Harald Musum <musum@verizonmedia.com>
2021-02-19 08:00:16 +0100

    Update limits after HW change

--- tests/performance/grouping/grouping.rb
+++ tests/performance/grouping/grouping.rb
@@ -67,7 +67,7 @@ class GroupingTest < PerformanceTest
     @graphs = [{ :x => 'legend',
                  :y => 'latency',
                  :filter => { 'clients' => [ 1 ], 'legend' => [ 'multilevel' ] },
-                 :y_min => 215,
+                 :y_min => 210,
                  :y_max => 250,
                  :historic => true
                },
