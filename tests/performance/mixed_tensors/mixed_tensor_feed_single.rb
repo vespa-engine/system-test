bb59fc2edb2e2b8ded8d182d17a21b5c55c302ec
Harald Musum <musum@verizonmedia.com>
2021-02-19 07:28:31 +0100

    Update limits after HW change

--- tests/performance/mixed_tensors/mixed_tensor_feed_single.rb
+++ tests/performance/mixed_tensors/mixed_tensor_feed_single.rb
@@ -45,10 +45,10 @@ class MixedTensorFeedSinglePerfTest < MixedTensorPerfTestBase
     [
       get_all_feed_throughput_graphs,
       get_feed_throughput_graph(PUTS, NUMBER, 1400, 1700),
-      get_feed_throughput_graph(PUTS, STRING, 1400, 1700),
-      get_feed_throughput_graph(UPDATES_ASSIGN, NUMBER, 1500, 1700),
-      get_feed_throughput_graph(UPDATES_ASSIGN, STRING, 1400, 1700),
-      get_feed_throughput_graph(UPDATES_ADD, NUMBER, 14500, 17000),
+      get_feed_throughput_graph(PUTS, STRING, 1300, 1700),
+      get_feed_throughput_graph(UPDATES_ASSIGN, NUMBER, 1400, 1700),
+      get_feed_throughput_graph(UPDATES_ASSIGN, STRING, 1300, 1700),
+      get_feed_throughput_graph(UPDATES_ADD, NUMBER, 14000, 17000),
       get_feed_throughput_graph(UPDATES_ADD, STRING, 13500, 17000),
       get_feed_throughput_graph(UPDATES_REMOVE, NUMBER, 15000, 18000),
       get_feed_throughput_graph(UPDATES_REMOVE, STRING, 14000, 17000)
