19a671906801a5dfe9e1a230f908219192e4b0e1
Henning Baldersheim <balder@yahoo-inc.com>
2021-03-22 21:06:56 +0000

    Keep timeout handling

--- lib/nodetypes/storage.rb
+++ lib/nodetypes/storage.rb
@@ -151,7 +151,7 @@ class Storage
           #@testcase.output("Checking #{node.type}.#{index}")
           if (index.to_i == node.index)
             max_fetch_time = 0
-            while (Time.now < endtime)
+            while true
               time_started = Time.now
               currVersion = vdsnode.get_cluster_state_version
               max_fetch_time = [Time.now - time_started, max_fetch_time].max
