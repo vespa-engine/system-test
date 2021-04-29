897fae5318d1447c58b3d89d14861e3d4d783375
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2021-04-29 17:23:07 +0200

    Add benchmarking of HTTP/2

--- tests/performance/container/http/container-http.rb
+++ tests/performance/container/http/container-http.rb
@@ -2,6 +2,7 @@ require 'app_generator/container_app'
 require 'http_client'
 require 'performance_test'
 require 'performance/fbench'
+require 'performance/h2load'
 require 'pp'
 
 
@@ -9,9 +10,13 @@ class ContainerHttp < PerformanceTest
 
   KEY_FILE = 'cert.key'
   CERT_FILE = 'cert.crt'
+
   STANDARD = 'standard'
   ASYNC_WRITE = 'asyncwrite'
   NON_PERSISTENT = 'nonpersistent'
+
+  HTTP1 = 'http1'
+  HTTP2 = 'http2'
 
   def initialize(*args)
     super(*args)
@@ -42,14 +47,22 @@ class ContainerHttp < PerformanceTest
     set_description('Test basic HTTP performance of container')
     @graphs = [
         {
-            :title => 'QPS all combined',
+            :title => 'QPS HTTP/1 all combined',
+            :filter => {'protocol' => HTTP1},
             :x => 'legend',
             :y => 'qps',
             :historic => true
         },
         {
-            :title => 'QPS HTTP/1.1',
-            :filter => {'legend' => STANDARD},
+            :title => 'QPS HTTP/2 (128 streams total)',
+            :filter => {'protocol' => HTTP2},
+            :x => 'clients',
+            :y => 'qps',
+            :historic => true
+        },
+        {
+            :title => 'QPS HTTP/1',
+            :filter => {'legend' => STANDARD, 'protocol' => HTTP1},
             :x => 'legend',
             :y => 'qps',
             :y_min => 145000,
@@ -57,8 +70,15 @@ class ContainerHttp < PerformanceTest
             :historic => true
         },
         {
-            :title => 'QPS HTTP/1.1 with async write',
-            :filter => {'legend' => ASYNC_WRITE },
+            :title => 'QPS HTTP/1 vs HTTP/2',
+            :filter => {'legend' => STANDARD, 'clients' => 128},
+            :x => 'protocol',
+            :y => 'qps',
+            :historic => true
+        },
+        {
+            :title => 'QPS HTTP/1 with async write',
+            :filter => {'legend' => ASYNC_WRITE, 'protocol' => HTTP1 },
             :x => 'legend',
             :y => 'qps',
             :y_min => 125000,
@@ -66,8 +86,8 @@ class ContainerHttp < PerformanceTest
             :historic => true
         },
         {
-            :title => 'QPS HTTP/1.1 without keep-alive',
-            :filter => {'legend' => NON_PERSISTENT },
+            :title => 'QPS HTTP/1 without keep-alive',
+            :filter => {'legend' => NON_PERSISTENT, 'protocol' => HTTP1 },
             :x => 'legend',
             :y => 'qps',
             :y_min => 3300,
@@ -75,17 +95,36 @@ class ContainerHttp < PerformanceTest
             :historic => true
         },
         {
+            :title => 'Latency HTTP/1',
+            :filter => {'protocol' => HTTP1},
             :x => 'legend',
             :y => 'latency',
             :historic => true
         },
         {
+            :title => 'Latency HTTP/1',
+            :filter => {'protocol' => HTTP1},
+            :x => 'legend',
+            :y => 'cpuutil',
+            :historic => true
+        },
+        {
+            :title => 'Latency HTTP/2',
+            :filter => {'protocol' => HTTP2},
+            :x => 'legend',
+            :y => 'latency',
+            :historic => true
+        },
+        {
+            :title => 'Latency HTTP/2',
+            :filter => {'protocol' => HTTP2},
             :x => 'legend',
             :y => 'cpuutil',
             :historic => true
         }
     ]
-    run_test
+    run_http1_tests
+    run_http2_tests
   end
 
   def test_container_http_performance_with_logging
@@ -99,7 +138,7 @@ class ContainerHttp < PerformanceTest
             :historic => true
         },
         {
-            :title => 'QPS HTTP/1.1',
+            :title => 'QPS HTTP/1',
             :filter => {'legend' => STANDARD},
             :x => 'legend',
             :y => 'qps',
@@ -108,7 +147,7 @@ class ContainerHttp < PerformanceTest
             :historic => true
         },
         {
-            :title => 'QPS HTTP/1.1 with async write',
+            :title => 'QPS HTTP/1 with async write',
             :filter => {'legend' => ASYNC_WRITE },
             :x => 'legend',
             :y => 'qps',
@@ -117,7 +156,7 @@ class ContainerHttp < PerformanceTest
             :historic => true
         },
         {
-            :title => 'QPS HTTP/1.1 without keep-alive',
+            :title => 'QPS HTTP/1 without keep-alive',
             :filter => {'legend' => NON_PERSISTENT },
             :x => 'legend',
             :y => 'qps',
@@ -136,7 +175,7 @@ class ContainerHttp < PerformanceTest
             :historic => true
         }
     ]
-    run_test
+    run_http1_tests
   end
 
   def deploy_test_app(access_logging:)
@@ -155,8 +194,10 @@ class ContainerHttp < PerformanceTest
             server(
               Server.new('http', @container.http_port)).
             server(
-              Server.new('https', '4443').ssl(
-                Ssl.new(private_key_file = "#{dirs.tmpdir}#{KEY_FILE}", certificate_file = "#{dirs.tmpdir}#{CERT_FILE}", ca_certificates_file=nil, client_authentication='disabled')))))
+              Server.new('https', '4443').
+                config(ConfigOverride.new("jdisc.http.connector").
+                  add("http2Enabled", true)).
+                ssl(Ssl.new(private_key_file = "#{dirs.tmpdir}#{KEY_FILE}", certificate_file = "#{dirs.tmpdir}#{CERT_FILE}", ca_certificates_file=nil, client_authentication='disabled')))))
     deploy_container_app(app)
   end
 
@@ -166,7 +207,7 @@ class ContainerHttp < PerformanceTest
     wait_for_application(@vespa.container.values.first, output)
   end
 
-  def run_test
+  def run_http1_tests
     container = (vespa.qrserver['0'] or vespa.container.values.first)
     container.copy(selfdir + "hello.txt", dirs.tmpdir)
     sync_req_file = dirs.tmpdir + "hello.txt"
@@ -177,18 +218,35 @@ class ContainerHttp < PerformanceTest
     run_fbench(container, 128, 20, []) # warmup
 
     profiler_start
-    run_fbench(container, 128, 120, [parameter_filler('legend', STANDARD)])
+    run_fbench(container, 128, 120, [parameter_filler('legend', STANDARD), parameter_filler('protocol', HTTP1)])
     profiler_report(STANDARD)
 
     @queryfile = async_req_file
     profiler_start
-    run_fbench(container, 128, 120, [parameter_filler('legend', ASYNC_WRITE)])
+    run_fbench(container, 128, 120, [parameter_filler('legend', ASYNC_WRITE), parameter_filler('protocol', HTTP1)])
     profiler_report(ASYNC_WRITE)
 
     @queryfile = sync_req_file
     profiler_start
-    run_fbench(container, 32, 120, [parameter_filler('legend', NON_PERSISTENT)], {:disable_http_keep_alive => true})
+    run_fbench(container, 32, 120, [parameter_filler('legend', NON_PERSISTENT), parameter_filler('protocol', HTTP1)],
+               {:disable_http_keep_alive => true})
     profiler_report(NON_PERSISTENT)
+  end
+
+  def run_http2_tests
+    run_http2_test(128, 1, 30)
+    run_http2_test(32, 4, 10)
+    run_http2_test(8, 16, 10)
+  end
+
+  def run_http2_test(clients, concurrent_streams, warmup)
+    perf = Perf::System.new(@container)
+    perf.start
+    h2load = Perf::H2Load.new(@container)
+    result = h2load.run_benchmark(clients: clients, concurrent_streams: concurrent_streams, warmup: warmup, duration: 120,
+                                  uri_port: 4443, uri_path: '/HelloWorld')
+    perf.end
+    write_report([result.filler, perf.fill, parameter_filler('legend', STANDARD), parameter_filler('protocol', STANDARD)])
   end
 
 end
