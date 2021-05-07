0b48501f80351c7fe6b0370223a5877795d2baf3
Håvard Pettersen <havardpe@oath.com>
2021-05-07 13:27:24 +0000

    added run_until_fail.sh (requested by geirst)

---
+++ bin/run_until_fail.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+cnt=0
+"$@"
+res=$?
+while [ $res == 0 ] ; do
+    cnt=$(($cnt + 1))
+    echo "PASS COUNT: $cnt"
+    "$@"
+    res=$?
+done
+echo "operation failed after $cnt passes"
