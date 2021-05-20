2532f64e159fcf29cf85dcb7951f080a02b52022
Harald Musum <musum@verizonmedia.com>
2021-05-20 06:44:03 +0200

    Allow some more slack

--- tests/performance/mixed_tensors/mixed_tensor_feed_single.rb
+++ tests/performance/mixed_tensors/mixed_tensor_feed_single.rb
@@ -45,13 +45,13 @@ class MixedTensorFeedSinglePerfTest < MixedTensorPerfTestBase
     [
       get_all_feed_throughput_graphs,
       get_feed_throughput_graph(PUTS, NUMBER, 1630, 1800),
-      get_feed_throughput_graph(PUTS, STRING, 1520, 1800),
+      get_feed_throughput_graph(PUTS, STRING, 1500, 1800),
       get_feed_throughput_graph(UPDATES_ASSIGN, NUMBER, 1600, 1800),
       get_feed_throughput_graph(UPDATES_ASSIGN, STRING, 1550, 1800),
       get_feed_throughput_graph(UPDATES_ADD, NUMBER, 17500, 19700),
       get_feed_throughput_graph(UPDATES_ADD, STRING, 16800, 19300),
-      get_feed_throughput_graph(UPDATES_REMOVE, NUMBER, 17500, 20500),
-      get_feed_throughput_graph(UPDATES_REMOVE, STRING, 17500, 20500)
+      get_feed_throughput_graph(UPDATES_REMOVE, NUMBER, 17500, 21000),
+      get_feed_throughput_graph(UPDATES_REMOVE, STRING, 17000, 20500)
     ]
   end
 
