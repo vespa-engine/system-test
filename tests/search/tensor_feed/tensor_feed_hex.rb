4a4f23e907077a2831f5098479ef48248344e7b6
Arne Juul <arnej@verizonmedia.com>
2021-05-21 12:03:13 +0000

    add mixed-tensor format test

--- tests/search/tensor_feed/tensor_feed_hex.rb
+++ tests/search/tensor_feed/tensor_feed_hex.rb
@@ -1,3 +1,4 @@
+# Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 require 'indexed_search_test'
 require 'search/tensor_feed/tensor_feed_base.rb'
@@ -12,7 +13,7 @@ class TensorFeedHexTest < IndexedSearchTest
   end
 
   def test_tensor_hex_feed
-    set_description("Test feeding of tensor field and retrieval via search and visit")
+    set_description("Test hex format feeding of tensor field and retrieval via search and visit")
     deploy_app(SearchApp.new.sd(@base_dir + "test.sd").enable_http_gateway)
     start
     feed_and_wait_for_docs("test", 4, :file => @base_dir + "docs.json")
@@ -42,54 +43,47 @@ class TensorFeedHexTest < IndexedSearchTest
   def assert_tensor_docs(docs)
     expect_8 = [ nil, 
                  [{'address'=>{'x'=>'0'}, 'value'=>0.0},
-                            {'address'=>{'x'=>'1'}, 'value'=>0.0},
-                            {'address'=>{'x'=>'2'}, 'value'=>0.0},
-                            {'address'=>{'x'=>'3'}, 'value'=>0.0},
-                            {'address'=>{'x'=>'4'}, 'value'=>0.0},
-                            {'address'=>{'x'=>'5'}, 'value'=>0.0},
-                            {'address'=>{'x'=>'6'}, 'value'=>0.0},
-                            {'address'=>{'x'=>'7'}, 'value'=>0.0}],
+                  {'address'=>{'x'=>'1'}, 'value'=>0.0},
+                  {'address'=>{'x'=>'2'}, 'value'=>0.0},
+                  {'address'=>{'x'=>'3'}, 'value'=>0.0},
+                  {'address'=>{'x'=>'4'}, 'value'=>0.0},
+                  {'address'=>{'x'=>'5'}, 'value'=>0.0},
+                  {'address'=>{'x'=>'6'}, 'value'=>0.0},
+                  {'address'=>{'x'=>'7'}, 'value'=>0.0}],
                  [{'address'=>{'x'=>'0'}, 'value'=>1.0},
-                            {'address'=>{'x'=>'1'}, 'value'=>2.0},
-                            {'address'=>{'x'=>'2'}, 'value'=>3.0},
-                            {'address'=>{'x'=>'3'}, 'value'=>4.0},
-                            {'address'=>{'x'=>'4'}, 'value'=>5.0},
-                            {'address'=>{'x'=>'5'}, 'value'=>6.0},
-                            {'address'=>{'x'=>'6'}, 'value'=>7.0},
-                            {'address'=>{'x'=>'7'}, 'value'=>8.0}],
+                  {'address'=>{'x'=>'1'}, 'value'=>2.0},
+                  {'address'=>{'x'=>'2'}, 'value'=>3.0},
+                  {'address'=>{'x'=>'3'}, 'value'=>4.0},
+                  {'address'=>{'x'=>'4'}, 'value'=>5.0},
+                  {'address'=>{'x'=>'5'}, 'value'=>6.0},
+                  {'address'=>{'x'=>'6'}, 'value'=>7.0},
+                  {'address'=>{'x'=>'7'}, 'value'=>8.0}],
                  [{'address'=>{'x'=>'0'}, 'value'=>1.0},
-                            {'address'=>{'x'=>'1'}, 'value'=>-128.0},
-                            {'address'=>{'x'=>'2'}, 'value'=>-1.0},
-                            {'address'=>{'x'=>'3'}, 'value'=>0.0},
-                            {'address'=>{'x'=>'4'}, 'value'=>127.0},
-                            {'address'=>{'x'=>'5'}, 'value'=>17.0},
-                            {'address'=>{'x'=>'6'}, 'value'=>42.0},
-                            {'address'=>{'x'=>'7'}, 'value'=>-52.0}] ]
+                  {'address'=>{'x'=>'1'}, 'value'=>-128.0},
+                  {'address'=>{'x'=>'2'}, 'value'=>-1.0},
+                  {'address'=>{'x'=>'3'}, 'value'=>0.0},
+                  {'address'=>{'x'=>'4'}, 'value'=>127.0},
+                  {'address'=>{'x'=>'5'}, 'value'=>17.0},
+                  {'address'=>{'x'=>'6'}, 'value'=>42.0},
+                  {'address'=>{'x'=>'7'}, 'value'=>-52.0}] ]
     expect_16 = [ nil,
-                  [{"address"=>{"x"=>"0"}, "value"=>0.0},
-                             {"address"=>{"x"=>"1"}, "value"=>0.0},
-                             {"address"=>{"x"=>"2"}, "value"=>0.0},
-                             {"address"=>{"x"=>"3"}, "value"=>0.0},
-                             {"address"=>{"x"=>"4"}, "value"=>0.0},
-                             {"address"=>{"x"=>"5"}, "value"=>0.0},
-                             {"address"=>{"x"=>"6"}, "value"=>0.0},
-                             {"address"=>{"x"=>"7"}, "value"=>0.0}],
-                  [{"address"=>{"x"=>"0"}, "value"=>100.0},
-                             {"address"=>{"x"=>"1"}, "value"=>200.0},
-                             {"address"=>{"x"=>"2"}, "value"=>300.0},
-                             {"address"=>{"x"=>"3"}, "value"=>400.0},
-                             {"address"=>{"x"=>"4"}, "value"=>500.0},
-                             {"address"=>{"x"=>"5"}, "value"=>600.0},
-                             {"address"=>{"x"=>"6"}, "value"=>700.0},
-                             {"address"=>{"x"=>"7"}, "value"=>800.0}],
-                  [{"address"=>{"x"=>"0"}, "value"=>0.0},
-                             {"address"=>{"x"=>"1"}, "value"=>0.0},
-                             {"address"=>{"x"=>"2"}, "value"=>0.0},
-                             {"address"=>{"x"=>"3"}, "value"=>0.0},
-                             {"address"=>{"x"=>"4"}, "value"=>0.0},
-                             {"address"=>{"x"=>"5"}, "value"=>0.0},
-                             {"address"=>{"x"=>"6"}, "value"=>0.0},
-                             {"address"=>{"x"=>"7"}, "value"=>0.0}] ]
+                  [],
+                  [{"address"=>{"x"=>"foo","y"=>"0"}, "value"=>100.0},
+                   {"address"=>{"x"=>"foo","y"=>"1"}, "value"=>200.0},
+                   {"address"=>{"x"=>"foo","y"=>"2"}, "value"=>300.0},
+                   {"address"=>{"x"=>"foo","y"=>"3"}, "value"=>400.0},
+                   {"address"=>{"x"=>"bar","y"=>"0"}, "value"=>500.0},
+                   {"address"=>{"x"=>"bar","y"=>"1"}, "value"=>600.0},
+                   {"address"=>{"x"=>"bar","y"=>"2"}, "value"=>700.0},
+                   {"address"=>{"x"=>"bar","y"=>"3"}, "value"=>800.0}],
+                  [{"address"=>{"x"=>"foo","y"=>"0"}, "value"=>0.0},
+                   {"address"=>{"x"=>"foo","y"=>"1"}, "value"=>0.0},
+                   {"address"=>{"x"=>"foo","y"=>"2"}, "value"=>0.0},
+                   {"address"=>{"x"=>"foo","y"=>"3"}, "value"=>0.0},
+                   {"address"=>{"x"=>"bar","y"=>"0"}, "value"=>0.0},
+                   {"address"=>{"x"=>"bar","y"=>"1"}, "value"=>0.0},
+                   {"address"=>{"x"=>"bar","y"=>"2"}, "value"=>0.0},
+                   {"address"=>{"x"=>"bar","y"=>"3"}, "value"=>0.0}] ]
 
     assert_nil(get_tensor_field(docs[0], 'my_8_tensor'))
     assert_nil(get_tensor_field(docs[0], 'my_16_tensor'))
@@ -131,30 +125,30 @@ class TensorFeedHexTest < IndexedSearchTest
        {"address"=>{"x"=>"7"}, "value"=>0.0}]
     ]
     expect_16 = [
-      [{"address"=>{"x"=>"0"}, "value"=>-1.0},
-       {"address"=>{"x"=>"1"}, "value"=>-2.0},
-       {"address"=>{"x"=>"2"}, "value"=>-3.0},
-       {"address"=>{"x"=>"3"}, "value"=>-4.0},
-       {"address"=>{"x"=>"4"}, "value"=>-5.0},
-       {"address"=>{"x"=>"5"}, "value"=>-6.0},
-       {"address"=>{"x"=>"6"}, "value"=>-7.0},
-       {"address"=>{"x"=>"7"}, "value"=>-8.0}],
-       [{"address"=>{"x"=>"0"}, "value"=>42.0},
-       {"address"=>{"x"=>"1"}, "value"=>1048576.0},
-       {"address"=>{"x"=>"2"}, "value"=>9.5367431640625e-07},
-       {"address"=>{"x"=>"3"}, "value"=>-255.0},
-       {"address"=>{"x"=>"4"}, "value"=>0.0},
-       {"address"=>{"x"=>"5"}, "value"=>-0.0},
-       {"address"=>{"x"=>"6"}, "value"=>1.1754943508222875e-38},
-       {"address"=>{"x"=>"7"}, "value"=>3.3895313892515355e+38}],
-       [{"address"=>{"x"=>"0"}, "value"=>100.0},
-       {"address"=>{"x"=>"1"}, "value"=>200.0},
-       {"address"=>{"x"=>"2"}, "value"=>300.0},
-       {"address"=>{"x"=>"3"}, "value"=>400.0},
-       {"address"=>{"x"=>"4"}, "value"=>500.0},
-       {"address"=>{"x"=>"5"}, "value"=>600.0},
-       {"address"=>{"x"=>"6"}, "value"=>700.0},
-       {"address"=>{"x"=>"7"}, "value"=>800.0}]
+      [{"address"=>{"x"=>"foo","y"=>"0"}, "value"=>-1.0},
+       {"address"=>{"x"=>"foo","y"=>"1"}, "value"=>-2.0},
+       {"address"=>{"x"=>"foo","y"=>"2"}, "value"=>-3.0},
+       {"address"=>{"x"=>"foo","y"=>"3"}, "value"=>-4.0},
+       {"address"=>{"x"=>"bar","y"=>"0"}, "value"=>-5.0},
+       {"address"=>{"x"=>"bar","y"=>"1"}, "value"=>-6.0},
+       {"address"=>{"x"=>"bar","y"=>"2"}, "value"=>-7.0},
+       {"address"=>{"x"=>"bar","y"=>"3"}, "value"=>-8.0}],
+      [{"address"=>{"x"=>"foo","y"=>"0"}, "value"=>42.0},
+       {"address"=>{"x"=>"foo","y"=>"1"}, "value"=>1048576.0},
+       {"address"=>{"x"=>"foo","y"=>"2"}, "value"=>9.5367431640625e-07},
+       {"address"=>{"x"=>"foo","y"=>"3"}, "value"=>-255.0},
+       {"address"=>{"x"=>"bar","y"=>"0"}, "value"=>0.0},
+       {"address"=>{"x"=>"bar","y"=>"1"}, "value"=>-0.0},
+       {"address"=>{"x"=>"bar","y"=>"2"}, "value"=>1.1754943508222875e-38},
+       {"address"=>{"x"=>"bar","y"=>"3"}, "value"=>3.3895313892515355e+38}],
+      [{"address"=>{"x"=>"foo","y"=>"0"}, "value"=>100.0},
+       {"address"=>{"x"=>"foo","y"=>"1"}, "value"=>200.0},
+       {"address"=>{"x"=>"foo","y"=>"2"}, "value"=>300.0},
+       {"address"=>{"x"=>"foo","y"=>"3"}, "value"=>400.0},
+       {"address"=>{"x"=>"bar","y"=>"0"}, "value"=>500.0},
+       {"address"=>{"x"=>"bar","y"=>"1"}, "value"=>600.0},
+       {"address"=>{"x"=>"bar","y"=>"2"}, "value"=>700.0},
+       {"address"=>{"x"=>"bar","y"=>"3"}, "value"=>800.0}]
     ]
     assert_tensor_field(expect_8[0], docs[0], 'my_8_tensor')
     assert_tensor_field(expect_8[1], docs[1], 'my_8_tensor')
