4ced4ee7aa2991384ca985fd7ca777aba00b94f5
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2021-05-28 16:49:05 +0200

    Revert "Merge pull request #1620 from vespa-engine/revert-1619-bjorncs/vespa-feed-client"
    This reverts commit 245864372de96eebf11f82914fb455666e753615, reversing
    changes made to c51ba09d3b6a7d04fd68430dec375e5667b2a6b0.

--- tests/search/httpclient_docproc/massive_http_client_feeding.rb
+++ tests/search/httpclient_docproc/massive_http_client_feeding.rb
@@ -15,32 +15,49 @@ class MassiveHttpClientFeedingTest < SearchTest
     end
     return ds
   end
+
   def setup
     set_owner("valerijf")
     set_description("Verifies that a container can handle more feed threads than it has thread.")
     @valgrind = false
-  end
 
-  def test_massive_feeding
-    container_cluster = Container.new("dpcluster1").
-                            jvmargs("-Xms4096m -Xmx4096m").
-                            search(Searching.new).
-                            gateway(ContainerDocumentApi.new).
-                            config(ConfigOverride.new("container.handler.threadpool").add("maxthreads", 4))
-    output = deploy_app(SearchApp.new.
-                cluster(SearchCluster.new.sd(SEARCH_DATA+"music.sd")).
-                container(container_cluster))
-    start
-    
-    admin_node = vespa.adminserver
     @feed_file = dirs.tmpdir + "temp.feed.xml"
     generate_documents(0, DOCUMENTS).write_vespafeed_xml(@feed_file)
-    gw = @vespa.container.values.first
-    wait_for_application(gw, output)
+  end
+
+
+
+  def test_vespa_http_client
+    gw = deploy_test_app
     feedfile(@feed_file, {:client => :vespa_http_client, :host => gw.name, :port => gw.http_port, :num_persistent_connections_per_endpoint => 16})
 
     # Don't care if we do not hit the spawned documents, we only care about feeding not getting stuck in this test.
     wait_for_hitcount("ronny", DOCUMENTS)
+  end
+
+  def test_vespa_feed_client
+    gw = deploy_test_app
+    feedfile(@feed_file, {:client => :vespa_feed_client, :host => gw.name, :port => gw.http_port})
+
+    # Don't care if we do not hit the spawned documents, we only care about feeding not getting stuck in this test.
+    wait_for_hitcount("ronny", DOCUMENTS)
+  end
+
+  private
+  def deploy_test_app
+    container_cluster = Container.new("dpcluster1").
+      jvmargs("-Xms4096m -Xmx4096m").
+      search(Searching.new).
+      gateway(ContainerDocumentApi.new).
+      config(ConfigOverride.new("container.handler.threadpool").add("maxthreads", 4))
+    output = deploy_app(SearchApp.new.
+      cluster(SearchCluster.new.sd(SEARCH_DATA+"music.sd")).
+      container(container_cluster))
+    start
+
+    gw = @vespa.container.values.first
+    wait_for_application(gw, output)
+    gw
   end
 
   def teardown
