361dc8898317ce83cf2b87a1c58fadd7348d8ae6
Harald Musum <musum@verizonmedia.com>
2021-06-03 07:25:21 +0200

    Allow lower file distribution time

--- tests/performance/tensor_constant/tensor_constant.rb
+++ tests/performance/tensor_constant/tensor_constant.rb
@@ -72,7 +72,7 @@ class TensorConstantPerfTest < PerformanceTest
       get_deploy_graph(TOTAL_TIME, nil, nil),
       get_deploy_graph(PREPARE_TIME, 2.0, 5.5),
       get_deploy_graph(ACTIVATE_TIME, nil, nil),
-      get_deploy_graph(FILE_DISTRIBUTION_TIME, 11.0, 15.5)
+      get_deploy_graph(FILE_DISTRIBUTION_TIME, 10.5, 15.5)
     ]
   end
 
