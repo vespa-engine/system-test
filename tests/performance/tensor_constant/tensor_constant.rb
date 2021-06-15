84f4061c594f87c5dc5d352b2e51c15873a153a1
Harald Musum <musum@verizonmedia.com>
2021-06-15 23:15:59 +0200

    Fix time calculation

--- tests/performance/tensor_constant/tensor_constant.rb
+++ tests/performance/tensor_constant/tensor_constant.rb
@@ -123,9 +123,8 @@ class TensorConstantPerfTest < PerformanceTest
       puts "Got config generation #{next_generation}"
     }
 
-    start = Time.now
     out, upload_time, prepare_time, activate_time = deploy_app(app, {:collect_timing => true, :separate_upload_and_prepare => true})
-    prepare_finished = Time.now.to_f - start.to_f - activate_time
+    prepare_finished = Time.now.to_f - activate_time
     total_time = (prepare_time + activate_time).to_f
 
     wait_for_config_thread.join
