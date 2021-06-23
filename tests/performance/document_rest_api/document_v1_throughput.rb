4f40a07e7697550c3c579707aa1f9ee7037ffd30
Harald Musum <musum@verizonmedia.com>
2021-06-23 06:39:45 +0200

    Allow some more slack

--- tests/performance/document_rest_api/document_v1_throughput.rb
+++ tests/performance/document_rest_api/document_v1_throughput.rb
@@ -82,7 +82,7 @@ class DocumentV1Throughput < PerformanceTest
           :clients => 8,
           :streams => 8,
           :threads => 8,
-          :metrics => {'qps' => {'get' => {:y_min => 42000, :y_max => 48000}, 'post' => {:y_min => 24500, :y_max => 29500}}}
+          :metrics => {'qps' => {'get' => {:y_min => 42000, :y_max => 48000}, 'post' => {:y_min => 23500, :y_max => 29500}}}
         }
       },
       {
