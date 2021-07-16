49ef804b9298d4ade09622a3822a55375f268b2a
Mathias Chunnoo <mchunnoo@verizonmedia.com>
2021-07-16 09:46:28 +0200

    indent cpp files with 4 space indentation

--- tests/performance/document_store/feed_generator.cpp
+++ tests/performance/document_store/feed_generator.cpp
@@ -1,41 +1,43 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 
 void randomString(unsigned int stringSize) {
-  const char alnum[] =
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const char alnum[] =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-  printf("\"");
-  for (unsigned int i = 0; i < stringSize; i++) {
-    printf("%c", alnum[rand() % 62]);
-  }
-  printf("\"");
+    printf("\"");
+    for (unsigned int i = 0; i < stringSize; i++) {
+        printf("%c", alnum[rand() % 62]);
+    }
+    printf("\"");
 }
 
 void content(unsigned int contentLength) {
-  printf("\"content\": ");
-  randomString(contentLength);
+    printf("\"content\": ");
+    randomString(contentLength);
 }
 
 void doc(unsigned int id, unsigned int contentLength) {
-  printf("{\"put\": \"id:doc:doc::%u\", \"fields\": { ", id);
-  printf("\"doc_id\": %u, ", id);
-  content(contentLength);
-  printf(" } }");
+    printf("{\"put\": \"id:doc:doc::%u\", \"fields\": { ", id);
+    printf("\"doc_id\": %u, ", id);
+    content(contentLength);
+    printf(" } }");
 }
 
 int main(int, char **argv) {
-  const unsigned int numDocs{static_cast<unsigned int>(atoi(argv[1]))};
-  const unsigned int contentLengths{static_cast<unsigned int>(atoi(argv[2]))};
+    const unsigned int numDocs{static_cast<unsigned int>(atoi(argv[1]))};
+    const unsigned int contentLengths{static_cast<unsigned int>(atoi(argv[2]))};
 
-  srand(7);
-  printf("[\n");
-  for (unsigned int i = 1; i < numDocs; i++) {
-    doc(i, contentLengths);
-    printf(",\n");
-  }
-  doc(numDocs, contentLengths);
-  printf("\n]\n");
-  return 0;
+    srand(7);
+    printf("[\n");
+    for (unsigned int i = 1; i < numDocs; i++) {
+        doc(i, contentLengths);
+        printf(",\n");
+    }
+    doc(numDocs, contentLengths);
+    printf("\n]\n");
+    return 0;
 }
--- tests/performance/document_store/query_generator.cpp
+++ tests/performance/document_store/query_generator.cpp
@@ -1,12 +1,14 @@
+// Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
 #include <cstdio>
 #include <cstdlib>
 
 int main(int, char **argv) {
-  const unsigned int numDocs{static_cast<unsigned int>(atoi(argv[1]))};
+    const unsigned int numDocs{static_cast<unsigned int>(atoi(argv[1]))};
 
-  for (unsigned int i = 1; i <= numDocs; i++) {
-    printf("/document/v1/doc/doc/docid/%u\n", i);
-  }
+    for (unsigned int i = 1; i <= numDocs; i++) {
+        printf("/document/v1/doc/doc/docid/%u\n", i);
+    }
 
-  return 0;
+    return 0;
 }
