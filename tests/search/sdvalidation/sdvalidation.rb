93c182d7210b2f93d6d630f488ee8a190f362fb4
Harald Musum <musum@yahooinc.com>
2021-09-28 08:39:30 +0200

    Remove tests covered by unit tests

--- tests/search/sdvalidation/sdvalidation.rb
+++ tests/search/sdvalidation/sdvalidation.rb
@@ -1,4 +1,4 @@
-# Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+# Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 require 'search_test'
 
 class SDValidation < SearchTest
@@ -8,8 +8,9 @@ class SDValidation < SearchTest
   end
 
   def test_sdvalidation    
-    # first one tests that file name in message is relative to app package path
+    # Tests that file name in error message is relative to app package path
     deploy_invalid_sd(selfdir+"invalid_il_expression_name.sd", "Could not parse schema file 'invalid_il_expression_name.sd'")
+
     deploy_invalid_sd(selfdir+"invalid_fieldtype.sd")
     deploy_invalid_sd(selfdir+"invalid_fieldbody.sd")
     deploy_invalid_sd(selfdir+"reserved_fieldname.sd")
@@ -19,41 +20,18 @@ class SDValidation < SearchTest
     deploy_invalid_sd(selfdir+"attribute_properties.sd")
   end
 
-  # Tests that we get a proper error message from il parser with line number when il script is invalid
-  def test_error_message_invalid_il
-    warning_regexp = Regexp.new("Could not parse indexing statement: Encountered \" <IDENTIFIER> \"" +
-                                "compact_string_to_single_term\"\" at line 5, column 57.")
-    begin
-      deploy_app(SearchApp.new.sd(selfdir+"invalid_il_expression_name.sd"))
-      assert(false)
-    rescue ExecuteError => ee
-      assert_match(warning_regexp, ee.output)
-    end
-  end
-
-  # Tests that we get a proper error message from sd parser with line number when sd is invalid
-  def test_error_message_invalid_sd
-    warning_regexp = Regexp.new("Could not parse schema file 'invalid_sd_construct.sd': Encountered \" \"\\(\" \"\\(\"\" at line 5, column 36")
-    begin
-      deploy_app(SearchApp.new.sd(selfdir+"invalid_sd_construct.sd"))
-      assert(false)
-    rescue ExecuteError => ee
-      assert_match(warning_regexp, ee.output)
-    end
-  end
-
   def test_position_expression
     deploy_app(SearchApp.new.sd(selfdir+"position_valid.sd"))
   end
 
   def test_attribute_properties
     # Test that specifying an attribute property for an attribute that is created later
-    # in another firld works.
-    output = deploy_app(SearchApp.new.sd(selfdir+"attribute_properties_ok.sd"))
+    # in another field works.
+    deploy_app(SearchApp.new.sd(selfdir+"attribute_properties_ok.sd"))
   end
 
   def test_rank_validation
-    set_description("Test that rank validation fails deploy application")
+    set_description("Tests that deploying fails when rank validation fails")
     begin
       deploy_app(SearchApp.new.sd(selfdir+"invalid_rank.sd"))
       assert(false)
