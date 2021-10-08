214cbc07feae1f27125353caa474bdfec71ce249
Jon Marius Venstad <venstad@gmail.com>
2021-10-08 16:40:38 +0200

    Add missing )

--- lib/nodetypes/feeder.rb
+++ lib/nodetypes/feeder.rb
@@ -140,7 +140,7 @@ module Feeder
     randomstring = "%04d" % (rand*10000).to_i
     tmpfeed = "#{Environment.instance.vespa_home}/tmp/tmpfeed#{timestamp}-#{randomstring}"
     execute("#{DataGenerator.new.feed_command(template: params[:template], count: params[:count])} > #{tmpfeed}")
-    feedfile(tmpfeed, params.merge({ :localfile => true, :deletefeed => true })
+    feedfile(tmpfeed, params.merge({ :localfile => true, :deletefeed => true }))
   end
 
   # Pipe the output of _command_ into the feeder binary instead of using an
