64fcdbe313228ddc92fc018deca168e05baab354
Jon Bratseth <bratseth@gmail.com>
2021-10-12 10:07:40 +0000

    Deploys vespa-cloud/album-recommendation on a self-hosted instance

---
+++ tests/cloudconfig/cloud_app_on_selfhosted/cloud_app_on_selfhosted.rb
@@ -0,0 +1,22 @@
+# Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+require 'indexed_search_test'
+
+class CloudAppOnSelfhosted < IndexedSearchTest
+
+  def setup
+    set_owner("bratseth")
+    set_description("Tests that this cloud sample app can be deployed on a self-hoted instance")
+  end
+
+  def test_cloud_app_on_selfhosted
+    deploy(selfdir + "album-recommendation/")
+    start
+    feed_and_wait_for_docs("music", 1, :file => selfdir + "A-Head-Full-of-Dreams.json")
+    assert_hitcount("?yql=select%20*%20from%20sources%20*%20where%20album%20contains%20%22head%22%3B", 1)
+  end
+
+  def teardown
+    stop
+  end
+
+end
