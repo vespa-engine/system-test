669304530d2940efcee590bc6830f92f9679bf66
Jon Bratseth <bratseth@gmail.com>
2021-10-20 23:34:47 +0200

    Update copyright headers

--- tests/container/servlet/servlet_reconfig/servlet_reconfig.rb
+++ tests/container/servlet/servlet_reconfig/servlet_reconfig.rb
@@ -1,4 +1,4 @@
-# Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+# Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 require 'app_generator/container_app'
 require 'app_generator/servlet'
 require 'container_test'
