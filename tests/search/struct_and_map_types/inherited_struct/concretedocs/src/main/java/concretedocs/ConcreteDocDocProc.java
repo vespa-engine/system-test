42c25bfda55ebbc5dd828745916d0b47cf591e66
Arne H Juul <arnej@yahooinc.com>
2021-12-14 11:06:30 +0000

    test using inherited struct in various ways

--- tests/search/struct_and_map_types/inherited_struct/concretedocs/src/main/java/concretedocs/ConcreteDocDocProc.java
+++ tests/search/struct_and_map_types/inherited_struct/concretedocs/src/main/java/concretedocs/ConcreteDocDocProc.java
@@ -28,7 +28,39 @@ public class ConcreteDocDocProc extends DocumentProcessor {
     }
 
     private Progress processUsebase(Usebase v) {
-        logger.info("Concrete processing Usebase :" + v.toJson());
+        logger.info("CDDP Concrete processing Usebase :" + v.toJson());
+
+        var f1 = v.getF1();
+        logger.info("CDDP f1 is: " + f1.getClass());
+        logger.info("CDDP f1.name = " + f1.getName());
+        logger.info("CDDP f1.age = " + f1.getAge());
+
+        var f2 = v.getF2();
+        logger.info("CDDP f2 is: " + f2.getClass());
+        logger.info("CDDP f2.name = " + f2.getName());
+        logger.info("CDDP f2.age = " + f2.getAge());
+
+        var f3 = v.getF3();
+        logger.info("CDDP f3 is: " + f3.getClass());
+        logger.info("CDDP f3.name = " + f3.getName());
+        logger.info("CDDP f3.age = " + f3.getAge());
+        logger.info("CDDP f3.desc = " + f3.getDesc());
+
+        var f4 = v.getF4();
+        logger.info("CDDP f4 is: " + f4.getClass());
+        logger.info("CDDP f4.basicinfo is: " + f4.getBasicinfo().getClass());
+        logger.info("CDDP f4 basicinfo.name = " + f4.getBasicinfo().getName());
+        logger.info("CDDP f4 basicinfo.age = " + f4.getBasicinfo().getAge());
+        logger.info("CDDP f4 occupation = " + f4.getOccupation());
+
+        var f5 = v.getF5();
+        logger.info("CDDP f5 is:" + f5 + " -> " + f5.getClass());
+
+        for (var elem : f5) {
+            logger.info("CDDP f5 element is:" + elem.getClass());
+            logger.info("CDDP f5 element.name = " + elem.getName());
+            logger.info("CDDP f5 element.age = " + elem.getAge());
+        }
         return Progress.DONE;
     }
 
