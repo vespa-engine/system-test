cdbf67775308aab2f7a0f15e98236aa5de0b31d0
Arne H Juul <arnej@yahooinc.com>
2022-02-14 07:39:21 +0000

    add missing lines

--- tests/search/bugs/5707026/bug5707026.rb
+++ tests/search/bugs/5707026/bug5707026.rb
@@ -30,6 +30,7 @@ class Bug5707026 < IndexedSearchTest
     query = "((yahoo OR ((y))) ) OR ((qterms:yahoo @@@) OR (qterms:y @@@)) OR ((weightedtags:yahoo @@@) OR (weightedtags:y @@@)) OR ((anchor:yahoo OR ((anchor:y))) ) OR ((keywords:yahoo OR ((keywords:y))) ) OR ((breadcrumb:yahoo OR ((breadcrumb:y))) ) OR ((title2:yahoo OR ((title2:y))) ) OR ((title3:yahoo OR ((title3:y))) ) OR ((catarr1:yahoo OR ((catarr1:y))) ) OR ((catarr4:yahoo OR ((catarr4:y))) ) OR ((uri:yahoo OR ((uri:y))) ) OR ((person:yahoo OR ((person:y))) )&type=adv"
     wait_for_hitcount(query, 1)
     exp_body = "<sep />! News RSS <hi>Y</hi>! News Alert All <hi>Yahoo</hi>! Trending Now<sep />"
+    result = search(query)
     assert_equal(1, result.hitcount)
     assert_equal(exp_body, result.hit[0].field['body'])
   end
@@ -43,6 +44,7 @@ class Bug5707026 < IndexedSearchTest
     wait_for_hitcount(query, 2)
     exp_body_0 = "<sep /><hi>Café</hi> Surf's <hi>Café</hi> Print <hi>Menu</hi> Email a Friend<sep /> Print <hi>Menu</hi> Print Details<sep />"
     exp_body_1 = "<sep />week s Tuesday Dinner <hi>Menu</hi>! This pilot program is currently scheduled<sep />Questions or comments? Reach us at <hi>cafe</hi>@yahoo-inc.com ."
+    result = search(query)
     assert_equal(2, result.hitcount)
     assert_equal(exp_body_0, result.hit[0].field['body'])
     assert_equal(exp_body_1, result.hit[1].field['body'])
