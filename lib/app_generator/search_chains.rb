b4aec3b27d5732bc7762daa0c69b494032dddbe6
Harald Musum <musum@yahooinc.com>
2022-02-24 08:22:16 +0100

    GC dead code

--- lib/app_generator/search_chains.rb
+++ lib/app_generator/search_chains.rb
@@ -1,4 +1,4 @@
-# Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+# Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 class Renderer
   include ChainedSetter
 
@@ -55,13 +55,6 @@ class SearchChain
     @inherits = inherits
     @searchers = []
     @excludes = excludes
-  end
-
-  def to_xml(indent="")
-    XmlHelper.new(indent).
-      tag("searchchain", :id => @id, :inherits => @inherits, :excludes => @excludes).
-        to_xml(@config).
-        to_xml(@searchers).to_s
   end
 
   def to_container_xml(indent)
@@ -128,13 +121,6 @@ class SearchChains
   def initialize()
     @search_chains = []
     @config = ConfigOverrides.new
-  end
-
-  def to_xml(indent="")
-    XmlHelper.new(indent).
-      tag("searchchains").
-        to_xml(@config).
-        to_xml(@search_chains).to_s
   end
 
   def to_container_xml(indent)
