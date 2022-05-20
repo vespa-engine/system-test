23369e82e623fa007bf392890bce7097929d14a2
Arne H Juul <arnej@yahooinc.com>
2022-05-20 11:37:44 +0000

    update expected json structure

--- tests/search/matched_elements_only/matched_elements_only.rb
+++ tests/search/matched_elements_only/matched_elements_only.rb
@@ -35,38 +35,34 @@ class MatchedElementsOnlyTest < IndexedStreamingSearchTest
     # Test fields with explicit 'matched-elements-only'.
     assert_summary_field("str_array contains 'bar'", "str_array", ["bar"])
     assert_summary_field("int_array contains '20'", "int_array", [20])
-    assert_summary_field("str_wset contains 'bar'", "str_wset", [elem("bar", 7)])
-    assert_summary_field("int_wset contains '20'", "int_wset", [elem(20, 7)])
+    assert_summary_field("str_wset contains 'bar'", "str_wset", { "bar" => 7 })
+    assert_summary_field("int_wset contains '20'", "int_wset", { "20" => 7 })
     assert_summary_field("str_array contains 'foo' or str_array contains 'bar'", "str_array", ["bar", "foo"])
-    assert_summary_field("str_wset contains 'foo' or str_wset contains 'bar'", "str_wset", [elem("bar", 7), elem("foo", 5)])
+    assert_summary_field("str_wset contains 'foo' or str_wset contains 'bar'", "str_wset", { "bar" => 7, "foo" => 5 })
     assert_summary_field("weightedSet(str_array, {\"foo\":1, \"baz\":2})", "str_array", ["foo"])
     assert_summary_field("weightedSet(str_array, {\"baz\":1, \"bar\":2})", "str_array", ["bar"])
     assert_summary_field("weightedSet(str_array, {\"foo\":1, \"bar\":2})", "str_array", ["bar", "foo"])
     assert_summary_field("weightedSet(int_array, {\"10\":1, \"11\":2})", "int_array", [10])
     assert_summary_field("weightedSet(int_array, {\"11\":1, \"20\":2})", "int_array", [20])
     assert_summary_field("weightedSet(int_array, {\"10\":1, \"20\":2})", "int_array", [10, 20])
-    assert_summary_field("weightedSet(str_wset, {\"foo\":1, \"baz\":2})", "str_wset", [elem("foo", 5)])
-    assert_summary_field("weightedSet(str_wset, {\"baz\":1, \"bar\":2})", "str_wset", [elem("bar", 7)])
-    assert_summary_field("weightedSet(str_wset, {\"foo\":1, \"bar\":2})", "str_wset", [elem("bar", 7), elem("foo", 5)])
-    assert_summary_field("weightedSet(int_wset, {\"10\":1, \"11\":2})", "int_wset", [elem(10, 5)])
-    assert_summary_field("weightedSet(int_wset, {\"11\":1, \"20\":2})", "int_wset", [elem(20, 7)])
-    assert_summary_field("weightedSet(int_wset, {\"10\":1, \"20\":2})", "int_wset", [elem(10, 5), elem(20, 7)])
+    assert_summary_field("weightedSet(str_wset, {\"foo\":1, \"baz\":2})", "str_wset", { "foo" => 5 })
+    assert_summary_field("weightedSet(str_wset, {\"baz\":1, \"bar\":2})", "str_wset", { "bar" => 7 })
+    assert_summary_field("weightedSet(str_wset, {\"foo\":1, \"bar\":2})", "str_wset", { "bar" => 7, "foo" => 5 })
+    assert_summary_field("weightedSet(int_wset, {\"10\":1, \"11\":2})", "int_wset", { "10" => 5 })
+    assert_summary_field("weightedSet(int_wset, {\"11\":1, \"20\":2})", "int_wset", { "20" => 7 })
+    assert_summary_field("weightedSet(int_wset, {\"10\":1, \"20\":2})", "int_wset", { "10" => 5, "20" => 7 })
 
     # Test summary fields with 'matched-elements-only' (in explicit summary class) that reference source fields.
     assert_summary_field("str_array_src contains 'bar'", "str_array_filtered", ["bar"], "filtered")
     assert_summary_field("int_array_src contains '20'", "int_array_filtered", [20], "filtered")
-    assert_summary_field("str_wset_src contains 'bar'", "str_wset_filtered", [elem("bar", 7)], "filtered")
-    assert_summary_field("int_wset_src contains '20'", "int_wset_filtered", [elem(20, 7)], "filtered")
+    assert_summary_field("str_wset_src contains 'bar'", "str_wset_filtered", { "bar" => 7 }, "filtered")
+    assert_summary_field("int_wset_src contains '20'", "int_wset_filtered", { "20" => 7 }, "filtered")
     assert_summary_field("str_array_src contains 'foo' or str_array_src contains 'bar'", "str_array_filtered", ["bar", "foo"], "filtered")
-    assert_summary_field("str_wset_src contains 'foo' or str_wset_src contains 'bar'", "str_wset_filtered", [elem("bar", 7), elem("foo", 5)], "filtered")
+    assert_summary_field("str_wset_src contains 'foo' or str_wset_src contains 'bar'", "str_wset_filtered", { "bar" => 7, "foo" => 5 }, "filtered")
 
     # The source fields are not filtered
     assert_summary_field("str_array_src contains 'bar'", "str_array_src", ["bar", "foo"])
-    assert_summary_field("str_wset_src contains 'bar'", "str_wset_src", [elem("bar", 7), elem("foo", 5)])
-  end
-
-  def elem(item, weight)
-    {"item" => item, "weight" => weight}
+    assert_summary_field("str_wset_src contains 'bar'", "str_wset_src", { "bar" => 7, "foo" => 5 })
   end
 
   def assert_summary_field(yql_filter, field_name, exp_field_value, summary = "default")
