d543f45e864d148b6de3f315ce2845e590be74b0
Arne H Juul <arnej@yahooinc.com>
2022-05-24 11:02:11 +0000

    update rendering for Vespa 8

--- tests/search/bugs/6874434/bug6874434.rb
+++ tests/search/bugs/6874434/bug6874434.rb
@@ -15,8 +15,10 @@ class PositionsBug < SearchTest
     start
     feed_and_wait_for_docs("test", 1, :file => selfdir+"feed.xml")
     vespa.adminserver.execute("vespa-visit --xmloutput")
-    #     save_result("query=title:test&streaming.userid=27959", selfdir+"result.xml")
+    #     save_result("query=title:test&streaming.userid=27959&format=xml", selfdir+"result.xml")
     assert_xml_result_with_timeout(2.0, "query=title:test&streaming.userid=27959", selfdir+"result.xml")
+    # save_result("query=title:test&streaming.userid=27959", selfdir+"result.json")
+    assert_result_with_timeout(2.0, "query=title:test&streaming.userid=27959", selfdir+"result.json")
   end
 
   def teardown
