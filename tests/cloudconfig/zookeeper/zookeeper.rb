64fa030323288c3a50d37be7d02cb5cf1e06403d
Harald Musum <musum@yahooinc.com>
2022-08-26 14:57:11 +0200

    Make test more robust to changes in output from ZooKeeper

--- tests/cloudconfig/zookeeper/zookeeper.rb
+++ tests/cloudconfig/zookeeper/zookeeper.rb
@@ -11,7 +11,7 @@ class ZooKeeper < CloudConfigTest
   def test_zookeeper_version
     deploy_app(CloudconfigApp.new)
 
-    output = vespa.nodeproxies.values.first.execute("vespa-zkflw localhost 2181 srvr |head -n 1")
+    output = vespa.nodeproxies.values.first.execute("vespa-zkflw localhost 2181 srvr | grep 'Zookeeper version'")
     expected_version = "3.8"
     assert(output.start_with?("Zookeeper version: #{expected_version}"),
            "Expected ZooKeeper version to be #{expected_version}, another version found in output: #{output}")
