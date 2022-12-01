a7bce6f1ad62a0d27613e56892112a4636234e44
Tor Egge <Tor.Egge@yahooinc.com>
2022-12-01 11:48:22 +0100

    Simplify sanitizer guard test.

--- tests/search/sanitizer_guard/sanitizer_guard.rb
+++ tests/search/sanitizer_guard/sanitizer_guard.rb
@@ -10,10 +10,9 @@ class SanitizerGuardTest < IndexedSearchTest
 
   def test_sanitizer_none
     set_description('Make sure we are not compiled with any sanitizers')
-    deploy_app(SearchApp.new.sd(selfdir + 'dummy.sd'))
-    searchnode = vespa.search['search'].first
+    node_proxy = vespa.nodeproxies.values.first
     command="#{Environment.instance.vespa_home}/bin/vespa-print-default sanitizers"
-    (exitcode, output) = searchnode.execute(command, {:exitcode => true, :exceptiononfailure => false})
+    (exitcode, output) = node_proxy.execute(command, {:exitcode => true, :exceptiononfailure => false})
     assert_equal(0, exitcode.to_i)
     assert_equal("none", output.chomp)
   end
