4a219f561a9f652aa5afedc32934356c35824582
Harald Musum <musum@yahooinc.com>
2023-02-10 13:22:05 +0100

    Reorder methods and remove unnecessary code

--- tests/performance/raw_indexing/raw_indexing.rb
+++ tests/performance/raw_indexing/raw_indexing.rb
@@ -13,16 +13,32 @@ class FeedingIndexTest < PerformanceTest
     set_owner("onorum")
     set_description("simple test for indexing and query performance")
     
-    @tainted = false
     @warmUpDocPath = "#{dirs.tmpdir}warm_up_docs.json"
     @docPath = "#{dirs.tmpdir}feed_docs.json"
+  end
 
+  def test_feeding_and_querying_docs
     deploy_app(create_app)
     @container = vespa.container.values.first
     set_up_files
 
     start
     vespa_destination_start
+
+    run_feeder(@warmUpDocPath, [parameter_filler("legend", "test_feeding_performance")], {localfile: true, :numthreads => 3, :route => '"combinedcontainer/chain.indexing null/default"'})
+
+    profiler_start
+    run_feeder(@docPath, [parameter_filler("legend", "test_feeding_performance")], {localfile: true})
+    profiler_report('profile_feed')
+
+    query_docs
+  end
+
+  def set_up_files
+    download_doc_file
+    make_warm_up_docs
+    make_feed_docs
+    make_queries
   end
 
   def download_doc_file
@@ -39,32 +55,6 @@ class FeedingIndexTest < PerformanceTest
 
   def make_queries
     @container.execute("cd #{dirs.tmpdir} && python3 #{selfdir}make_queries.py feed_docs.json queries.txt 3")
-  end
-
-  def set_up_files
-    download_doc_file
-    make_warm_up_docs
-    make_feed_docs
-    make_queries
-  end
-
-  def test_feeding_and_querying_docs
-    set_description("testing indexing performance and query performance")
-    if @tainted
-      puts 'Wiping existing index data on node to ensure previous tests do not pollute results'
-      node = vespa.storage['search'].storage['0']
-      node.stop_base
-      node.clean_indexes
-      node.start_base
-    end
-
-    run_feeder(@warmUpDocPath, [parameter_filler("legend", "test_feeding_performance")], {localfile: true, :numthreads => 3, :route => '"combinedcontainer/chain.indexing null/default"'})
-
-    profiler_start
-    run_feeder(@docPath, [parameter_filler("legend", "test_feeding_performance")], {localfile: true})
-    profiler_report('profile_feed')
-
-    query_docs
   end
 
   def query_docs
