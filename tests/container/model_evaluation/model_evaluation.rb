4365678a6ac2cb231a73550f571591f0b663963c
Arne Juul <arnej@yahooinc.com>
2023-02-12 11:07:27 +0000

    * update expected output (short tensors, with type information) * re-enable tests

--- tests/container/model_evaluation/model_evaluation.rb
+++ tests/container/model_evaluation/model_evaluation.rb
@@ -13,57 +13,57 @@ class ModelEvaluation < SearchContainerTest
     start
   end
 
-  def tensorflow_test_model_evaluation
+  def test_model_evaluation
 
     # ---- xgboost
     result = query("/models/?model=xgboost_2_2&function=xgboost_2_2")
-    assert_equal("{\"cells\":[{\"address\":{},\"value\":2.496898}]}", result.body)
+    assert_equal('{"type":"tensor()","cells":[{"address":{},"value":2.496898}]}', result.body)
 
     # ---- mnist_softmax (onnx)
     #    - no argument
     result = query("/models/?model=mnist_softmax&function=y" +
                    "&argumentName=Placeholder_0" +
                    "&argumentValue=" + CGI::escape(generateAllZeroArgument()))
-    assert_equal("{\"cells\":[{\"address\":{},\"value\":-1.6372650861740112E-6}]}", result.body)
+    assert_equal('{"type":"tensor()","cells":[{"address":{},"value":-1.6372650861740112E-6}]}', result.body)
     #    - with argument
     result = query("/models/?model=mnist_softmax&function=y" +
                    "&argumentName=Placeholder_0" +
                    "&argumentValue=" + CGI::escape(generateArgument()))
-    assert_equal("{\"cells\":[{\"address\":{},\"value\":2.4199485778808594E-5}]}", result.body)
-
+    assert_equal('{"type":"tensor()","cells":[{"address":{},"value":2.4199485778808594E-5}]}', result.body)
   end
 
-  def tensorflow_test_model_evaluation_rest_api
+  def test_model_evaluation_rest_api
 
     # list available models
     result = query("/model-evaluation/v1/")
+    puts "model list: #{result.body}"
     assert(result.body.include? "\"xgboost_2_2\"")
     assert(result.body.include? "\"mnist_softmax\"")
-    assert(result.body.include? "\"mnist_softmax_saved\"")
-    assert(result.body.include? "\"mnist_saved\"")
 
     # TODO: add type test when it is available
 
     # evaluate xboost_2_2 model (only has one function, thus optional)
     result = query("/model-evaluation/v1/xgboost_2_2/eval")
-    assert_equal("{\"cells\":[{\"address\":{},\"value\":2.496898}]}", result.body)
+    assert_equal('{"type":"tensor()","values":[2.496898]}', result.body)
 
     # evaluate mnist_softmax model (only has one function, thus optional)
     result = query("/model-evaluation/v1/mnist_softmax/eval" +
                    "?Placeholder_0=" + CGI::escape(generateArgument()))
-    assert_equal("{\"cells\":[{\"address\":{\"d0\":\"0\",\"d1\":\"0\"},\"value\":0.8232707381248474},{\"address\":{\"d0\":\"0\",\"d1\":\"1\"},\"value\":-9.757275581359863},{\"address\":{\"d0\":\"0\",\"d1\":\"2\"},\"value\":6.982227802276611},{\"address\":{\"d0\":\"0\",\"d1\":\"3\"},\"value\":6.577242851257324},{\"address\":{\"d0\":\"0\",\"d1\":\"4\"},\"value\":-6.682279109954834},{\"address\":{\"d0\":\"0\",\"d1\":\"5\"},\"value\":6.788743495941162},{\"address\":{\"d0\":\"0\",\"d1\":\"6\"},\"value\":0.4913627505302429},{\"address\":{\"d0\":\"0\",\"d1\":\"7\"},\"value\":-3.539299726486206},{\"address\":{\"d0\":\"0\",\"d1\":\"8\"},\"value\":1.3475146293640137},{\"address\":{\"d0\":\"0\",\"d1\":\"9\"},\"value\":-3.0314836502075195}]}", result.body)
+    assert_equal(
+      '{"type":"tensor<float>(d0[1],d1[10])","values":[[0.8232707381248474,-9.757275581359863,6.982227802276611,6.577242851257324,-6.682279109954834,6.788743495941162,0.4913627505302429,-3.539299726486206,1.3475146293640137,-3.0314836502075195]]}',
+      result.body)
 
     # ---- vespa (only tested with this API since we require 2 arguments which the ad hoc (models) API doesn't support
     #    - function using small constant
     result = query("/model-evaluation/v1/vespa_example/foo1/eval" +
                    "?input1=" + CGI::escape("{{name:a, x:0}: 1, {name:a, x:1}: 2, {name:a, x:2}: 3}") +
                    "&input2=" + CGI::escape("{{x:0}:3, {x:1}:6, {x:2}:9}"))
-    assert_equal("{\"cells\":[{\"address\":{},\"value\":202.5}]}", result.body)
+    assert_equal('{"type":"tensor()","values":[202.5]}', result.body)
     #    - function using large constant
     result = query("/model-evaluation/v1/vespa_example/foo2/eval" +
                    "?input1=" + CGI::escape("{{name:a, x:0}: 1, {name:a, x:1}: 2, {name:a, x:2}: 3}") +
                    "&input2=" + CGI::escape("{{x:0}:3, {x:1}:6, {x:2}:9}"))
-    assert_equal("{\"cells\":[{\"address\":{},\"value\":202.5}]}", result.body)
+    assert_equal('{"type":"tensor()","values":[202.5]}', result.body)
   end
 
   def query(query_string)
