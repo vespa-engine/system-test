7ed8d4b38f75b4ace70bcd8d063c5fe2a0f48421
Henning Baldersheim <balder@yahoo-inc.com>
2023-04-18 19:29:09 +0000

    We no longer encode base64 with padding

--- tests/search/raw_attributes/raw_attributes.rb
+++ tests/search/raw_attributes/raw_attributes.rb
@@ -16,16 +16,16 @@ class RawAttributesTest < IndexedStreamingSearchTest
     start
     feed_and_wait_for_docs('test', 5, :file => selfdir + 'docs.json')
     5.times do |id|
-      assert_field(['dGhpcyBpcyByYXcgZGF0YQ=='], id.to_s)
+      assert_field(['dGhpcyBpcyByYXcgZGF0YQ'], id.to_s)
     end
     assert_grouping('all(group(id) each(output(max(raw))))', selfdir + 'initial_group_by_id.json')
     assert_grouping('all(group(raw) each(output(sum(value))))', selfdir + 'initial_group_by_raw.json')
     feed(:file => selfdir + 'updates.json', :exceptiononfailure => true)
-    assert_field(['dGhpcyBpcyByYXcgZGF0YQ=='], '0') # "this is raw data"
+    assert_field(['dGhpcyBpcyByYXcgZGF0YQ'], '0') # "this is raw data"
     assert_field([nil], '1')
-    assert_field(['aGVsbG8gd29ybGQ='], '2')         # "hello world"
-    assert_field(['dGhpcyBpcyByYXcgZGF0YQ=='], '3')
-    assert_field(['0IA='], '4')                     # "Ѐ", i.e. 0xd0 0x80
+    assert_field(['aGVsbG8gd29ybGQ'], '2')         # "hello world"
+    assert_field(['dGhpcyBpcyByYXcgZGF0YQ'], '3')
+    assert_field(['0IA'], '4')                     # "Ѐ", i.e. 0xd0 0x80
     assert_grouping('all(group(id) each(output(max(raw))))', selfdir + 'final_group_by_id.json')
     assert_grouping('all(group(raw) each(output(sum(value))))', selfdir + 'final_group_by_raw.json')
     assert_sorting('-raw +id', selfdir + 'sort_by_desc_raw.json')
@@ -42,6 +42,12 @@ class RawAttributesTest < IndexedStreamingSearchTest
     else
       hit = vespa.document_api_v1.get("#{@id_prefix}#{id}")
       field = hit.fields['raw']
+      if exp_value
+        # TODO Remove this hack once we no longer render raw fields with padding.
+        remain = field[exp_value.length..]
+        assert("====".start_with?(remain))
+        field = field[0, exp_value.length]
+      end
     end
     assert_equal(exp_value, field)
   end
