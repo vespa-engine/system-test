f0666f01642488a01d17260a784d300d947854ab
Henning Baldersheim <balder@yahoo-inc.com>
2023-04-19 08:19:13 +0000

    Increase wantedDocumentCount from 1k to 4k

--- tests/performance/document_rest_api/visiting.rb
+++ tests/performance/document_rest_api/visiting.rb
@@ -96,7 +96,7 @@ class Visiting < PerformanceTest
                              parameters: parameters.merge({ :stream => true }))
 
         benchmark_operations(legend: "chunked-#{s_name}-#{concurrency}c-#{slices}s", selections: s_value,
-                             parameters: parameters.merge({ :wantedDocumentCount => 1024 })) if slices < 8
+                             parameters: parameters.merge({ :wantedDocumentCount => 4096 })) if slices < 8
       end
     end
   end
