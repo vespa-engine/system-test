7c9b350e1828f690875c9e5e99ffb12427022e41
Håvard Pettersen <havardpe@yahooinc.com>
2023-05-09 12:39:56 +0000

    test conditional put and delete

--- tests/vds/documentapi/document_api_v1_base.rb
+++ tests/vds/documentapi/document_api_v1_base.rb
@@ -23,8 +23,8 @@ class DocumentApiV1Base < SearchTest
     start
 
     # Just to make it possible to run test repeatedly without taking down cluster
-    vespa.document_api_v1.http_delete('/document/v1/storage_test/music/number/1/8')
-    vespa.document_api_v1.http_delete('/document/v1/storage_test/music/number/2/9')
+    api_http_delete('/document/v1/storage_test/music/number/1/8')
+    api_http_delete('/document/v1/storage_test/music/number/2/9')
 
     feed_single(1, 8)
   end
@@ -52,18 +52,35 @@ class DocumentApiV1Base < SearchTest
     vespa.document_api_v1.http_put(path, content, {}, headers)
   end
 
+  def api_http_delete(path)
+    vespa.document_api_v1.http_delete(path)
+  end
+
   def api_http_get(path)
     response = vespa.document_api_v1.http_get(path)
     vespa.document_api_v1.assert_response_ok(response)
     response.body
   end
 
-  def feed_single(uid, doc_num, title = 'title')
-    response = api_http_post("/document/v1/storage_test/music/number/#{uid}/#{doc_num}", "{\"fields\":{\"title\":\"#{title}\"}}")
+  def feed_single(uid, doc_num, title = 'title', condition = nil, create = false)
+    maybe_condition = condition.nil? ? "" : "?condition=#{CGI.escape(condition)}"
+    maybe_create = create ? '"create":true,' : ""
+    response = api_http_post("/document/v1/storage_test/music/number/#{uid}/#{doc_num}#{maybe_condition}",
+                             "{#{maybe_create}\"fields\":{\"title\":\"#{title}\"}}")
     assert_json_string_equal(
       "{\"id\":\"id:storage_test:music:n=#{uid}:#{doc_num}\",\"pathId\":\"/document/v1/storage_test/music/number/#{uid}/#{doc_num}\"}",
       response)
     response
+  end
+
+  def assert_title(uid, doc_num, title)
+    fields = JSON.parse(api_http_get("/document/v1/storage_test/music/number/#{uid}/#{doc_num}"))['fields']
+    assert_equal({'title' => title}, fields)
+  end
+
+  def assert_not_found(uid, doc_num)
+    response = vespa.document_api_v1.http_get("/document/v1/storage_test/music/number/#{uid}/#{doc_num}")
+    assert_equal(404, response.code.to_i)
   end
 
   def assert_fails_with_precondition_violation
@@ -79,4 +96,3 @@ class DocumentApiV1Base < SearchTest
     stop
   end
 end
-
