1d582eb338f661d471d5c63766c05e00a67954ae
Bjørn Christian Seime <bjorncs@yahooinc.com>
2023-05-22 13:38:43 +0200

    Add feeding benchmark for paraphrase-multilingual-MiniLM-L12-v2 ONNX model

---
+++ tests/performance/embedding/embedding.rb
@@ -0,0 +1,25 @@
+require 'performance_test'
+require 'app_generator/search_app'
+require 'environment'
+
+
+class Embedding < PerformanceTest
+
+  def setup
+    set_owner("bjorncs")
+  end
+
+  def test_huggingface
+    set_description('Benchmark feed throughput with paraphrase-multilingual-MiniLM-L12-v2 ONNX model')
+    deploy(selfdir + "app")
+    start
+    feed_file = 'miracl-te-docs.100k.json.gz'
+    remote_file = "https://data.vespa.oath.cloud/tests/performance/#{feed_file}"
+    local_file =  dirs.tmpdir + feed_file
+    cmd = "wget -O'#{local_file}' '#{remote_file}'"
+    puts "Running command #{cmd}"
+    result = `#{cmd}`
+    puts "Result: #{result}"
+    run_feeder(local_file, [])
+  end
+end
