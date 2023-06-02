ffa42ecfba03614f60a0c9ce71a32a1b7e322c72
Geir Storli <geirst@yahooinc.com>
2023-06-02 13:05:12 +0000

    Make create_docs more flexible for large scale testing.

--- tests/performance/nearest_neighbor_streaming/create_docs.cpp
+++ tests/performance/nearest_neighbor_streaming/create_docs.cpp
@@ -4,19 +4,20 @@
 #include <iostream>
 #include <random>
 #include <string>
+#include <unistd.h>
 #include <vector>
 
-bool verify_usage(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cerr << argv[0] << " <batches> <dimension>" << std::endl;
-        return false;
+using IntVec = std::vector<int>;
+
+size_t calc_total_docs(const IntVec& spec) {
+    size_t res = 0;
+    for (size_t i = 0; i < spec.size(); i += 2) {
+        res += spec[i] * spec[i + 1];
     }
-    return true;
+    return res;
 }
 
-using IntVec = std::vector<uint32_t>;
-
-void populate_ids(IntVec& ids, uint32_t num_users, uint32_t docs_per_user, uint32_t start_id) {
+void populate_ids_for_batch(IntVec& ids, int num_users, int docs_per_user, int start_id) {
     for (size_t i = 0; i < num_users; ++i) {
         for (size_t j = 0; j < docs_per_user; ++j) {
             ids.push_back(start_id + i);
@@ -24,23 +25,26 @@ void populate_ids(IntVec& ids, uint32_t num_users, uint32_t docs_per_user, uint3
     }
 }
 
-IntVec populate_ids(uint32_t batches) {
+IntVec populate_user_ids(const IntVec& spec, size_t total_docs, int parts, int part_id) {
     IntVec ids;
-    ids.reserve(500000 * batches);
-    uint32_t id_range = 10000000;
-    // One batch contains 500k documents, with 100k documents in each of the five categories:
-    // 10, 100, 1000, 10000, and 100000 docs per user.
-    populate_ids(ids, 10000 * batches, 10,     id_range);
-    populate_ids(ids, 1000 * batches,  100,    id_range * 2);
-    populate_ids(ids, 100 * batches,   1000,   id_range * 3);
-    populate_ids(ids, 10 * batches,    10000,  id_range * 4);
-    populate_ids(ids, 1 * batches,     100000, id_range * 5);
+    size_t docs_per_part = total_docs / parts;
+    ids.reserve(docs_per_part);
+    int user_id_range = 10000000;
+    int batch = 1;
+    for (size_t i = 0; i < spec.size(); i += 2) {
+        int docs_per_user = spec[i];
+        int num_users = spec[i + 1];
+        int users_per_part = num_users / parts;
+        int start_user_id = (user_id_range * batch) + (users_per_part * part_id);
+        populate_ids_for_batch(ids, users_per_part, docs_per_user, start_user_id);
+        ++batch;
+    }
     return ids;
 }
 
 void gen_vector(size_t dimension) {
     for (size_t i = 0; i < dimension; i++) {
-        float v = (rand() % 10000) / 10000.0;
+        float v = (rand() % 100000) / 100000.0;
         if (i > 0) {
             printf(",%f", v);
         } else {
@@ -49,35 +53,84 @@ void gen_vector(size_t dimension) {
     }
 }
 
-void gen_put(uint32_t user_id, uint32_t doc_id, uint32_t dimension) {
-    printf("{\"put\":\"id:test:test:n=%u:%u\",\"fields\":{\"id\":%u,\"embedding\":[", user_id, doc_id, doc_id);
+void gen_put(int user_id, size_t doc_id, int dimension) {
+    printf("{\"put\":\"id:test:test:n=%u:%zu\",\"fields\":{\"id\":%u,\"embedding\":[", user_id, doc_id, doc_id);
     gen_vector(dimension);
     printf("]}}");
 }
 
-void gen_puts(const IntVec& ids, uint32_t dimension) {
+void gen_puts(const IntVec& user_ids, size_t start_doc_id, int dimension) {
     printf("[\n");
-    for (size_t i = 0; i < ids.size(); i++) {
-        uint32_t user_id = ids[i];
+    for (size_t i = 0; i < user_ids.size(); i++) {
+        int user_id = user_ids[i];
+        size_t doc_id = start_doc_id + i;
         if (i > 0) {
             printf(",\n");
         }
-        gen_put(user_id, i, dimension);
+        gen_put(user_id, doc_id, dimension);
     }
     printf("\n]\n");
 }
 
 int main(int argc, char *argv[]) {
-    if (!verify_usage(argc, argv) ) {
+    int dimension = 1;
+    int parts = 1;
+    int part_id = 0;
+    int seed = 1234;
+    bool shuffle = true;
+
+    int option;
+    while ((option = getopt(argc, argv, "d:p:i:ho")) != -1) {
+        switch (option) {
+            case 'd':
+                dimension = std::stoi(optarg);
+                break;
+            case 'p':
+                parts = std::stoi(optarg);
+                break;
+            case 'i':
+                part_id = std::stoi(optarg);
+                break;
+            case 'o':
+                shuffle = false;
+                break;
+            case 'h':
+                std::cerr << argv[0] << " -o (ordered by user id) -h (help) -d <dimension> -p <parts> -i <part_id> <user batch spec>" << std::endl;
+                std::cerr << "The user batch spec is a list of {docs_per_user, num_users} pairs." << std::endl;
+                std::cerr << "Example: -d 128 -p 2 -i 0 10 2000 100 50" << std::endl;
+                std::cerr << "    Where 2000 users have 10 documents each, and 50 users have 100 documents each." << std::endl;
+                std::cerr << "    In total 25000 documents, where this invocation generates part 0 (of 2 in total)." << std::endl;
+                return 1;
+            default:
+                return 1;
+        }
+    }
+    if ((argc - optind) % 2 != 0) {
+        std::cerr << "The user batch spec is malformed" << std::endl;
         return 1;
     }
-    uint32_t batches = strtoul(argv[1], nullptr, 0);
-    uint32_t dimension = strtoul(argv[2], nullptr, 0);
-    auto ids = populate_ids(batches);
-    std::default_random_engine engine(1234);
-    std::shuffle(std::begin(ids), std::end(ids), engine);
-    srand(1234);
-    gen_puts(ids, dimension);
+    IntVec spec;
+    for (int i = optind; i < argc; ++i) {
+        spec.push_back(std::stoi(argv[i]));
+    }
+    for (size_t i = 0; i < spec.size(); i += 2) {
+        int docs_per_user = spec[i];
+        int num_users = spec[i + 1];
+        if (num_users % parts != 0) {
+            std::cerr << "Number of users in {" << docs_per_user << "," << num_users << "} cannot be evenly split into parts (" << parts << ")" << std::endl;
+            return 1;
+        }
+    }
+
+    size_t total_docs = calc_total_docs(spec);
+    auto user_ids = populate_user_ids(spec, total_docs, parts, part_id);
+    if (shuffle) {
+        std::default_random_engine engine(seed);
+        std::shuffle(std::begin(user_ids), std::end(user_ids), engine);
+    }
+    srand(seed);
+    size_t start_doc_id = (total_docs / parts) * part_id;
+    gen_puts(user_ids, start_doc_id, dimension);
     return 0;
 }
 
