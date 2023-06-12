f52e760874c707dd9ede911748e4e602c39e7be1
Harald Musum <musum@yahooinc.com>
2023-06-12 22:44:42 +0200

    Blob is already decoded, don't do it twice

--- tests/search/event-loggging/project/src/main/java/com/yahoo/example/EventStoreImpl.java
+++ tests/search/event-loggging/project/src/main/java/com/yahoo/example/EventStoreImpl.java
@@ -4,7 +4,6 @@ package com.yahoo.example;
 import com.yahoo.component.AbstractComponent;
 import com.yahoo.search.logging.LoggerEntry;
 import com.yahoo.text.Utf8;
-import java.util.Base64;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,7 +19,7 @@ public class EventStoreImpl extends AbstractComponent implements EventStore {
     private volatile String last = "";
 
     public void add(LoggerEntry entry) {
-        last = Utf8.toString(Base64.getDecoder().decode(entry.blob().array()));
+        last = Utf8.toString(entry.blob().array());
         log.log(Level.FINE, "Adding event " + entry + ", last blob=" + last);
         events.incrementAndGet();
     }
