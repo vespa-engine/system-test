0f8dbeeea92c433767dd9ea2142e7a15d71bacc1
gjoranv <gv@yahooinc.com>
2023-06-16 13:44:56 +0200

    Add new test for a special type of container failure.

---
+++ tests/container/component_invalid_app2/Fail2Handler.java
@@ -0,0 +1,24 @@
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.test;
+
+import java.util.List;
+import java.util.ArrayList;
+import com.yahoo.jdisc.Request;
+import com.yahoo.jdisc.Response;
+import com.yahoo.jdisc.handler.*;
+
+public class Fail2Handler extends AbstractRequestHandler {
+
+    public Fail2Handler() {
+        String nullString = null;
+        System.out.println("This will throw an IndexOutOfBoundsException: " + nullString.length());
+    }
+
+    @Override
+    public ContentChannel handleRequest(Request request, ResponseHandler handler) {
+        FastContentWriter writer = ResponseDispatch.newInstance(Response.Status.OK).connectFastWriter(handler);
+        writer.write("Fail2");
+        writer.close();
+        return null;
+    }
+}
---
+++ tests/container/component_invalid_app2/src/main/java/com/yahoo/test/Ok1Handler.java
@@ -0,0 +1,24 @@
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.yahoo.test;
+
+import com.yahoo.jdisc.Request;
+import com.yahoo.jdisc.Response;
+import com.yahoo.jdisc.handler.*;
+import com.yahoo.test.ResponseConfig;
+
+public class Ok1Handler extends AbstractRequestHandler {
+
+    private final String response;
+
+    public Ok1Handler(ResponseConfig config) {
+        this.response = config.response();
+    }
+
+    @Override
+    public ContentChannel handleRequest(Request request, ResponseHandler handler) {
+        FastContentWriter writer = ResponseDispatch.newInstance(Response.Status.OK).connectFastWriter(handler);
+        writer.write(response);
+        writer.close();
+        return null;
+    }
+}
