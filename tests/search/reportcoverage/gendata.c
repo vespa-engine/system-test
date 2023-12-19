34fa351efb39ab1eb179781492f921c3a8b4b627
Henning Baldersheim <balder@yahoo-inc.com>
2023-12-19 14:33:31 +0000

    Add comment about randomness.

--- tests/search/reportcoverage/gendata.c
+++ tests/search/reportcoverage/gendata.c
@@ -6,30 +6,31 @@ const double inv = 123456789.0 / RAND_MAX;
 
 void header(int num)
 {
-	printf("<document type='covtest' id='id:test:covtest::%d'>\n", num);
-        printf("<title>the coverage %d test</title>\n", num);
+    printf("<document type='covtest' id='id:test:covtest::%d'>\n", num);
+    printf("<title>the coverage %d test</title>\n", num);
 }
 
 void footer(int num)
 {
-	double score = random() * inv;
-	printf("<sortlimnum>%d</sortlimnum>\n", (int)score);
-	score = random() * inv * 0.001;
-	printf("<weight>%f</weight>\n", score);
-	printf("</document>\n");
+    // TODO Avoid randomness to make matchphase limiting more predictable.
+    double score = random() * inv;
+    printf("<sortlimnum>%d</sortlimnum>\n", (int)score);
+    score = random() * inv * 0.001;
+    printf("<weight>%f</weight>\n", score);
+    printf("</document>\n");
 }
 
 int main(int argc, char **argv)
 {
-	int i;
-	int documents = 100000;
-	if (argc > 1) { documents = atoi(argv[1]); }
-	srandom(42);
-	printf("<vespafeed>\n");
-	for (i = 0; i < documents; i++) {
-		header(i);
-		footer(i);
-	}
-	printf("</vespafeed>\n");
-	return 0;
+    int i;
+    int documents = 100000;
+    if (argc > 1) { documents = atoi(argv[1]); }
+    srandom(42);
+    printf("<vespafeed>\n");
+    for (i = 0; i < documents; i++) {
+        header(i);
+        footer(i);
+    }
+    printf("</vespafeed>\n");
+    return 0;
 }
