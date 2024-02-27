b40fac7661ccfa7ea5a83f9484c1d5ff951ad40c
Tor Egge <Tor.Egge@online.no>
2024-02-27 13:00:34 +0100

    Run clearfield system test in indexed and streaming mode.

--- tests/search/clearfield/clearfield.rb
+++ tests/search/clearfield/clearfield.rb
@@ -1,8 +1,8 @@
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-require 'search_test'
+require 'indexed_streaming_search_test'
 
-class ClearField < SearchTest
+class ClearField < IndexedStreamingSearchTest
 
   def setup
     set_owner("balder")
