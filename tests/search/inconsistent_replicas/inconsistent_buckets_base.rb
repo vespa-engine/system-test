6057cca2e6570ede5412bd29eb3f3668c458269d
Tor Egge <Tor.Egge@online.no>
2024-02-27 17:20:03 +0100

    Run inconsistent replicas system tests only in indexed mode.

--- tests/search/inconsistent_replicas/inconsistent_buckets_base.rb
+++ tests/search/inconsistent_replicas/inconsistent_buckets_base.rb
@@ -1,7 +1,7 @@
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-require 'search_test'
+require 'indexed_only_search_test'
 
-class InconsistentBucketsBase < SearchTest
+class InconsistentBucketsBase < IndexedOnlySearchTest
 
   def setup
     deploy_app(make_app())
