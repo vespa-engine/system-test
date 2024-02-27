fb5d693372d5c3f4239f32f2e886cfae403dbe8a
Tor Egge <Tor.Egge@online.no>
2024-02-27 13:08:03 +0100

    Run config library system test in indexed and streaming mode.

--- tests/search/configlibrary/configlibrary.rb
+++ tests/search/configlibrary/configlibrary.rb
@@ -1,7 +1,7 @@
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-require 'search_test'
+require 'indexed_streaming_search_test'
 
-class ConfigLibrary < SearchTest
+class ConfigLibrary < IndexedStreamingSearchTest
 
   def initialize(*args)
     super(*args)
