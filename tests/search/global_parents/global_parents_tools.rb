de0f5214032ea2af6f4b881769982f04a2d3e717
Tor Egge <Tor.Egge@online.no>
2024-02-27 16:32:38 +0100

    Run global parents system tests only in indexed mode.

--- tests/search/global_parents/global_parents_tools.rb
+++ tests/search/global_parents/global_parents_tools.rb
@@ -1,7 +1,7 @@
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-require 'search_test'
+require 'indexed_only_search_test'
 
-class GlobalParentsToolTest < SearchTest
+class GlobalParentsToolTest < IndexedOnlySearchTest
 
   def setup
     set_owner("toregge")
