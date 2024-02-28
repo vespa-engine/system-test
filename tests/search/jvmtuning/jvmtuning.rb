8fc231b4aeabb157b2aa6bd25b910f11058e2825
Tor Egge <Tor.Egge@online.no>
2024-02-28 15:48:37 +0100

    Run jvm tuning system test only in indexed mode.

--- tests/search/jvmtuning/jvmtuning.rb
+++ tests/search/jvmtuning/jvmtuning.rb
@@ -1,9 +1,9 @@
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 require 'docproc_test'
-require 'search_test'
+require 'indexed_only_search_test'
 require 'app_generator/http'
 
-class JvmTuning < SearchTest
+class JvmTuning < IndexedOnlySearchTest
 
   def can_share_configservers?(method_name=nil)
     false
