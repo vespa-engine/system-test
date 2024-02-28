41c7f317033a96ffd0fb00bbbcfd5e0f0141fd26
Tor Egge <Tor.Egge@online.no>
2024-02-28 12:46:54 +0100

    Run match phase degradation system test only in indexed mode.

--- tests/search/matchphasedegradation/match_phase_degradation_test.rb
+++ tests/search/matchphasedegradation/match_phase_degradation_test.rb
@@ -1,9 +1,9 @@
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-require 'search_test'
+require 'indexed_only_search_test'
 require 'app_generator/container_app'
 require 'pp'
 
-class MatchPhaseDegradationTest < SearchTest
+class MatchPhaseDegradationTest < IndexedOnlySearchTest
 
   def setup
     set_owner("arnej")
