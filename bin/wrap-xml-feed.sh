6d706f16eaaa49340fc8cd4fced08727aa510c25
Henning Baldersheim <balder@yahoo-inc.com>
2024-03-01 13:53:23 +0000

    Avoid repeating header by just checking number of lines matching pattern

--- bin/wrap-xml-feed.sh
+++ bin/wrap-xml-feed.sh
@@ -2,9 +2,8 @@
 # If there is no xml header in the given file it will prepend a header,
 # and append a footer to make it legal xml feed.
 
-head -1 $1 | grep "xml version"
-valid_xml=$?
-if [ $valid_xml -ne 0 ]; then
+valid_xml=`head -1 $1 | grep "xml version" | wc -l`
+if [ $valid_xml -ne 1 ]; then
     echo '<?xml version="1.0" encoding="utf-8"?>'
     echo "<vespafeed>"
     cat $1 
