a7fc08c0e7032d4e83f7e8ac4db70a708f3d4f2f
Harald Musum <musum@yahooinc.com>
2024-03-07 10:42:41 +0100

    Test should be for indexed only and use app generator

--- tests/search/linguistics/lucene/lucene_linguistics.rb
+++ tests/search/linguistics/lucene/lucene_linguistics.rb
@@ -1,7 +1,7 @@
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-require 'indexed_search_test'
+require 'indexed_only_search_test'
 
-class LuceneLinguistics < IndexedSearchTest
+class LuceneLinguistics < IndexedOnlySearchTest
 
   def setup
     set_owner("hmusum")
@@ -9,11 +9,24 @@ class LuceneLinguistics < IndexedSearchTest
   end
 
   def test_simple_linguistics
-    deploy(selfdir + "app/")
+    deploy_app(SearchApp.new.
+                 container(
+                   Container.new('default').
+                     component(lucene_linguistics_component).
+                     search(Searching.new).
+                     docproc(DocumentProcessing.new)).
+                 sd(selfdir + 'lucene.sd'))
     start
     feed_and_wait_for_docs("lucene", 1, :file => selfdir + "document.json")
 
     assert_hitcount("query=dog", 1)
+  end
+
+  def lucene_linguistics_component
+      Component.new('lucene-linguistics').
+        klass('com.yahoo.language.lucene.LuceneLinguistics').
+        bundle('lucene-linguistics').
+        config(ConfigOverride.new('com.yahoo.language.lucene.lucene-analysis'))
   end
 
   def teardown
