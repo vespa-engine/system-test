58f98d1bbc78dfd78abc461ba5f51c0ada5635f3
Harald Musum <musum@yahooinc.com>
2024-03-11 12:26:17 +0100

    Convert feed files to json, part 2

--- tests/docproc/multichain-multisearch-additionalchain/multichain-multisearch-additionalchain.rb
+++ tests/docproc/multichain-multisearch-additionalchain/multichain-multisearch-additionalchain.rb
@@ -39,10 +39,10 @@ class MultiChainMultiSearchAdditionalChain < DocprocTest
   end
 
   def test_multichain_multisearch_additionalchain
-    feed_and_wait_for_docs("music", 4, :file => selfdir+"music.4.xml", :cluster => "music", :route => "\"default/chain.default default/chain.jalla indexing\"")
+    feed_and_wait_for_docs("music", 4, :file => selfdir+"music.4.json", :cluster => "music", :route => "\"default/chain.default default/chain.jalla indexing\"")
     assert_result("query=sddocname:music", selfdir + "music.4.result.json")
 
-    feed_and_wait_for_docs("muzak", 4, :file => selfdir+"muzak.4.xml", :cluster => "muzak", :route => "\"default/chain.default default/chain.jalla indexing\"")
+    feed_and_wait_for_docs("muzak", 4, :file => selfdir+"muzak.4.json", :cluster => "muzak", :route => "\"default/chain.default default/chain.jalla indexing\"")
     assert_result("query=sddocname:muzak", selfdir + "muzak.4.result.json")
   end
 
