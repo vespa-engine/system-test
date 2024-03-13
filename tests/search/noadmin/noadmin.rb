6f437229f9b2ed4cb7829f191666dc80d9884040
Tor Egge <Tor.Egge@online.no>
2024-03-13 16:37:30 +0100

    Rewrite no admin system test to app generator.

--- tests/search/noadmin/noadmin.rb
+++ tests/search/noadmin/noadmin.rb
@@ -1,7 +1,13 @@
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-require 'search_test'
+require 'indexed_search_test'
 
-class NoAdminInServices < SearchTest
+class NoAdminInServices < IndexedSearchTest
+
+  class NoAdmin < Admin
+    def to_xml(indent)
+      ''
+    end
+  end
 
   def setup
     set_owner("musum")
@@ -9,12 +15,7 @@ class NoAdminInServices < SearchTest
   end
 
   def test_no_admin_search
-    deploy(selfdir + "app-search", SEARCH_DATA + "music.sd")
-    start_and_feed
-  end
-
-  def test_no_admin_content
-    deploy(selfdir + "app-content", SEARCH_DATA + "music.sd")
+    deploy_app(SearchApp.new.admin(NoAdmin.new).sd(SEARCH_DATA + 'music.sd'))
     start_and_feed
   end
 
