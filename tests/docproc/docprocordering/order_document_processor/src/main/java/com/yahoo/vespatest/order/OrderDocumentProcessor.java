95480831fb08fb8b9e6340c9050ee6441de7b239
Harald Musum <musum@vespa.ai>
2024-03-14 13:40:49 +0100

    Revert "Revert "Handle empty array""

--- tests/docproc/docprocordering/order_document_processor/src/main/java/com/yahoo/vespatest/order/OrderDocumentProcessor.java
+++ tests/docproc/docprocordering/order_document_processor/src/main/java/com/yahoo/vespatest/order/OrderDocumentProcessor.java
@@ -5,19 +5,23 @@ import com.yahoo.docproc.DocumentProcessor;
 import com.yahoo.document.datatypes.Array;
 import com.yahoo.document.datatypes.StringFieldValue;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * @author Einar M R Rosenvinge
  */
 public abstract class OrderDocumentProcessor extends DocumentProcessor {
 
     protected void assertArraySize(Array<StringFieldValue> stringArray, int correctSize) {
-        if (stringArray.size() != correctSize) {
-            throw new RuntimeException(this + " found array with size " + stringArray.size() + " for field 'stringarray', order must be wrong");
+        int size = stringArray == null ? 0 : stringArray.size();
+        if (size != correctSize) {
+            throw new RuntimeException(this + " found array with size " + size + " for field 'stringarray', order must be wrong");
         }
 
     }
 
     protected void assertArray(Array<StringFieldValue> stringArray, int index, StringFieldValue correctString) {
+        requireNonNull(stringArray, "array cannot be null");
         if (!stringArray.get(index).equals(correctString)) {
             throw new RuntimeException(this + " found element '" + stringArray.get(index) + "' at position " + index + ", order must be wrong");
         }
