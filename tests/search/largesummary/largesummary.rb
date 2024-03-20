50757de1b628f9a19061f42c8c6f5ef00736a79d
Harald Musum <musum@yahooinc.com>
2024-03-20 20:45:05 +0100

    Convert feed files to json

--- tests/search/largesummary/largesummary.rb
+++ tests/search/largesummary/largesummary.rb
@@ -10,7 +10,7 @@ class LargeSummary < IndexedStreamingSearchTest
   end
 
   def test_largesummary
-    feed_and_wait_for_docs("music", 1, :file => selfdir+"largesummary.1.xml", :timeout => 240)
+    feed_and_wait_for_docs("music", 1, :file => selfdir+"largesummary.1.json", :timeout => 240)
     assert_hitcount("query=sddocname:music", 1)
     result = search("query=sddocname:music", 0)
     assert(result.hit[0].field["song"].to_s.length > 65536, "Field length is #{result.hit[0].field["song"].to_s.length}, expected more than 65536")
