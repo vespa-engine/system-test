df037b50a742d7ed74afa12ea9359f54c6128b9d
Geir Storli <geirst@yahooinc.com>
2024-04-05 14:15:56 +0000

    Also test IN operator with 10k and 100k tokens.
    This also changes to use a Java Searcher to populate the InItem,
    otherwise the HTTP GET request becomes too large.

---
+++ tests/performance/in_operator/InItemBuilder.java
@@ -0,0 +1,46 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package ai.vespa.test;
+
+import com.yahoo.component.chain.dependencies.After;
+import com.yahoo.prelude.query.Item;
+import com.yahoo.prelude.query.NumericInItem;
+import com.yahoo.prelude.query.ToolBox;
+import com.yahoo.processing.request.CompoundName;
+import com.yahoo.search.Query;
+import com.yahoo.search.Result;
+import com.yahoo.search.Searcher;
+import com.yahoo.search.searchchain.Execution;
+
+@After("ExternalYql")
+public class InItemBuilder extends Searcher {
+    @Override
+    public Result search(Query query, Execution execution) {
+        var lower = query.properties().getLong(new CompoundName("inbuilder.lower"));
+        var upper = query.properties().getLong(new CompoundName("inbuilder.upper"));
+        if (lower != null && upper != null) {
+            ToolBox.visit(new InItemVisitor(lower, upper), query.getModel().getQueryTree().getRoot());
+        }
+        return execution.search(query);
+    }
+
+    private static class InItemVisitor extends ToolBox.QueryVisitor {
+
+        private long lower;
+        private long upper;
+
+        public InItemVisitor(long lower, long upper) {
+            this.lower = lower;
+            this.upper = upper;
+        }
+
+        @Override
+        public boolean visit(Item item) {
+            if (item instanceof NumericInItem inItem) {
+                for (long token = lower; token < upper; ++token) {
+                    inItem.addToken(token);
+                }
+            }
+            return true;
+        }
+    }
+}
