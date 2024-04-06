75d7604ee7c7fdbc950808900433e2bcae1d800d
Harald Musum <musum@yahooinc.com>
2024-04-06 11:15:29 +0200

    Convert feed files to json, part 162

--- tests/search/routing/routing.rb
+++ tests/search/routing/routing.rb
@@ -36,7 +36,7 @@ class RoutingTest < IndexedStreamingSearchTest
   def test_multicluster_with_selection_get
     deploy_app(two_cluster_app)
     start
-    feed_and_wait_for_docs("music", 1, :file => selfdir + "bookandmusic_feed.xml", :trace => 9)
+    feed_and_wait_for_docs("music", 1, :file => selfdir + "bookandmusic_feed.json", :trace => 9)
     vespa.document_api_v1.put(getIronMaiden)
     wait_for_hitcount("sddocname:music", 2)
     assert_result("search=music&query=metallica", selfdir + "metallica_result.json")
@@ -48,19 +48,19 @@ class RoutingTest < IndexedStreamingSearchTest
   def test_feedToSearchAndStorage
     deploy_app(three_cluster_app)
     start
-    # Feed to search. Because the feed.xml file only contains the actual document we need to pre- and postfix the start-
+    # Feed to search. Because the feed.json file only contains the actual document we need to pre- and postfix the start-
     # and end-of-feed elements.
-    feed_and_wait_for_docs("music", 1, :file => selfdir + "bobdylan_feed.xml", :route => "indexing", :trace => 9)
+    feed_and_wait_for_docs("music", 1, :file => selfdir + "bobdylan_feed.json", :route => "indexing", :trace => 9)
     assert_result("search=music&query=bob", selfdir + "bobdylan_result.json")
 
     # Feed to storage. Here we need to use the 'feed' method that creates the necessary vespafeed elements surrounding
-    # the content of the feed.xml.
-    feed(:file => selfdir + "bobdylan_feed.xml", :route => "storage", :trace => 9)
+    # the content of the feed.json.
+    feed(:file => selfdir + "bobdylan_feed.json", :route => "storage", :trace => 9)
     assert_equal(getBobDylan(), vespa.document_api_v1.get("id:music:music::http://music.yahoo.com/bobdylan/BestOf", @get_params))
 
     # Feed to both search and storage. Here we can use the 'index' method since that will generate the surrouding vespa-,
     # start- and end-of-feed elements. When sending to search AND storage, at least one recipient accepts them.
-    feed_and_wait_for_docs("music", 2, :file => selfdir + "metallica_feed.xml", :route => "\"[AND:indexing storage]\"", :trace => 9)
+    feed_and_wait_for_docs("music", 2, :file => selfdir + "metallica_feed.json", :route => "\"[AND:indexing storage]\"", :trace => 9)
     assert_result("search=music&query=metallica", selfdir + "metallica_result.json")
     assert_equal(getMetallica(), vespa.document_api_v1.get("id:music:music::http://music.yahoo.com/metallica/BestOf", @get_params))
   end
@@ -68,7 +68,7 @@ class RoutingTest < IndexedStreamingSearchTest
   def test_musicClusterIgnoresBooks
     deploy_app(three_cluster_app)
     start
-    feed_and_wait_for_docs("music", 1, :file => selfdir + "bookandmusic_feed.xml", :route => "\"[AND:indexing storage]\"", :trace => 9)
+    feed_and_wait_for_docs("music", 1, :file => selfdir + "bookandmusic_feed.json", :route => "\"[AND:indexing storage]\"", :trace => 9)
     vespa.document_api_v1.put(getIronMaiden)
     wait_for_hitcount("sddocname:music", 2)
     assert_result("search=music&query=metallica", selfdir + "metallica_result.json")
