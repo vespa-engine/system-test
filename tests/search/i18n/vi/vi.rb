3efb9648fb03e40a715f5317364fb3d57e2e7783
Harald Musum <musum@yahooinc.com>
2024-04-07 11:17:48 +0200

    Convert feed files to json, part 168

--- tests/search/i18n/vi/vi.rb
+++ tests/search/i18n/vi/vi.rb
@@ -11,7 +11,7 @@ class Vi < IndexedStreamingSearchTest
   end
 
   def test_vietnamese_deaccent
-    feed_and_wait_for_docs("vietnamese", 3, :file => "#{selfdir}/vietnamese.xml")
+    feed_and_wait_for_docs("vietnamese", 3, :file => "#{selfdir}/vietnamese.json")
 
     # accented query
     assert_hitcount_with_timeout(10, 'query=%C3%A2m%20d%C6%B0%C6%A1ng%20l%E1%BB%8Bch%20vi%E1%BB%87t%20nam&language=vi&type=all', 2)
