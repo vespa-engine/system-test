4e521c7bc4e9c364820dda737e2fe3e253385c87
Harald Musum <musum@vespa.ai>
2024-04-16 21:14:03 +0200

    Revert "Use vespa-feed-client as default feed client"

--- lib/testcase.rb
+++ lib/testcase.rb
@@ -143,7 +143,7 @@ class TestCase
 
   # Returns the name of the feeder binary to be used.
   def feeder_binary
-    "vespa-feed-client"
+    "vespa-feeder --abortondataerror no --abortonsenderror no"
   end
 
   def can_share_configservers?(method_name=nil)
