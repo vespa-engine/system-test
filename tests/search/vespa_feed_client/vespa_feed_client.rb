d2b50e53446558092ce8056ccc9c2401aa7308ca
Harald Musum <musum@yahooinc.com>
2024-04-23 11:44:57 +0200

    Simplify by feeding to default container

--- tests/search/vespa_feed_client/vespa_feed_client.rb
+++ tests/search/vespa_feed_client/vespa_feed_client.rb
@@ -6,7 +6,7 @@ class VespaFeedClient < IndexedStreamingSearchTest
 
   def setup
     set_owner("vekterli")
-    deploy_app(SearchApp.new.sd(SEARCH_DATA + "music.sd").enable_document_api)
+    deploy_app(SearchApp.new.sd(SEARCH_DATA + "music.sd"))
     start
   end
 
@@ -14,16 +14,16 @@ class VespaFeedClient < IndexedStreamingSearchTest
     node = vespa.nodeproxies.values.first
 
     # Feed with valid docs
-    (exitcode, output) = feed(:file => selfdir + 'music.json', :client => :vespa_feed_client, :port => 19020, :exitcode => true, :stderr => true)
+    (exitcode, output) = feed(:file => selfdir + 'music.json', :client => :vespa_feed_client, :exitcode => true, :stderr => true)
     assert_equal(0, exitcode.to_i)
 
     # Test that we fail decently when using unknown document type
-    (exitcode, output) = feed(:file => selfdir + 'unknown_doctype.json', :client => :vespa_feed_client, :port => 19020, :exitcode => true, :stderr => true)
+    (exitcode, output) = feed(:file => selfdir + 'unknown_doctype.json', :client => :vespa_feed_client, :exitcode => true, :stderr => true)
     assert_equal(0, exitcode.to_i) # Some docs fed successfully, so expect 0
     assert(output.index("Document type nonexistingtype does not exist"))
 
     # Test that we fail decently when having a document with a field that does not exist
-    (exitcode, output) = feed(:file => selfdir + 'wrongfield.json', :client => :vespa_feed_client, :port => 19020, :exitcode => true, :stderr => true)
+    (exitcode, output) = feed(:file => selfdir + 'wrongfield.json', :client => :vespa_feed_client, :exitcode => true, :stderr => true)
     assert_equal(0, exitcode.to_i) # Some docs fed successfully, so expect 0
     assert(output.index("No field 'wrong' in the structure of type 'music'"))
   end
