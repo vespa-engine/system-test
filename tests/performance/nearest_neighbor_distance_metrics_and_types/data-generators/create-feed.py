7269475e5acbd202743c2ac557e54cab4ce043a4
Jo Kristian Bergum <bergum@yahooinc.com>
2024-04-25 22:06:11 +0200

    new test with a larger feed file, avoiding encoding

---
+++ tests/performance/nearest_neighbor_distance_metrics_and_types/data-generators/create-feed.py
@@ -0,0 +1,55 @@
+
+from datasets import load_dataset
+import numpy as np
+import json
+
+
+def get_mrl_vector(embedding, dim, type=np.float32):
+  vector = np.array(embedding,dtype=np.float32)
+  vector = vector[0:dim]
+  vector = vector/np.linalg.norm(vector)
+  return vector
+
+
+def binary_quantize_vector(vector):
+  return np.packbits(np.where(vector > 0, 1, 0)).astype(np.int8)
+
+def scalar_quantize_vector(vector):
+  return np.round(vector*127).astype(np.int8)
+
+docs = load_dataset("Cohere/wikipedia-2023-11-embed-multilingual-v3", "nn", split="train")
+
+feed_docs = []
+print("[")
+i = 0
+for doc in docs:
+  doc_id = doc['_id']
+  i += 1
+  emb = doc['emb']
+  embeddings = dict()
+  for dims in [256,384,512,768,1024]:
+    mrl_vector = get_mrl_vector(emb, dims)
+    binary_vector = binary_quantize_vector(mrl_vector)
+    scalar_vector = scalar_quantize_vector(mrl_vector)
+    for distance in ['angular','euclidean','dotproduct', "prenormalized_angular"]:
+      embeddings[f'float_{dims}_{distance}'] = mrl_vector.tolist()
+    for distance in ['angular','euclidean','dotproduct']:
+        embeddings[f'scalar_int8_{dims}_{distance}'] = scalar_vector.tolist()
+    if dims > 384:
+        embeddings[f'binary_int8_{int(dims/8)}_hamming'] = binary_vector.tolist()
+  
+  json_doc = {
+     "id": "id:doc:vector::" + doc_id,
+     "fields": {
+        "id": doc_id,
+        **embeddings
+      }
+  }
+  json_line = json.dumps(json_doc)
+  if i < 500000:
+    print(json_line + ",")  
+  else:
+    print(json_line)
+    break
+print("]") 
+  
---
+++ tests/performance/nearest_neighbor_distance_metrics_and_types/nearest_neighbor_distance_metrics.rb
@@ -0,0 +1,81 @@
+require 'performance_test'
+require 'app_generator/search_app'
+require 'environment'
+
+
+class NearestNeighborDistanceMetricAndTypesPerfTest < PerformanceTest
+
+  def setup
+    set_owner("bergum")
+  end
+
+  def test_metrics_and_types
+    set_description('Benchmark distance metrics and types using 500K documents. Exact nearest neighbor search.')
+    deploy_app(SearchApp.new.sd(selfdir + 'vector.sd'))
+    start
+
+    feed_file = 'vectors.perf.json.zstd'
+    remote_file = "https://data.vespa.oath.cloud/tests/performance/#{feed_file}"
+    local_file =  dirs.tmpdir + feed_file
+    cmd = "wget -O'#{local_file}' '#{remote_file}'"
+    puts "Running command #{cmd}"
+    result = `#{cmd}`
+    puts "Result: #{result}"
+    run_feeder(local_file, [])
+
+
+    container = (vespa.qrserver["0"] or vespa.container.values.first)
+    runtime=20 # The runtime in seconds
+    test_container_query_directory = dirs.tmpdir + "qd"
+    for client in [1, 16] do
+      for dimension in [256, 384, 768, 1024] do
+        for metric in ["prenormalized_angular", "angular", "euclidean", "dotproduct"] do
+          query_file = selfdir + "queries/float-#{dimension}-#{metric}.txt"
+          container.copy(query_file, test_container_query_directory)
+          remote_file = test_container_query_directory + "/" + File.basename(query_file)
+          run_fbench(container, client, runtime, remote_file, "float-#{metric}-#{dimension}")
+        end
+      end
+    end
+
+    for client in [1,16] do
+      for dimension in [256, 384, 768, 1024] do
+        for metric in ["angular", "euclidean", "dotproduct"] do
+          query_file = selfdir + "queries/scalar-int8-#{dimension}-#{metric}.txt"
+          container.copy(query_file, test_container_query_directory)
+          remote_file = test_container_query_directory + "/" + File.basename(query_file)
+          run_fbench(container, client, runtime, remote_file, "scalar-int8-#{metric}-#{dimension}")
+        end
+      end
+    end
+
+    for client in [1,16] do
+      for dimension in [64, 96, 128] do
+        metric = "hamming"
+        query_file = selfdir + "queries/binary-int8-#{dimension}-#{metric}.txt"
+        container.copy(query_file, test_container_query_directory)
+        remote_file = test_container_query_directory + "/" + File.basename(query_file)
+        run_fbench(container, client, runtime, remote_file, "binary-int8-#{metric}-#{dimension}")
+      end
+    end
+  end
+
+  def run_fbench(qrserver, clients, runtime, queries, legend, post=true)
+    custom_fillers = [parameter_filler("legend", legend)]
+    system_fbench = Perf::System.new(qrserver)
+    system_fbench.start
+    fbench = Perf::Fbench.new(qrserver, qrserver.name, qrserver.http_port)
+    fbench.max_line_size = 100000
+    fbench.runtime = runtime
+    fbench.clients = clients
+    if post
+        fbench.use_post = true
+    end
+    profiler_start
+    fbench.query(queries)
+    system_fbench.end
+    profiler_report(legend)
+    fillers = [fbench.fill, system_fbench.fill]
+    write_report(fillers + custom_fillers)
+  end
+end
