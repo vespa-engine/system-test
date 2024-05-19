c82abfe46e17f129bd8cce8f3aa6c45349807b65
Harald Musum <musum@yahooinc.com>
2024-05-19 08:14:40 +0200

    Convert feed files to json, part 228

--- tests/performance/basic/dispatch/gendata.c
+++ tests/performance/basic/dispatch/gendata.c
@@ -1,40 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 void header(int num)
 {
-	printf("<document type='foobar' id='id:test:foobar::%d'>\n", num);
-        printf("<title>doc %d here</title>\n", num);
+	printf("{ \"put\": \"id:test:foobar::%d\",\n", num);
+        printf("  \"fields\": { \"title\": \"doc %d here\",\n", num);
 }
 
 void footer(int num)
 {
-	printf("<order>%d</order>\n", num);
-	printf("</document>\n");
+	printf("    \"order\": %d }\n}\n", num);
 }
 
 void matches(int num)
 {
 	int i;
-	printf("<foo>\n");
+        bool firstElement = true;
+	printf("    \"foo\": [");
 	for (i = 0; i < num; i++) {
                 if ((random() % 100) < 9) {
-			printf("<item>%d</item>\n", i);
+                        if (firstElement) {
+                                firstElement = false;
+                        } else {
+                                printf(",");
+                        }
+                        printf("%d", i);
+
                 }
 	}
-	printf("</foo>\n");
+	printf("],\n");
 }
 
 int main(int argc, char **argv)
 {
 	int i;
 	srandom(42);
-	printf("<vespafeed>\n");
+	printf("[\n");
 	for (i = 0; i < 12345; i++) {
 		header(i);
 		matches(10000);
 		footer(i);
+                if (i < 12344) {
+                        printf(",");
+                }
 	}
-	printf("</vespafeed>\n");
+	printf("]\n");
 	return 0;
 }
