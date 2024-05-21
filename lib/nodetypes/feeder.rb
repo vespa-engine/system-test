2bbc2d5eba32de9fff7d72dc005d194a230ba4ee
Harald Musum <musum@vespa.ai>
2024-05-21 13:49:25 +0200

    Revert "Revert "Reapply "Use client params and default_feed_client only"""

--- lib/nodetypes/feeder.rb
+++ lib/nodetypes/feeder.rb
@@ -157,8 +157,7 @@ module Feeder
       execute("sync")
     end
     if !params[:client]
-      # Set default feeder to 'vespa-feeder'
-      params[:client] = :vespa_feeder
+      params[:client] = testcase.default_feed_client
     end
     if !params[:stderr]
       params[:stderr] = :true
@@ -303,10 +302,12 @@ module Feeder
 
   private
   def select_feeder(params)
-    if params[:client] == :vespa_feeder
-      return "#{testcase.feeder_binary} "
-    elsif params[:client] == :vespa_feed_client
-      return "vespa-feed-client"
+    if params[:client] == :vespa_feed_client
+      return "vespa-feed-client "
+    elsif params[:client] == :vespa_feeder
+      return "vespa-feeder --abortondataerror no --abortonsenderror no"
+    elsif params[:client] == :vespa_feed_perf
+      return "vespa-feed-perf"
     else
       raise "Unsupported feed client '#{client}'"
     end
