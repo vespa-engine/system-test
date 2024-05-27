b7def2abe0485de7f6042f725912c73793793ac7
Tor Egge <Tor.Egge@online.no>
2024-05-27 13:27:54 +0200

    Reorder hits in second phase.

--- tests/search/first-phase-rank/first_phase_rank.rb
+++ tests/search/first-phase-rank/first_phase_rank.rb
@@ -16,14 +16,17 @@ class FirstPhaseRank < IndexedOnlySearchTest
     # All documents from first phase are reranked
     result = search({'query' => 'sddocname:test'})
     assert_equal(5, result.hitcount)
-    assert_equal([300.0, 250.0, 200.0, 150.0, 100.0], extract_features(result, 'matchfeatures', 'firstPhase'))
-    assert_equal([1.0, 2.0, 3.0, 4.0, 5.0], extract_features(result, 'matchfeatures', 'firstPhaseRank'))
+    assert_equal([1220.0, 360.0, 280.0, 165.0, 112.0], extract_scores(result))
+    assert_equal([200.0, 300.0, 250.0, 150.0, 100.0], extract_features(result, 'matchfeatures', 'firstPhase'))
+    assert_equal([1220.0, 360.0, 280.0, 165.0, 112.0], extract_features(result, 'matchfeatures', 'secondPhase'))
+    assert_equal([3.0, 1.0, 2.0, 4.0, 5.0], extract_features(result, 'matchfeatures', 'firstPhaseRank'))
     assert_equal([Float::MAX, Float::MAX, Float::MAX, Float::MAX, Float::MAX], extract_features(result, 'summaryfeatures', 'firstPhaseRank'))
+    assert_equal([200.0, 300.0, 250.0, 150.0, 100.0], extract_features(result, 'summaryfeatures', 'secondPhase'))
 
     # 3 documents from first phase are reranked
     result = search({'query' => 'sddocname:test', 'ranking.rerankCount' => 3})
     assert_equal(5, result.hitcount)
-    assert_equal([1.0, 2.0, 3.0, Float::MAX, Float::MAX], extract_features(result, 'matchfeatures', 'firstPhaseRank'))
+    assert_equal([3.0, 1.0, 2.0, Float::MAX, Float::MAX], extract_features(result, 'matchfeatures', 'firstPhaseRank'))
   end
 
   def extract_features(result, field, feature)
@@ -32,6 +35,14 @@ class FirstPhaseRank < IndexedOnlySearchTest
       features.push(h.field[field][feature])
     end
     features
+  end
+
+  def extract_scores(result)
+    scores = []
+    result.hit.each do |h|
+      scores.push(h.field['relevancy'])
+    end
+    scores
   end
 
   def teardown
