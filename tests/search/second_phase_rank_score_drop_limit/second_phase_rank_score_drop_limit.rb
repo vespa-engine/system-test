b1273e548399c514d008c056831b2f90e0a9807f
Tor Egge <Tor.Egge@online.no>
2024-06-05 16:59:40 +0200

    Trim down second phase rank score drop limit system test.

--- tests/search/second_phase_rank_score_drop_limit/second_phase_rank_score_drop_limit.rb
+++ tests/search/second_phase_rank_score_drop_limit/second_phase_rank_score_drop_limit.rb
@@ -13,41 +13,21 @@ class SecondPhaseRankScoreDropLimit < IndexedOnlySearchTest
     start
     feed_and_wait_for_docs('test', 7, :file => selfdir + 'docs.json')
 
-    query = {'yql' => 'select * from sources * where true'}
-    # All documents from first phase are reranked
-    result = search(query)
-    assert_equal([[10,1201.0],[5,1101.0],[16,1001.0],[15,901.0],[14,501.0],[11,14.0],[12,13.0]], extract_ids_and_scores(result))
+    query = {'yql' => 'select * from sources * where true',
+            'ranking.rerankCount' => 2}
 
     # 2 documents from first phase are reranked
-    result = search(query.merge({'ranking.rerankCount' => 2}))
+    result = search(query)
     assert_equal([[11,14.0],[12,13.0],[10,12.0],[5,11.0],[16,10.0],[15,9.0],[14,5.0]], extract_ids_and_scores(result))
 
     # 2 documents from first phase are reranked
-    # second phase rank score drop limit is 9.0
-    result = search(query.merge({'ranking.rerankCount' => 2, 'ranking' => 'second-phase-rank-score-drop-limit-9'}))
+    # Drop is among rescaled scores from first phase ranking
+    result = search(query.merge({'ranking' => 'second-phase-rank-score-drop-limit-9'}))
     assert_equal([[11,14.0],[12,13.0],[10,12.0],[5,11.0],[16,10.0]], extract_ids_and_scores(result))
 
-    # Track score of 10 best hits from first phase
-    # 2 documents from first phare reranked
-    # second phase rank score drop limit is 9.0
-    # 1 match thread
-    # Ask for 4 hits
-    result = search(query.merge({'ranking.keepRankCount' => 10, 'ranking.rerankCount' => 2, 'ranking' => 'second-phase-rank-score-drop-limit-9', 'ranking.matching.numThreadsPerSearch' => 1, 'hits' => 4}))
-    assert_equal([[11,14.0],[12,13.0],[10,12.0],[5,11.0]], extract_ids_and_scores(result))
-    assert_equal(5, result.hitcount)
-
-    # Track score of 4 best hits from first phase
-    # 2 documents from first phare reranked
-    # second phase rank score drop limit is 9.0
-    # 1 match thread
-    # Ask for 4 hits
-    result = search(query.merge({'ranking.keepRankCount' => 4, 'ranking.rerankCount' => 2, 'ranking' => 'second-phase-rank-score-drop-limit-9', 'ranking.matching.numThreadsPerSearch' => 1, 'hits' => 4}))
-    assert_equal([[11,14.0],[12,13.0],[10,12.0],[5,11.0]], extract_ids_and_scores(result))
-    assert_equal(4, result.hitcount)
-
     # 2 documents from first phase are reranked
-    # second phase rank score drop limit is 9.0
-    result = search(query.merge({'ranking.rerankCount' => 2, 'ranking' => 'second-phase-rank-score-drop-limit-13'}))
+    # Drop is among reranked hits in second phase
+    result = search(query.merge({'ranking' => 'second-phase-rank-score-drop-limit-13'}))
     assert_equal([[11,14.0]], extract_ids_and_scores(result))
 
 end
