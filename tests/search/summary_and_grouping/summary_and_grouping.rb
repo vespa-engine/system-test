6a8b956dd3897eb6753fef58129451d9156e7672
Henning Baldersheim <balder@yahoo-inc.com>
2024-06-19 12:43:52 +0000

    Very unhandled juniper summaries in grouping for indexed search.

--- tests/search/summary_and_grouping/summary_and_grouping.rb
+++ tests/search/summary_and_grouping/summary_and_grouping.rb
@@ -7,6 +7,10 @@ class SummaryAndGrouping < IndexedStreamingSearchTest
     set_owner("balder")
   end
 
+  def grouping(summary)
+    return "all(group(f3) max(1) each(output(count()) each(output(summary(#{summary})))))"
+  end
+
   def test_summary_both_flat_and_grouped
     deploy_app(SearchApp.new.sd(selfdir+'test.sd'))
     start
@@ -19,18 +23,43 @@ class SummaryAndGrouping < IndexedStreamingSearchTest
     result = search("query=sddocname:test")
     assert_equal(1, result.hit[0].field['f3'])
 
-    grouping = "all(group(f3) max(1) each(output(count()) each(output(summary(a)))))"
-    result = search("query=sddocname:test&select=#{grouping}&summary=b")
+    result = search("query=sddocname:test&select=#{grouping('a')}&summary=b")
     assert_equal(nil, result.hit[0].field['f1'])
     assert_equal('F2', result.hit[0].field['f2'])
     assert_equal(nil, result.groupings['group:root:0']['children'][0]['children'][0]['children'][0]['children'][0]['fields']['f2'])
     assert_equal('F1', result.groupings['group:root:0']['children'][0]['children'][0]['children'][0]['children'][0]['fields']['f1'])
 
-    result = search("query=sddocname:test&select=#{grouping}")
+    result = search("query=sddocname:test&select=#{grouping('a')}")
     assert_equal(nil, result.hit[0].field['f1'])
     assert_equal(1, result.hit[0].field['f3'])
     assert_equal(nil, result.groupings['group:root:0']['children'][0]['children'][0]['children'][0]['children'][0]['fields']['f3'])
     assert_equal('F1', result.groupings['group:root:0']['children'][0]['children'][0]['children'][0]['children'][0]['fields']['f1'])
+
+    body_exp = "<hi>Haskell</hi> is a programming language. The programming language is <hi>haskell</hi>."
+    result = search("query=body:haskell&select=#{grouping('a')}&summary=body_1")
+    puts "body_1: " + result.xmldata.to_s
+    assert_equal(body_exp, result.hit[0].field['body'])
+    assert_equal('F1', result.groupings['group:root:0']['children'][0]['children'][0]['children'][0]['children'][0]['fields']['f1'])
+
+    result = search("query=body:haskell&select=#{grouping('a')}&summary=body_2")
+    puts "body_2: " + result.xmldata.to_s
+    assert_equal(body_exp, result.hit[0].field['body'])
+    assert_equal(body_exp, result.hit[0].field['snippet'])
+    assert_equal('F1', result.groupings['group:root:0']['children'][0]['children'][0]['children'][0]['children'][0]['fields']['f1'])
+
+    result = search("query=body:haskell&select=#{grouping('body_1')}&summary=body_2")
+    puts "body_1 and body_2: " + result.xmldata.to_s
+    assert_equal(body_exp, result.hit[0].field['body'])
+    assert_equal(body_exp, result.hit[0].field['snippet'])
+    assert_equal(body_exp, result.groupings['group:root:0']['children'][0]['children'][0]['children'][0]['children'][0]['fields']['body'])
+    assert_equal(nil, result.groupings['group:root:0']['children'][0]['children'][0]['children'][0]['children'][0]['fields']['snippet'])
+
+    result = search("query=body:haskell&select=#{grouping('body_2')}&summary=body_1")
+    puts "body_2 and body_1: " + result.xmldata.to_s
+    assert_equal(body_exp, result.hit[0].field['body'])
+    assert_equal(nil, result.hit[0].field['snippet'])
+    assert_equal(body_exp, result.groupings['group:root:0']['children'][0]['children'][0]['children'][0]['children'][0]['fields']['body'])
+    assert_equal(body_exp, result.groupings['group:root:0']['children'][0]['children'][0]['children'][0]['children'][0]['fields']['snippet'])
   end
 
   def teardown
