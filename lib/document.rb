82e387c5d521ea5257b6402fd8f3be8fba05a5b8
Harald Musum <musum@yahooinc.com>
2024-06-23 12:12:11 +0200

    Remove xml support from document, document_set and documentupdate

--- lib/document.rb
+++ lib/document.rb
@@ -6,29 +6,6 @@ class Document
   attr_reader :documenttype
   attr_reader :fields
 
-  def self.create_from_xml(xml)
-    doc = Document.new(xml.attributes["documenttype"], xml.attributes["documentid"])
-
-    xml.elements.each { |elem|
-      if (elem.has_elements?)
-        val=[]
-
-        elem.each_element { |e|
-          if (e.attributes["weight"])
-            val.push([e.text, e.attributes["weight"]])
-          else
-            val.push(e.text)
-          end
-        }
-
-        doc.add_field(elem.name, val)
-      else
-        doc.add_field(elem.name, elem.text)
-      end
-    }
-    return doc
-  end
-
   def self.create_from_json(json, document_type)
     doc = Document.new(document_type, json["id"])
     json["fields"].each do |name, value|
@@ -65,76 +42,6 @@ class Document
       content = {operation.to_s => @documentid, "fields" => @fields}
     end
     JSON.dump(in_array ? [content] : content)
-  end
-
-  def xmlQuote(s, isAttr=false)
-    r = ""
-    s.each_char do |c|
-      if c == '&'
-        r << "&amp;"
-      elsif c == '<'
-        r << "&lt;"
-      elsif c == '>'
-        r << "&gt;"
-      elsif isAttr && c == '"'
-        r << "&quot;"
-      else
-        r << c
-      end
-    end
-    return r
-  end
-
-  # TODO: Remove when all tests using this class are migrated to json
-  def to_xml
-    ret = "<document documenttype=\"" + @documenttype +
-          "\" documentid=\"" + xmlQuote(@documentid, true) + "\">\n"
-
-    fields.sort.each { | key,value |
-      if value.class == Array
-        ret << "  <" + key + ">\n"
-        if value[0].class == Array
-	  value.sort.each { | v |
-            ret << "    <item weight=\"" + v[1].to_s + "\">" + xmlQuote(v[0].to_s) + "</item>\n"
-          }
-        else
-	  value.each { | v |
-            ret << "    <item>" + xmlQuote(v.to_s) + "</item>\n"
-          }
-        end
-        ret << "  </" + key + ">\n"
-      elsif value.class == Hash
-        ret << "  <" + key + ">\n"
-        value.sort.each { | k, v |
-          ret << "    <item><key>#{k}</key><value>#{v}</value></item>\n"
-        }
-        ret << "  </" + key + ">\n"
-      elsif value.class == String or value.is_a? Numeric
-        ret << "  <" + key + ">" + xmlQuote(value.to_s) + "</" + key + ">\n"
-      elsif value  # Struct
-        ret << "  <" + key + ">\n"
-        value.each_pair { | name, val |
-          ret << "    <#{name}>#{xmlQuote(val)}</#{name}>\n"
-        }
-        ret << "  </" + key + ">\n"
-      else # nil value, eg. <foo/>
-        ret << "  <" + key + "/>\n"
-      end
-    }
-    ret << "</document>"
-    ret
-  end
-
-  def write_xml(f)
-    f.write(to_xml)
-  end
-
-  def to_rm_xml
-    return "<remove documentid=\"" + xmlQuote(@documentid, true) + "\" />"
-  end
-
-  def write_rm_xml(f)
-    f.write(to_rm_xml)
   end
 
   def write_json(f, operation = :put)
--- lib/documentupdate.rb
+++ lib/documentupdate.rb
@@ -24,33 +24,6 @@ class Update
     "Update: " + @operation.inspect + " field:" + @fieldname.inspect
   end
 
-  def to_xml
-    res = "  <" + @operation + " field=\"" + @fieldname + "\">"
-    if @value_type != :array
-      res += @params.first.to_s + "</" + @operation + ">\n"
-    else
-      if @params.size == 0
-        res += "</" + @operation + ">\n"
-      else
-        res += "\n"
-        @params.each { | val |
-          if val.class == Array
-            if val.size == 2
-              # val[0] is value
-              # val[1] is weight
-              res += "    <item weight=\"" + val[1].to_s + "\">" + val[0].to_s + "</item>\n"
-            else
-              raise "ERROR: Bug in array values"
-            end
-          else
-            res += "    <item>" + val.to_s + "</item>\n"
-          end
-        }
-        res += "  </" + @operation + ">\n"
-      end
-    end
-  end
-
 end
 
 
@@ -67,17 +40,6 @@ class SimpleAlterUpdate
     @params = [number]
   end
 
-  def to_xml
-    res = "  <" + @operation + " field=\"" + @fieldname + "\" by=\"" + @number.to_s + "\""
-    if @key == nil
-      res += "/>\n"
-    else
-      res += ">\n"
-      res += "    <key>" + @key.to_s + "</key>\n"
-      res += "  </" + @operation + ">\n"
-    end
-    res
-  end
 end
 
 
@@ -118,23 +80,6 @@ class DocumentUpdate
   def addSimpleAlterOperation(operation, fieldname, number, key = nil)
     alter = SimpleAlterUpdate.new(operation, fieldname, number, key)
     @updateops.push(alter)
-  end
-
-  def to_xml
-    ret = "<update documenttype=\"" + @documenttype + "\" documentid=\"" + @documentid + "\">\n"
-
-    if @updateops.size() > 0
-      @updateops.each { |value|
-         ret += value.to_xml
-      }
-    end
-
-    ret += "</update>"
-    ret
-  end
-
-  def write_xml(f)
-    f.write(to_xml)
   end
 
   def fields_to_json
