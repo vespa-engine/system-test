32b96a5d3a21c58d613a61895a5bb64944892298
Tor Egge <Tor.Egge@online.no>
2024-07-05 13:59:50 +0200

    Test document frequency annotations passed to backend.

--- tests/search/queryfeatures/queryfeaturesdocfreq.rb
+++ tests/search/queryfeatures/queryfeaturesdocfreq.rb
@@ -1,7 +1,8 @@
+# coding: utf-8
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-require 'indexed_only_search_test'
+require 'indexed_streaming_search_test'
 
-class QueryFeaturesDocFreq < IndexedOnlySearchTest
+class QueryFeaturesDocFreq < IndexedStreamingSearchTest
 
   def setup
     set_owner("geirst")
@@ -13,7 +14,12 @@ class QueryFeaturesDocFreq < IndexedOnlySearchTest
     deploy_app(SearchApp.new.sd(selfdir+"docfreq.sd"))
     start
     feed_and_wait_for_docs("docfreq", 10, :file => selfdir + "docfreq.json")
+    run_test_term_with_docfreq_from_backend unless is_streaming
+    run_test_term_with_significance_from_query
+    run_test_term_with_docfreq_from_query
+  end
 
+  def run_test_term_with_docfreq_from_backend
     last = assert_greater_significance(0.0,  "a")
     last = assert_greater_significance(last, "b")
     last = assert_greater_significance(last, "c")
@@ -39,6 +45,36 @@ class QueryFeaturesDocFreq < IndexedOnlySearchTest
     puts "assert_greater_significance: #{act} > #{exp}"
     assert(act > exp)
     return act
+  end
+
+  def get_significance(annotation)
+    yql = "select * from sources * where default contains ({#{annotation}}\"a\")"
+    sf = search({'yql' => yql}).hit[0].field["summaryfeatures"]
+    sf.fetch("term(0).significance")
+  end
+
+  def get_df_significance(frequency, count)
+    get_significance("documentFrequency: { frequency: #{frequency}, count: #{count}}")
+  end
+
+  def run_test_term_with_significance_from_query
+    assert_approx(0.1, get_significance('significance: 0.1'))
+    assert_approx(0.5, get_significance('significance: 0.5'))
+    assert_approx(1.0, get_significance('significance: 1.0'))
+  end
+
+  def run_test_term_with_docfreq_from_query
+    eps = 1e-7
+    assert_approx(1.0, get_df_significance(1, 1000000), eps)
+    assert_approx(0.5, get_df_significance(1000000, 1000000), eps)
+    sig_10_of_100 = get_df_significance(10, 100)
+    sig_8_of_100 = get_df_significance(8, 100)
+    sig_10_of_110 = get_df_significance(10, 110)
+    assert(sig_10_of_100 < sig_8_of_100, "sig_10_of_100 < sig_8_of_100 failed")
+    assert(sig_10_of_100 < sig_10_of_110, "sig_10_of_100 < sig_10_of_110 failed")
+    # Scaling frequency and count doesn't change legacy significance
+    assert_approx(sig_10_of_100, get_df_significance(100, 1000), eps)
+    assert_approx(sig_8_of_100, get_df_significance(80, 1000), eps)
   end
 
   def teardown
