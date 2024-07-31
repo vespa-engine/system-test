31e376509e2a88ddb860c639c36f095457054f73
Harald Musum <musum@yahooinc.com>
2024-07-31 11:14:42 +0200

    Minor refactoring, no functional changes

--- tests/search/vespa_feed_client/feed_when_container_oom.rb
+++ tests/search/vespa_feed_client/feed_when_container_oom.rb
@@ -6,20 +6,21 @@ require 'search_test'
 class FeedWhenContainerOom < SearchTest
 
   def setup
-    @valgrind = false
     set_owner("hmusum")
     set_description("Tests feeding against a container with too little memory to handle feed")
-
-    @feed_file = dirs.tmpdir + "feed.json"
-    deploy_app(app())
     set_expected_logged(//, :slow_processing => true)
-    start
+
+    @valgrind = false
+    @feed_file = dirs.tmpdir + "feed.json"
   end
 
   def test_feeding_when_container_goes_oom
     doc_count = 5
     generate_feed(doc_count)
 
+    # Deploy app with container with too little memory to handle the 5 large documents => will OOM
+    deploy_app(app('1g'))
+    start
     feed(:file => @feed_file, :ignore_errors => true, :stderr => true, :verbose => true, :timeout => 30, :log_config => selfdir + 'logging.properties')
     result = search("sddocname:music")
     assert(result.hitcount < doc_count)
@@ -28,19 +29,18 @@ class FeedWhenContainerOom < SearchTest
     feed_thread= Thread.new(){
       feed(:file => @feed_file, :ignore_errors => true, :stderr => true, :verbose => true, :timeout => 150, :log_config => selfdir + 'logging.properties')
     }
-
     # Sleep to make sure feeder has started before deploy
-    sleep 10
-    # deploy with more container memory
+    sleep 5
+
+    # deploy app with more container memory, feed should succeed
     deploy_app(app('3g'))
     feed_thread.join
     assert_hitcount("query=sddocname:music", doc_count)
   end
 
-  def app(heap_mem = '1g')
+  def app(heap_mem)
     SearchApp.new.sd(VDS + '/schemas/music.sd').
                  container(Container.new.
-                             # Intentionally setup container to have too little memory to handle the 5 large documents => will OOM
                              jvmoptions("-Xms#{heap_mem} -Xmx#{heap_mem}").
                              search(Searching.new).
                              documentapi(ContainerDocumentApi.new))
