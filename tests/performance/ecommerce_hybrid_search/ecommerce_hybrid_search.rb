3f0612b2651aca9fce5173059f82d99e8fc49fdb
Geir Storli <geirst@vespa.ai>
2024-09-16 15:29:06 +0000

    Run more fbench cases with mixed workloads.

--- tests/performance/ecommerce_hybrid_search/ecommerce_hybrid_search.rb
+++ tests/performance/ecommerce_hybrid_search/ecommerce_hybrid_search.rb
@@ -20,11 +20,11 @@ class EcommerceHybridSearchTest < EcommerceHybridSearchTestBase
 
     @search_node.trigger_flush
     benchmark_queries("after_flush", false, [1, 2, 4, 8, 16, 32, 64])
-    benchmark_queries("after_flush", true, [1])
+    benchmark_queries("after_flush", true, [1, 16, 64])
 
     feed_thread = Thread.new { benchmark_feed(feed_file_name, "refeed") }
     sleep 5
-    benchmark_queries("during_refeed", false, [1])
+    benchmark_queries("during_refeed", false, [1, 16, 64])
     feed_thread.join
     benchmark_feed("vespa_update-1M.json.zst", "update")
   end
