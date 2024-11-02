3affac70785855b06b519ffcee71314e52d19502
Håvard Pettersen <havardpe@yahooinc.com>
2024-11-02 20:50:04 +0000

    more testing

--- tests/performance/wand_stopwords/MicroBmSearcher.java
+++ tests/performance/wand_stopwords/MicroBmSearcher.java
@@ -90,17 +90,29 @@ public class MicroBmSearcher extends Searcher {
         execution.fill(andResult);
         var andSet = getHitIds(andResult);
 
-        Query weakAndQuery20 = changeRoot(weakAndQuery, new WeakAndItem());
-        weakAndQuery20.properties().set("rankproperty.vespa.matching.weakand.stop_word_adjust_limit", "0.20");
-        Result weakAndResult20 = execution.search(weakAndQuery20);
-        execution.fill(weakAndResult20);
-        var weakAndSet20 = getHitIds(weakAndResult20);
+        Query weakAndQueryA20 = changeRoot(weakAndQuery, new WeakAndItem());
+        weakAndQueryA20.properties().set("rankproperty.vespa.matching.weakand.stop_word_adjust_limit", "0.20");
+        Result weakAndResultA20 = execution.search(weakAndQueryA20);
+        execution.fill(weakAndResultA20);
+        var weakAndSetA20 = getHitIds(weakAndResultA20);
 
-        Query weakAndQuery05 = changeRoot(weakAndQuery, new WeakAndItem());
-        weakAndQuery05.properties().set("rankproperty.vespa.matching.weakand.stop_word_adjust_limit", "0.05");
-        Result weakAndResult05 = execution.search(weakAndQuery05);
-        execution.fill(weakAndResult05);
-        var weakAndSet05 = getHitIds(weakAndResult05);
+        Query weakAndQueryA05 = changeRoot(weakAndQuery, new WeakAndItem());
+        weakAndQueryA05.properties().set("rankproperty.vespa.matching.weakand.stop_word_adjust_limit", "0.05");
+        Result weakAndResultA05 = execution.search(weakAndQueryA05);
+        execution.fill(weakAndResultA05);
+        var weakAndSetA05 = getHitIds(weakAndResultA05);
+
+        Query weakAndQueryS20 = changeRoot(weakAndQuery, new WeakAndItem());
+        weakAndQueryS20.properties().set("rankproperty.vespa.matching.weakand.stop_word_score_limit", "0.20");
+        Result weakAndResultS20 = execution.search(weakAndQueryS20);
+        execution.fill(weakAndResultS20);
+        var weakAndSetS20 = getHitIds(weakAndResultS20);
+
+        Query weakAndQueryS05 = changeRoot(weakAndQuery, new WeakAndItem());
+        weakAndQueryS05.properties().set("rankproperty.vespa.matching.weakand.stop_word_score_limit", "0.05");
+        Result weakAndResultS05 = execution.search(weakAndQueryS05);
+        execution.fill(weakAndResultS05);
+        var weakAndSetS05 = getHitIds(weakAndResultS05);
 
         Query weakAndQueryD20 = changeRoot(weakAndQuery, new WeakAndItem());
         weakAndQueryD20.properties().set("rankproperty.vespa.matching.weakand.stop_word_drop_limit", "0.20");
@@ -114,36 +126,55 @@ public class MicroBmSearcher extends Searcher {
         execution.fill(weakAndResultD05);
         var weakAndSetD05 = getHitIds(weakAndResultD05);
 
+        Query weakAndQueryX = changeRoot(weakAndQuery, new WeakAndItem());
+        weakAndQueryX.properties().set("rankproperty.vespa.matching.weakand.stop_word_adjust_limit", "0.02");
+        weakAndQueryX.properties().set("rankproperty.vespa.matching.weakand.stop_word_drop_limit", "0.20");
+        Result weakAndResultX = execution.search(weakAndQueryX);
+        execution.fill(weakAndResultX);
+        var weakAndSetX = getHitIds(weakAndResultX);
+
         // double orTime = timeQuery(orQuery, execution, 1);
         double weakAndTime = timeQuery(weakAndQuery, execution);
-        double weakAndTime20 = timeQuery(weakAndQuery20, execution);
-        double weakAndTime05 = timeQuery(weakAndQuery05, execution);
+        double weakAndTimeA20 = timeQuery(weakAndQueryA20, execution);
+        double weakAndTimeA05 = timeQuery(weakAndQueryA05, execution);
+        double weakAndTimeS20 = timeQuery(weakAndQueryS20, execution);
+        double weakAndTimeS05 = timeQuery(weakAndQueryS05, execution);
         double weakAndTimeD20 = timeQuery(weakAndQueryD20, execution);
         double weakAndTimeD05 = timeQuery(weakAndQueryD05, execution);
+        double weakAndTimeX = timeQuery(weakAndQueryX, execution);
         double andTime = timeQuery(andQuery, execution);
 
         Hit meta = new Hit("meta");
         meta.setMeta(true);
         meta.setField("andQuality", quality(orSet, andSet));
         meta.setField("weakAndQuality", quality(orSet, weakAndSet));
-        meta.setField("weakAndQuality20", quality(orSet, weakAndSet20));
-        meta.setField("weakAndQuality05", quality(orSet, weakAndSet05));
+        meta.setField("weakAndQualityA20", quality(orSet, weakAndSetA20));
+        meta.setField("weakAndQualityA05", quality(orSet, weakAndSetA05));
+        meta.setField("weakAndQualityS20", quality(orSet, weakAndSetS20));
+        meta.setField("weakAndQualityS05", quality(orSet, weakAndSetS05));
         meta.setField("weakAndQualityD20", quality(orSet, weakAndSetD20));
         meta.setField("weakAndQualityD05", quality(orSet, weakAndSetD05));
+        meta.setField("weakAndQualityX", quality(orSet, weakAndSetX));
         meta.setField("orHits", orResult.getTotalHitCount());
         meta.setField("andHits", andResult.getTotalHitCount());
         meta.setField("weakAndHits", weakAndResult.getTotalHitCount());
-        meta.setField("weakAndHits20", weakAndResult20.getTotalHitCount());
-        meta.setField("weakAndHits05", weakAndResult05.getTotalHitCount());
+        meta.setField("weakAndHitsA20", weakAndResultA20.getTotalHitCount());
+        meta.setField("weakAndHitsA05", weakAndResultA05.getTotalHitCount());
+        meta.setField("weakAndHitsS20", weakAndResultS20.getTotalHitCount());
+        meta.setField("weakAndHitsS05", weakAndResultS05.getTotalHitCount());
         meta.setField("weakAndHitsD20", weakAndResultD20.getTotalHitCount());
         meta.setField("weakAndHitsD05", weakAndResultD05.getTotalHitCount());
+        meta.setField("weakAndHitsX", weakAndResultX.getTotalHitCount());
         meta.setField("orTime", orTime);
         meta.setField("andTime", andTime);
         meta.setField("weakAndTime", weakAndTime);
-        meta.setField("weakAndTime20", weakAndTime20);
-        meta.setField("weakAndTime05", weakAndTime05);
+        meta.setField("weakAndTimeA20", weakAndTimeA20);
+        meta.setField("weakAndTimeA05", weakAndTimeA05);
+        meta.setField("weakAndTimeS20", weakAndTimeS20);
+        meta.setField("weakAndTimeS05", weakAndTimeS05);
         meta.setField("weakAndTimeD20", weakAndTimeD20);
         meta.setField("weakAndTimeD05", weakAndTimeD05);
+        meta.setField("weakAndTimeX", weakAndTimeX);
         Result result = new Result(weakAndQuery);
         result.setTotalHitCount(weakAndResult.getTotalHitCount());
         result.hits().add(meta);
