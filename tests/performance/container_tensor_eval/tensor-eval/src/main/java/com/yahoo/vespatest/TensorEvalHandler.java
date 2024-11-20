6c547ae2a5788912dd8e22924210c86cbf6c7941
Gleb Sizov <gleb@vespa.ai>
2024-11-20 12:59:38 +0000

    Implemented simple perf test with handler.

---
+++ tests/performance/container_tensor_eval/tensor-eval/src/main/java/com/yahoo/vespatest/TensorEvalHandler.java
@@ -0,0 +1,31 @@
+package com.yahoo.vespatest;
+
+import com.google.inject.Inject;
+import com.yahoo.container.jdisc.HttpRequest;
+import com.yahoo.container.jdisc.HttpResponse;
+import com.yahoo.container.jdisc.ThreadedHttpRequestHandler;
+import com.yahoo.jdisc.Metric;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.Executor;
+
+public class TensorEvalHandler extends ThreadedHttpRequestHandler {
+
+    @Inject
+    public TensorEvalHandler(Executor executor, Metric metric) {
+        super(executor, metric);
+    }
+
+    @Override
+    public HttpResponse handle(HttpRequest request) {
+        return new HttpResponse(200) {
+            @Override
+            public void render(OutputStream outputStream) {
+                PrintStream out = new PrintStream(outputStream);
+                out.print("OK");
+                out.flush();
+            }
+        };
+    }
+}
