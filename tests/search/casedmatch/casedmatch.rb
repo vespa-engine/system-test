525bd80ec6169f90503a190200d37cb58b3080e1
Jon Bratseth <bratseth@vespa.ai>
2025-03-24 20:03:15 +0100

    Add test for cased matching

--- tests/search/casedmatch/casedmatch.rb
+++ tests/search/casedmatch/casedmatch.rb
@@ -2,22 +2,18 @@
 
 require 'indexed_streaming_search_test'
 
-class ExactMatch < IndexedStreamingSearchTest
+class CasedMatch < IndexedStreamingSearchTest
 
   def setup
     set_owner("bratseth")
   end
 
-  def test_exact_match
+  def test_cased_match
     deploy_app(SearchApp.new.sd(selfdir+"casedmatch.sd"))
     start
 
     feed_and_wait_for_docs("casedmatch", 2, :file => selfdir+"feed.json")
 
-    exactmatch_common
-  end
-
-  def exactmatch_common
     assert_hitcount("query=field1:Foo", 2)
     assert_hitcount("query=field1:Bar", 1)
     assert_hitcount("query=field1:Baz", 0)
