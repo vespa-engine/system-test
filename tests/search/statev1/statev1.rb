7d04cac7641eb5202c1c85c980720bcf124ff189
Jan Böker <boeker@vespa.ai>
2025-04-24 10:08:41 +0000

    Clean up /state/v1/-test

--- tests/search/statev1/statev1.rb
+++ tests/search/statev1/statev1.rb
@@ -12,50 +12,40 @@ class StateV1 < IndexedStreamingSearchTest
     start
 
     # Note that we do not test all services since a VespaNode object is not created for every service
-    puts "Checking " + @vespa.services.length.to_s + " services"
+    puts "Testing #{@vespa.services.length.to_s} services"
 
-    # As of the creation of this test, there are 9 services that are tested
-    # To make sure that the test fails if the services list is empty (or fewer services than expected are added to it),
-    # we add the following assert
-    assert(@vespa.services.length >= 9, "The number of services that are tested should at least be 9")
+    # As a sanity check, make sure that there is at least one service being tested
+    assert(@vespa.services.length > 0, "There should be at least one service being tested")
 
     @vespa.services.each do |service|
-      puts "Testing service '" + service.servicetype + "'"
+      puts "Testing service '#{service.servicetype}'"
 
       unless service.get_state_port.nil?
-        # /state/v1/config
-        config = get_state_v1(service, "config")
+        puts "/state/v1/config"
+        config = service.get_state_v1("config")
         # service should be reporting a generation
         generation = config["config"]["generation"]
-        assert(!generation.nil?)
+        assert_not_nil generation
 
-        # /state/v1/version
-        version = get_state_v1(service, "version")
+        puts "/state/v1/version"
+        version = service.get_state_v1("version")
         # service should be reporting a version number
         version_number = version["version"]
-        assert(!version_number.nil?)
+        assert_not_nil version_number
 
-        # /state/v1/health
-        health = get_state_v1(service, "health")
+        puts "/state/v1/health"
+        health = service.get_state_v1("health")
         # service should be reporting itself as up
         assert_equal("up", health["status"]["code"])
 
-        # /state/v1/metrics
-        metrics = get_state_v1(service, "metrics")
+        puts "/state/v1/metrics"
+        metrics = service.get_state_v1("metrics")
         # service should be reporting itself as up
         assert_equal("up", metrics["status"]["code"])
       else
         puts "The service does not have a state port"
       end
     end
-  end
-
-  def get_state_v1(service, path)
-    puts "Getting state/v1/" + path
-    answer = nil
-    assert_nothing_raised() { answer = service.get_state_v1(path) }
-    assert(!answer.nil?, "Answer is nil")
-    answer
   end
 
   def teardown
