fbd419cfe6efcdb749a7d028e09160fbf8739cdd
Arne Juul <arnej@vespa.ai>
2025-06-18 09:08:53 +0000

    explore details about local LLMs
    - decrease number of threads
    - increase timeouts
    - print some information about the environment in which we are running

--- tests/performance/feeding_generate_local_llm/feeding_generate_local_llm.rb
+++ tests/performance/feeding_generate_local_llm/feeding_generate_local_llm.rb
@@ -8,19 +8,23 @@ class FeedingGenerateLocalLLMPerformanceTests < PerformanceTest
   end
 
   def timeout_seconds
-    1800
+    2400
   end
 
   def test_feeding_generate_local_llm
     set_description("Performance test for feeding with text generation using local LLM.")
-    
+
     deploy(selfdir + "app")
     puts "Starting application: this might take a while due to download of LLM..."
     start(900)
     puts "Application started."
 
+    node = (vespa.qrserver["0"] or vespa.container.values.first)
+    node.execute('tail -n 50 /proc/cpuinfo')
+    node.execute('printenv')
+
     feed_file = (selfdir + "data/feed.jsonl")
-    feeder_options = { :client => :vespa_feed_client }
+    feeder_options = { :client => :vespa_feed_client, :timeout => 600 }
     run_feeder(feed_file, [], feeder_options)
   end
 end
