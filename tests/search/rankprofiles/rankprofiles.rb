62f1b43b0605d63b778a46092bdf2becc3539972
Harald Musum <musum@vespa.ai>
2025-06-18 11:10:33 +0200

    Add rankprofiles tests
    Moved from internal repo

--- tests/search/rankprofiles/rankprofiles.rb
+++ tests/search/rankprofiles/rankprofiles.rb
@@ -6,10 +6,45 @@ require 'indexed_streaming_search_test'
 class RankProfiles < IndexedStreamingSearchTest
 
   def setup
-    set_owner("geirst")
+    set_owner("hmusum")
   end
 
-  def test_rankProfiles
+  def test_rankprofiles
+    set_description("Test use of different rank profiles")
+    deploy_app(SearchApp.new.sd(selfdir+"reload1/reload.sd"))
+    start
+    feed_and_wait_for_docs("reload", 1, :file => selfdir + "reload.json")
+    wait_for_hitcount("query=foo&streaming.selection=true", 1)
+
+    # Check that default rankprofile is 'default'
+    assert_relevancy("query=foo&streaming.selection=true", 10)
+    assert_relevancy("query=foo&ranking=default&streaming.selection=true", 10)
+
+    # Check another profile
+    assert_relevancy("query=foo&ranking=first&streaming.selection=true", 20)
+
+    # Check non-existing profile fails
+    assert_hitcount("query=foo&ranking=not&streaming.selection=true", 0)
+  end
+
+  def test_reload_config
+    set_description("Test reload of rank-profiles config")
+    deploy_app(SearchApp.new.sd(selfdir+"reload1/reload.sd"))
+    start
+    #qrserver().logctl("container:com.yahoo.container.di", "debug=on")
+    #qrserver().logctl("configproxy:com.yahoo.vespa.config.proxy.ClientUpdater", "debug=on")
+    #qrserver().logctl("configproxy:com.yahoo.config.subscription.impl.JRTConfigRequester", "debug=on")
+    feed_and_wait_for_docs("reload", 1, :file => selfdir + "reload.json")
+    run_reload_config_test_first
+    output = deploy_app(SearchApp.new.sd(selfdir+"reload2/reload.sd"))
+    wait_for_application(vespa.container.values.first, output)
+    run_reload_config_test_second
+    output = deploy_app(SearchApp.new.sd(selfdir+"reload1/reload.sd"))
+    wait_for_application(vespa.container.values.first, output)
+    run_reload_config_test_first
+  end
+
+  def test_rank_profiles_extended
     deploy_files = { }
     for file in [ 'schemas/type1/first.profile',
                   'schemas/type1/subdir/default.profile',
@@ -56,7 +91,26 @@ class RankProfiles < IndexedStreamingSearchTest
 
     assert_expression(sf5, query4, 0)
     assert_expression(sf5, query5, 0)
+  end
 
+  def run_reload_config_test_first
+    wait_for_hitcount("query=sddocname:reload&streaming.selection=true", 1)
+    query = "query=foo&streaming.selection=true&nocache&ranking="
+    wait_for_relevancy(query + "default", 10)
+    wait_for_relevancy(query + "first",   20)
+    assert_hitcount(query + "second",  0) # 'second' does not exist, should fail
+  end
+
+  def run_reload_config_test_second
+    wait_for_hitcount("query=sddocname:reload&streaming.selection=true", 1)
+    query = "query=foo&streaming.selection=true&nocache&ranking="
+    wait_for_relevancy(query + "default", 11)
+    wait_for_relevancy(query + "first",   21)
+    wait_for_relevancy(query + "second",  30)
+  end
+
+  def qrserver
+    vespa.container.values.first || vespa.qrservers.values.first
   end
 
   def assert_expression(exp, query, docid)
@@ -66,4 +120,5 @@ class RankProfiles < IndexedStreamingSearchTest
   def teardown
     stop
   end
+
 end
