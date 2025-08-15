3efecb57dec2369d3970a2568c12f18353e9aad2
Johan Solbakken <johsol@vespa.ai>
2025-08-15 12:01:04 +0000

    Make logical predicates infix

--- tests/search/grouping_adv/grouping_base.rb
+++ tests/search/grouping_adv/grouping_base.rb
@@ -289,9 +289,9 @@ module GroupingBase
   end
 
   def querytest_filter
-    check_query('all(group(a) filter(not(regex("^a1$", a))) each(output(count())))', 'predicate-1')
-    check_query('all(group(a) filter(or(regex("^a1$", a),regex("^a2$", a))) each(output(count())))', 'predicate-2')
-    check_query('all(group(a) filter(and(regex("^a*$", s),regex("^b1$", b))) each(output(count())))', 'predicate-3')
+    check_query('all(group(a) filter(not regex("^a1$", a)) each(output(count())))', 'predicate-1')
+    check_query('all(group(a) filter(regex("^a1$", a) or regex("^a2$", a)) each(output(count())))', 'predicate-2')
+    check_query('all(group(a) filter(regex("^a*$", s) and regex("^b1$", b)) each(output(count())))', 'predicate-3')
 
     check_query('all(group(a) filter(regex("^a1$", a)) each(output(count())))', 'filter-1')
     check_query('all(group(a) filter(regex("^b1$", b)) each(output(count())))', 'filter-2')
