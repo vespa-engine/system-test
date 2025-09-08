5b01d08347f421f73e5a9faf210ce4ed17367257
Jan Böker <boeker@vespa.ai>
2025-09-08 11:24:33 +0000

    Make initialization-status test more general

--- tests/search/statev1/initialization.rb
+++ tests/search/statev1/initialization.rb
@@ -11,16 +11,16 @@ class Initialization < IndexedOnlySearchTest
     deploy_app(SearchApp.new.sd(selfdir + "initialization.sd"))
     start
 
+    dbs = { "initialization" => ["int_field1", "string_field1", "string_field3", "tensor_field"] }
+
     searchnode_array = @vespa.services.select {|service| service.servicetype == "searchnode"}
     assert_equal(1, searchnode_array.length)
     searchnode = searchnode_array[0]
 
-    puts "/state/v1/initialization"
-
     # Verify that initialization status is as expected
     initialization = searchnode.get_state_v1("initialization")
     puts JSON.pretty_generate(initialization)
-    assert_initialization_finished(initialization)
+    assert_loaded(initialization, dbs)
 
     # Feed a document
     doc = Document.new("id:initialization:initialization::0")
@@ -40,7 +40,7 @@ class Initialization < IndexedOnlySearchTest
     # Verify that initialization status is as expected
     initialization = searchnode.get_state_v1("initialization")
     puts JSON.pretty_generate(initialization)
-    assert_initialization_finished(initialization)
+    assert_loaded(initialization, dbs)
 
     # Flush and restart Vespa
     vespa.search["search"].first.trigger_flush
@@ -52,7 +52,7 @@ class Initialization < IndexedOnlySearchTest
     # Verify that initialization status is as expected
     initialization = searchnode.get_state_v1("initialization")
     puts JSON.pretty_generate(initialization)
-    assert_initialization_finished(initialization)
+    assert_loaded(initialization, dbs)
   end
 
   def restart_vespa
@@ -69,93 +69,137 @@ class Initialization < IndexedOnlySearchTest
     puts "# System is up"
   end
 
-  def assert_initialization_finished(initialization)
-    # overall state
+  def assert_loaded(initialization, expected_dbs)
+    # Overall state
     assert_not_nil(initialization["state"])
     assert_equal("ready", initialization["state"])
 
-    # time
-    assert_not_nil(initialization["start_time"])
-    start_time = initialization["start_time"].to_f
-    assert(start_time > 0)
+    # Timestamps
+    start_time = initialization["start_time"]
+    assert_not_nil start_time
+    assert start_time.to_f > 0
 
-    assert_not_nil(initialization["end_time"])
-    end_time = initialization["end_time"].to_f
-    assert(end_time >= start_time)
+    end_time = initialization["end_time"]
+    assert_not_nil end_time
+    assert end_time.to_f >= start_time.to_f
 
-    assert_not_nil(initialization["current_time"])
-    current_time = initialization["current_time"].to_f
-    assert(current_time >= end_time)
+    current_time = initialization["current_time"]
+    assert_not_nil current_time
+    assert current_time.to_f >= end_time.to_f
 
-    # db
-    assert_not_nil(initialization["dbs"][0]["state"])
-    assert_equal("ready", initialization["dbs"][0]["state"])
-    assert_not_nil(initialization["dbs"][0]["name"])
-    assert_equal("initialization", initialization["dbs"][0]["name"])
+    dbs = initialization["dbs"]
+    assert_not_nil dbs
+    assert_equal(dbs.length, expected_dbs.length)
 
-    # replay_progress for db
-    assert_not_nil(initialization["dbs"][0]["replay_progress"])
-    replay_progress = initialization["dbs"][0]["replay_progress"].to_f
-    assert((replay_progress - 1.0).abs < 0.001)
-
-    # time for db
-    assert_not_nil(initialization["dbs"][0]["start_time"])
-    start_time = initialization["dbs"][0]["start_time"].to_f
-    assert(start_time > 0)
-
-    assert_not_nil(initialization["dbs"][0]["replay_start_time"])
-    replay_start_time = initialization["dbs"][0]["replay_start_time"].to_f
-    assert(replay_start_time >= start_time)
-
-    assert_not_nil(initialization["dbs"][0]["replay_end_time"])
-    replay_end_time = initialization["dbs"][0]["replay_end_time"].to_f
-    assert(replay_end_time >= replay_start_time)
-
-    assert_not_nil(initialization["dbs"][0]["end_time"])
-    end_time = initialization["dbs"][0]["end_time"].to_f
-    assert(end_time >= replay_end_time)
-    assert(current_time >= end_time)
-
-    # attributes
-    loaded_attributes = initialization["dbs"][0]["ready_subdb"]["loaded_attributes"]
-    assert_equal(4, loaded_attributes.length)
-
-    # check that all attributes occur
-    assert_loaded_attribute(loaded_attributes, "int_field1")
-    assert_loaded_attribute(loaded_attributes, "string_field1")
-    assert_loaded_attribute(loaded_attributes, "string_field3")
-    assert_loaded_attribute(loaded_attributes, "tensor_field")
-
-    # since we made sure that there only 4 attributes,
-    # we implicitly checked that string_field2 does not occur
-    # (field string_field2 is not an attribute)
-
-    loading_attributes = initialization["dbs"][0]["ready_subdb"]["loading_attributes"]
-    assert_equal(0, loading_attributes.length)
-
-    queued_attributes = initialization["dbs"][0]["ready_subdb"]["queued_attributes"]
-    assert_equal(0, queued_attributes.length)
+    expected_dbs.each do |expected_name, expected_attribute_names|
+      assert_db_loaded(dbs, current_time, expected_name, expected_attribute_names)
+    end
   end
 
-  def assert_loaded_attribute(loaded_attributes, name)
-    loaded_attributes.each do |attribute|
-      assert_not_nil attribute["name"]
-      if attribute["name"] == name
-        assert_not_nil attribute["status"]
-        status = attribute["status"]
-        assert_equal(status, "loaded")
-
-        loading_started = attribute["loading_started"]
-        assert_not_nil loading_started
-
-        loading_finished = attribute["loading_finished"]
-        assert_not_nil loading_finished
-
-        assert loading_finished.to_f >= loading_started.to_f
+  def assert_db_loaded(dbs, current_time, expected_name, expected_attribute_names)
+    # Get status of db "name"
+    name_db = nil
+    dbs.each do |db|
+      assert_not_nil db["name"]
+      if db["name"] == expected_name
+        name_db = db
       end
     end
 
-    false
+    assert_not_nil name_db
+    assert_specific_db_loaded(name_db, current_time, expected_name, expected_attribute_names)
+  end
+
+  def assert_specific_db_loaded(db, current_time, expected_name, expected_attribute_names)
+    # Name
+    db_name = db["name"]
+    assert_not_nil db_name
+    assert_equal(db_name, expected_name)
+
+    # State
+    db_state = db["state"]
+    assert_not_nil db_state
+    assert_equal(db_state, "ready")
+
+    # Replay progress
+    db_replay_progress = db["replay_progress"]
+    assert_not_nil db_replay_progress
+    assert((db_replay_progress.to_f - 1.0).abs < 0.001)
+
+    # Timestamps
+    db_start_time = db["start_time"]
+    assert_not_nil db_start_time
+    assert(db_start_time.to_f > 0)
+
+    db_replay_start_time = db["replay_start_time"]
+    assert_not_nil db_replay_start_time
+    assert(db_replay_start_time.to_f >= db_start_time.to_f)
+
+    db_replay_end_time = db["replay_end_time"]
+    assert_not_nil db_replay_end_time
+    assert(db_replay_end_time.to_f >= db_replay_start_time.to_f)
+
+    db_end_time = db["end_time"]
+    assert_not_nil db_end_time
+    assert(db_end_time.to_f >= db_replay_end_time.to_f)
+    assert(current_time.to_f >= db_end_time.to_f)
+
+    # Attributes
+    db_loaded_attributes = db["ready_subdb"]["loaded_attributes"]
+
+    # Check that the number of attributes is corrected
+    assert_equal(db_loaded_attributes.length, expected_attribute_names.length)
+
+    # Check that all attributes actually occur
+    expected_attribute_names.each do |attribute_name|
+      assert_attribute_loaded(db_loaded_attributes, current_time, attribute_name)
+    end
+
+    db_loading_attributes = db["ready_subdb"]["loading_attributes"]
+    assert_equal(db_loading_attributes.length, 0)
+
+    db_queued_attributes = db["ready_subdb"]["queued_attributes"]
+    assert_equal(db_queued_attributes.length, 0)
+  end
+
+  def assert_attribute_loaded(loaded_attributes, current_time, expected_name)
+    name_attribute = nil
+    loaded_attributes.each do |attribute|
+      assert_not_nil attribute["name"]
+      if attribute["name"] == expected_name
+        name_attribute = attribute
+      end
+    end
+
+    assert_not_nil name_attribute
+    assert_specific_attribute_loaded(name_attribute, current_time, expected_name)
+  end
+
+  def assert_specific_attribute_loaded(attribute, current_time, expected_name)
+    # Name
+    attribute_name = attribute["name"]
+    assert_equal(attribute_name, expected_name)
+
+    # Status
+    attribute_status = attribute["status"]
+    assert_not_nil attribute_status
+    assert_equal(attribute_status, "loaded")
+
+    # Timestamps
+    attribute_loading_started = attribute["loading_started"]
+    assert_not_nil attribute_loading_started
+    assert(attribute_loading_started.to_f > 0)
+
+    attribute_loading_finished = attribute["loading_finished"]
+    assert_not_nil attribute_loading_finished
+    assert attribute_loading_finished.to_f >= attribute_loading_started.to_f
+    assert current_time.to_f >= attribute_loading_finished.to_f
+  end
+
+  def get_searchnode
+    searchnode_array = @vespa.services.select {|service| service.servicetype == "searchnode"}
+    assert_equal(1, searchnode_array.length)
+    searchnode_array[0]
   end
 
   def teardown
