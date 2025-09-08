c85470e8f5994b891efb2751bcba4060d0d4ee28
Arne Juul <arnej@vespa.ai>
2025-09-08 06:39:57 +0000

    wip wip

--- tests/search/onnx_models/app/files/multiply_add.py
+++ tests/search/onnx_models/app/files/multiply_add.py
@@ -6,19 +6,22 @@ from onnx.helper import (
     make_tensor_value_info)
 from onnx.checker import check_model
 
-X = make_tensor_value_info('model_input_1', TensorProto.DOUBLE, [2, 2])
-A = make_tensor_value_info('model_input_2', TensorProto.DOUBLE, [2])
-B = make_tensor_value_info('model_input_3', TensorProto.DOUBLE, [2])
-Y = make_tensor_value_info('model_output_1', TensorProto.DOUBLE, [2])
+X = make_tensor_value_info('model_input_1', TensorProto.FLOAT, ['cat', 2])
+A = make_tensor_value_info('model_input_2', TensorProto.FLOAT, [2])
+B = make_tensor_value_info('model_input_3', TensorProto.FLOAT, ['cat'])
+Y = make_tensor_value_info('model_output_1', TensorProto.FLOAT, ['cat'])
+Z = make_tensor_value_info('model_output_2', TensorProto.FLOAT, ['cat'])
 
 node1 = make_node('MatMul', ['model_input_1', 'model_input_2'], ['XA'])
 node2 = make_node('Add', ['XA', 'model_input_3'], ['model_output_1'])
+node3 = make_node('MatMul', ['model_input_3', 'model_input_1'], ['model_output_2'])
 
-graph = make_graph([node1, node2], 'multiply_add', [X, A, B], [Y])
+graph = make_graph([node1, node2, node3], 'multiply_add', [X, A, B], [Y, Z])
 
 onnx_model = make_model(graph)
 
 # Generate older version
+onnx_model.ir_version = 10
 del onnx_model.opset_import[:]
 opset = onnx_model.opset_import.add()
 opset.version = 14
