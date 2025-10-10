1ed4b3b42ee815e2a0f7ad73bb0297b13753a40b
Edvard Dingsør <edvard.wdingsor@gmail.com>
2025-10-10 12:58:57 +0200

    feat(mcp): working tests for main functionality

--- tests/container/mcp_server/bundle/src/main/java/ai/vespa/mcp/package-info.java
+++
@@ -1,5 +0,0 @@
-@ExportPackage(version = @Version(major = 1, minor = 0, micro = 0))
-package ai.vespa.mcp;
-
-import com.yahoo.osgi.annotation.ExportPackage;
-import com.yahoo.osgi.annotation.Version;
--- tests/container/mcp_server/mcp_server.rb
+++ tests/container/mcp_server/mcp_server.rb
@@ -17,7 +17,25 @@ class McpServerTest < SearchContainerTest
     @container = vespa.container.values.first
   end
 
-  def test_initalize_request
+  def test_all
+    # Run all tests in sequence
+    print_test_header("INITIALIZE REQUEST")
+    initalize_request_test()
+
+    print_test_header("LIST TOOLS")
+    list_tools_test()
+    
+    print_test_header("GET SCHEMAS")
+    get_schemas_test()
+    
+    print_test_header("EXECUTE QUERY")
+    execute_query_test()
+    
+    print_test_header("GET DOCUMENTATION")
+    get_documentation_test()
+  end
+
+  def initalize_request_test
     # Simple JSON-RPC request to test MCP endpoint
     jsonrpc_request = {
       "jsonrpc" => "2.0",
@@ -41,63 +59,105 @@ class McpServerTest < SearchContainerTest
     assert_equal(1, parsed_response["id"])
   end
 
-  # def test_list_tools
-  #   jsonrpc_request = {
-  #       "jsonrpc" => "2.0",
-  #       "id" => "2",
-  #       "method" => "tools/list",
-  #       "params" => {}
-  #   }
-  #   # Checks if the MCP server lists the "getDocumentation" tool
-  #   response = send_jsonrpc_request(jsonrpc_request)
-  #   assert_not_nil(response)
-  #   parsed_response = JSON.parse(response)
-  #   assert(parsed_response["result"].key?("tools"))
-  #   assert(parsed_response["result"]["tools"].any? { |tool| tool["name"] == "getDocumentation" })
-  #
-  # end
-  #
-  #
-  # def test_get_schemas
-  #   jsonrpc_request = {
-  #     "jsonrpc" => "2.0",
-  #     "id" => 3,
-  #     "method" => "tools/call",
-  #     "params" => {
-  #       "name" => "getSchemas",
-  #       "arguments" => {}
-  #     }
-  #   }
-  #   # Checks if the MCP server finds the "person" schema
-  #   response = send_jsonrpc_request(jsonrpc_request)
-  #   assert_not_nil(response)
-  #   parsed_response = JSON.parse(response)
-  #   assert(parsed_response["result"].key?("content"))
-  #   assert(parsed_response["result"]["content"].key?("text"))
-  #   assert(parsed_response["result"]["content"]["text"].include?("person"))
-  # end
-  #
-  # def test_simple_query
-  #   jsonrpc_request = {
-  #     "jsonrpc" => "2.0",
-  #     "id" => 4,
-  #     "method" => "tools/call",
-  #     "params" => {
-  #       "name" => "executeQuery",
-  #       "arguments" => {
-  #         "parameters" => {
-  #           "sources" => "person"
-  #         },
-  #         "yql" => "select * from sources where true"
-  #       }
-  #     }
-  #   }
-  #
-  #   response = send_jsonrpc_request(jsonrpc_request)
-  #   assert_not_nil(response)
-  #   parsed_response = JSON.parse(response)
-  #   assert_equal(false, parsed_response["result"]["isError"])
-  # end
+  def list_tools_test
+    jsonrpc_request = {
+        "jsonrpc" => "2.0",
+        "id" => "2",
+        "method" => "tools/list",
+        "params" => {}
+    }
+    # Checks if the MCP server lists the "getDocumentation" tool
+    response = send_jsonrpc_request(jsonrpc_request)
+    assert_not_nil(response)
+    parsed_response = JSON.parse(response)
+    assert(parsed_response["result"].key?("tools"))
+    assert(parsed_response["result"]["tools"].any? { |tool| tool["name"] == "getDocumentation" })
+  
+  end
+  
+  
+  def get_schemas_test
+    jsonrpc_request = {
+      "jsonrpc" => "2.0",
+      "id" => 3,
+      "method" => "tools/call",
+      "params" => {
+        "name" => "getSchemas",
+        "arguments" => {}
+      }
+    }
+    # Checks if the MCP server finds the "person" schema
+    response = send_jsonrpc_request(jsonrpc_request)
+    assert_not_nil(response)
+    parsed_response = JSON.parse(response)
+    assert(parsed_response["result"].key?("content"))
+    assert(parsed_response["result"]["content"].is_a?(Array))
+    assert(parsed_response["result"]["content"][0]["text"].include?("person"))
+  end
+  
+  def execute_query_test
+    jsonrpc_request = {
+      "jsonrpc" => "2.0",
+      "id" => 4,
+      "method" => "tools/call",
+      "params" => {
+        "name" => "executeQuery",
+        "arguments" => {
+          "parameters" => {
+            "sources" => "person"
+          },
+          "yql" => "select * from sources where true"
+        }
+      }
+    }
+    # Checks if the execute query tool call doesn't return an error
+    response = send_jsonrpc_request(jsonrpc_request)
+    
+    assert_not_nil(response)
+    parsed_response = JSON.parse(response)
+    assert_equal(false, parsed_response["result"]["isError"])
+    assert(parsed_response["result"].key?("content"))
+    assert(parsed_response["result"]["content"].is_a?(Array))
+    parsed_content = JSON.parse(parsed_response["result"]["content"][0]["text"])
+
+    # Check that root exists and children is empty (no documents fed yet)
+    assert(parsed_content.key?("root"))
+    assert_equal(0, parsed_content["root"]["fields"]["totalCount"])
+
+    # Feed document and re-run the query to check if we get results
+    feed_and_wait_for_docs("person", 1, :file => "#{selfdir}/docs.json", :maxpending => 1)
+    jsonrpc_request["id"] = 5
+
+    response = send_jsonrpc_request(jsonrpc_request)
+    assert_not_nil(response)
+    parsed_response = JSON.parse(response)
+    assert_equal(false, parsed_response["result"]["isError"]) 
+    assert(parsed_response["result"].key?("content"))
+    assert(parsed_response["result"]["content"].is_a?(Array))
+    assert_not_nil(parsed_response["result"]["content"][0]["text"].include?("John Doe"))
+  end
+
+  def get_documentation_test
+    jsonrpc_request = {
+      "jsonrpc" => "2.0",
+      "id" => 6,
+      "method" => "tools/call",
+      "params" => {
+        "name" => "getDocumentation",
+        "arguments" => {
+          "userinput" => "Vespa CLI"
+        }
+      }
+    }
+    # Checks if the get documentation tool call returns some content
+    response = send_jsonrpc_request(jsonrpc_request)
+    assert_not_nil(response)
+    parsed_response = JSON.parse(response)
+    assert_equal(false, parsed_response["result"]["isError"])
+    assert(parsed_response["result"].key?("content"))
+    assert(parsed_response["result"]["content"].is_a?(Array))
+    assert_not_nil(parsed_response["result"]["content"][0]["text"])
+  end
 
   def send_jsonrpc_request(request_body)
         # Helper function to send a JSON-RPC request to the MCP endpoint
@@ -107,18 +167,16 @@ class McpServerTest < SearchContainerTest
         }
         response = vespa.container.values.first.http_get("localhost", 0, "/mcp/", request_body.to_json, header)
 
-        # uri = URI("http://#{@container.hostname}:#{@container.http_port}/mcp/")
-        # puts "Sending JSON-RPC request to #{uri}"
-        # http = Net::HTTP.new(uri.host, uri.port)
-        #
-        # request = Net::HTTP::Post.new(uri)
-        # request['Content-Type'] = 'application/json'
-        # request['Accept'] = 'application/json'
-        # request.body = request_body.to_json
-        #
-        # response = http.request(request)
-        # response.body
         response.body
+  end
+
+  def print_test_header(test_name)
+    width = 60
+    text = "Running the following test: " + test_name
+    puts "#" * width
+    puts "# " +  text.center(width - 4) + " #"
+    puts "#" * width
+    print "\n"
   end
 
   def teardown
