0e900606306f57b34ab5a3d75f50caa903590ef0
Arne Juul <arnej@vespa.ai>
2026-02-12 08:54:21 +0000

    search.num0 -> search

--- tests/search/searchtimeout/searchtimeout.rb
+++ tests/search/searchtimeout/searchtimeout.rb
@@ -16,10 +16,11 @@ class SearchTimeoutTest < IndexedOnlySearchTest
     assert_result_with_timeout(40.0, "query=sddocname:banana&hits=1&nocache", selfdir + "result.1.json")
     # TODO This will start failing once timeout are best effort.
     assert_query_errors_without_timeout("query=sddocname:banana&hits=1&nocache&timeout=1.0&ranking.softtimeout.enable=false",
-                                    ["Timeout while waiting for search.num0|Query timed out in search.num0"])
+                                    ["Timeout while waiting for search|Query timed out in search"])
     assert_hitcount_withouttimeout("query=sddocname:banana&hits=1&nocache&timeout=40.0", 20)
     assert_hitcount_withouttimeout("query=sddocname:banana&hits=1&nocache&timeout=5.0&ranking.softtimeout.enable=true", 3)
     assert_hitcount_withouttimeout("query=sddocname:banana&hits=1&nocache&timeout=5.0&ranking.softtimeout.enable=true&ranking.softtimeout.factor=0.70", 4)
+    puts("Expecting 3 hits, checking 40 times")
     for i in 0..40 do
         result = search_base("query=sddocname:banana&hits=1&nocache&timeout=5.0&ranking.softtimeout.enable=true")
         puts "Query " + i.to_s + " has " + result.hitcount.to_s + " hits."
