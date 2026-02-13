54b8def64ef366279e09b4721b47ba5598e0de2e
Jan Böker <boeker@vespa.ai>
2026-02-13 10:50:33 +0000

    Add test case for elementFilter annotation with multiple ids

--- tests/search/same_element_operator/array_indexing.rb
+++ tests/search/same_element_operator/array_indexing.rb
@@ -44,6 +44,63 @@ class ElementFilterAnnotation < IndexedStreamingSearchTest
     #end
   end
 
+
+  def test_element_filter_annotation_when_empty
+    set_description('Use elementFilter annotation of sameElement operator for array indexing using multiple indices.')
+    deploy_app(SearchApp.new.sd(selfdir+'arrays.sd'))
+    start
+
+    feed_docs
+
+    # An empty list means "no filtering" and behaves as if no annotation is present.
+    assert_docs("string_array contains sameElement(\"baz\")", [0, 4])
+    assert_docs("string_array contains ({elementFilter:[]}sameElement(\"baz\"))", [0, 4])
+
+    ["byte_array", "int_array", "long_array"].each do |array_name|
+      assert_docs("#{array_name} contains sameElement(\"9\")", [2, 3, 4])
+      assert_docs("#{array_name} contains ({elementFilter:[]}sameElement(\"9\"))", [2, 3, 4])
+    end
+
+  end
+
+
+  def test_element_filter_annotation_with_multiple_ids
+    set_description('Use elementFilter annotation of sameElement operator for array indexing using multiple indices.')
+    deploy_app(SearchApp.new.sd(selfdir+'arrays.sd'))
+    start
+
+    feed_docs
+
+    assert_docs("string_array contains ({elementFilter:[0]}sameElement(\"baz\"))", [4])
+    assert_docs("string_array contains ({elementFilter:[0,1]}sameElement(\"baz\"))", [4])
+    assert_docs("string_array contains ({elementFilter:[0,1,2]}sameElement(\"baz\"))", [0, 4])
+    assert_docs("string_array contains ({elementFilter:[0,1,2,3]}sameElement(\"baz\"))", [0, 4])
+    assert_docs("string_array contains ({elementFilter:[1,2,3]}sameElement(\"baz\"))", [0, 4])
+    assert_docs("string_array contains ({elementFilter:[2,3]}sameElement(\"baz\"))", [0, 4])
+    assert_docs("string_array contains ({elementFilter:[3]}sameElement(\"baz\"))", [])
+
+    # Test wrong order and duplicated elements
+    assert_docs("string_array contains ({elementFilter:[0,0,0,0]}sameElement(\"baz\"))", [4])
+    assert_docs("string_array contains ({elementFilter:[25,25,25,0]}sameElement(\"baz\"))", [4])
+
+    ["byte_array", "int_array", "long_array"].each do |array_name|
+      assert_docs("#{array_name} contains ({elementFilter:[0,1,2,3]}sameElement(\"1\"))", [0])
+
+      assert_docs("#{array_name} contains ({elementFilter:[0]}sameElement(\"9\"))", [3, 4])
+      assert_docs("#{array_name} contains ({elementFilter:[0,1]}sameElement(\"9\"))", [3, 4])
+      assert_docs("#{array_name} contains ({elementFilter:[0,1,2]}sameElement(\"9\"))", [2, 3, 4])
+      assert_docs("#{array_name} contains ({elementFilter:[0,1,2,3]}sameElement(\"9\"))", [2, 3, 4])
+      assert_docs("#{array_name} contains ({elementFilter:[1,2,3]}sameElement(\"9\"))", [2, 4])
+      assert_docs("#{array_name} contains ({elementFilter:[2,3]}sameElement(\"9\"))", [2, 4])
+      assert_docs("#{array_name} contains ({elementFilter:[3]}sameElement(\"9\"))", [])
+
+      # Test wrong order and duplicated elements
+      assert_docs("#{array_name} contains ({elementFilter:[25,42,50000,0,0,0,0]}sameElement(\"9\"))", [3, 4])
+      assert_docs("#{array_name} contains ({elementFilter:[3,2,3,2,1,0]}sameElement(\"9\"))", [2, 3, 4])
+    end
+
+    end
+
   def feed_docs
     string_arrays = [
       ["foo", "bar", "baz"],
