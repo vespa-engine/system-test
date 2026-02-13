7acb534c2011881a8ca160c4fc6021ae957423b4
Jan Böker <boeker@vespa.ai>
2026-02-13 13:22:20 +0000

    Clean up test a bit

--- tests/search/same_element_operator/array_indexing.rb
+++ tests/search/same_element_operator/array_indexing.rb
@@ -9,7 +9,39 @@ class ElementFilterAnnotation < IndexedStreamingSearchTest
     set_owner('boeker')
   end
 
-  def test_element_filter_annotation
+
+  def feed_docs
+    string_arrays = [["foo", "bar", "baz"], ["foo", "bar", "bar"], ["foo", "foo", "foo"], ["bar", "bar", "foo"], ["baz", "baz", "baz"]]
+    int_arrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [9, 5, 4], [9, 9, 9]]
+    float_arrays = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [9.0, 5.0, 4.0], [9.0, 9.0, 9.0]]
+
+    for i in 0...5 do
+      vespa.document_api_v1.put(Document.new("id:arrays:arrays::#{i}")
+                                        .add_field("id", i)
+                                        .add_field("string_array", string_arrays[i])
+                                        .add_field("byte_array", int_arrays[i])
+                                        .add_field("int_array", int_arrays[i])
+                                        .add_field("long_array", int_arrays[i])
+                                        .add_field("float_array", float_arrays[i])
+                                        .add_field("double_array", float_arrays[i])
+      )
+    end
+
+    wait_for_hitcount("?query=sddocname:arrays", 5)
+  end
+
+
+  def assert_docs(match_condition, expected_docids)
+    query = {'yql' => "select * from sources * where #{match_condition} order by id asc"}
+    result = search(query)
+    assert_hitcount(result, expected_docids.length)
+    for i in 0...expected_docids.length do
+      assert_field_value(result, "documentid", "id:arrays:arrays::#{expected_docids[i]}", i)
+    end
+  end
+
+
+  def test_element_filter_annotation_with_single_id
     set_description('Use elementFilter annotation of sameElement operator for array indexing.')
     deploy_app(SearchApp.new.sd(selfdir+'arrays.sd'))
     start
@@ -100,57 +132,6 @@ class ElementFilterAnnotation < IndexedStreamingSearchTest
     end
 
     end
-
-  def feed_docs
-    string_arrays = [
-      ["foo", "bar", "baz"],
-      ["foo", "bar", "bar"],
-      ["foo", "foo", "foo"],
-      ["bar", "bar", "foo"],
-      ["baz", "baz", "baz"]
-    ]
-
-    int_arrays = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-      [9, 5, 4],
-      [9, 9, 9]
-    ]
-
-    float_arrays = [
-      [1.0, 2.0, 3.0],
-      [4.0, 5.0, 6.0],
-      [7.0, 8.0, 9.0],
-      [9.0, 5.0, 4.0],
-      [9.0, 9.0, 9.0]
-    ]
-
-    for i in 0...5 do
-      vespa.document_api_v1.put(Document.new("id:arrays:arrays::#{i}")
-                                        .add_field("id", i)
-                                        .add_field("string_array", string_arrays[i])
-                                        .add_field("byte_array", int_arrays[i])
-                                        .add_field("int_array", int_arrays[i])
-                                        .add_field("long_array", int_arrays[i])
-                                        .add_field("float_array", float_arrays[i])
-                                        .add_field("double_array", float_arrays[i])
-      )
-    end
-    wait_for_hitcount("?query=sddocname:arrays", 5)
-  end
-
-  def assert_docs(match_condition, expected_docids)
-    query = {'yql' => "select * from sources * where #{match_condition} order by id asc"}
-    puts "query: #{query}"
-    puts "expected_docids: #{expected_docids}"
-    result = search(query)
-    #puts "result: #{result.json}"
-    assert_hitcount(result, expected_docids.length)
-    for i in 0...expected_docids.length do
-      assert_field_value(result, "documentid", "id:arrays:arrays::#{expected_docids[i]}", i)
-    end
-  end
 
 
 end
