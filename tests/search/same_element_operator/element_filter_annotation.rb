a84b6c0d63d1dfc8ee2e82f4ded7bc2cab9ab1d3
Jan Böker <boeker@vespa.ai>
2026-02-17 07:31:36 +0000

    Reorganize test by array type

--- tests/search/same_element_operator/element_filter_annotation.rb
+++ tests/search/same_element_operator/element_filter_annotation.rb
@@ -7,29 +7,35 @@ class ElementFilterAnnotation < IndexedStreamingSearchTest
 
   def setup
     set_owner('boeker')
+    # This test uses the elementFilter annotation of the sameElement operator to limit it to specific ids,
+    # effectively indexing the array at the specified positions.
   end
 
-
-  def feed_docs
-    string_arrays = [["foo", "bar", "baz"], ["foo", "bar", "bar"], ["foo", "foo", "foo"], ["bar", "bar", "foo"], ["baz", "baz", "baz"]]
-    bool_arrays = [[true, true, false], [false, false, true], [true, false, false], [false, true, false], [false, false, false]]
-    int_arrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [9, 5, 4], [9, 9, 9]]
-    float_arrays = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [9.0, 5.0, 4.0], [9.0, 9.0, 9.0]]
-
-    for i in 0...5 do
-      vespa.document_api_v1.put(Document.new("id:arrays:arrays::#{i}")
-                                        .add_field("id", i)
-                                        .add_field("string_array", string_arrays[i])
-                                        .add_field("bool_array", bool_arrays[i])
-                                        .add_field("byte_array", int_arrays[i])
-                                        .add_field("int_array", int_arrays[i])
-                                        .add_field("long_array", int_arrays[i])
-                                        .add_field("float_array", float_arrays[i])
-                                        .add_field("double_array", float_arrays[i])
-      )
+  def feed_docs(array_hash)
+    # Get length of longest array
+    max_length = 0
+    array_hash.each do |name, arrays|
+      max_length = [max_length, arrays.length].max
     end
 
-    wait_for_hitcount("?query=sddocname:arrays", 5)
+    # Create array of documents
+    docs = []
+    (0...max_length).each do |i|
+      docs.push(Document.new("id:arrays:arrays::#{i}").add_field("id", i))
+    end
+
+    # Add arrays to documents
+    array_hash.each do |name, arrays|
+      (0...arrays.length).each do |i|
+        docs[i].add_field(name, arrays[i])
+      end
+    end
+
+    # Feed documents
+    docs.each do |doc|
+      vespa.document_api_v1.put(doc)
+    end
+    wait_for_hitcount("?query=sddocname:arrays", max_length)
   end
 
 
@@ -43,20 +49,14 @@ class ElementFilterAnnotation < IndexedStreamingSearchTest
   end
 
 
-  def test_element_filter_annotation_with_single_id
-    set_description('Use elementFilter annotation of sameElement operator for array indexing.')
+  def test_bool_array
+    set_description('Use elementFilter annotation of sameElement operator for indexing array of bools.')
     deploy_app(SearchApp.new.sd(selfdir+'arrays.sd'))
     start
 
-    feed_docs
+    feed_docs({"bool_array" => [[true, true, false], [false, false, true], [true, false, false], [false, true, false], [false, false, false]]})
 
-    # Use the elementFilter annotation of the sameElement operator to limit it to specific ids,
-    # effectively indexing the array at the specified positions.
-    assert_docs("string_array contains ({elementFilter:0}sameElement(\"baz\"))", [4])
-    assert_docs("string_array contains ({elementFilter:1}sameElement(\"baz\"))", [4])
-    assert_docs("string_array contains ({elementFilter:2}sameElement(\"baz\"))", [0, 4])
-    assert_docs("string_array contains ({elementFilter:3}sameElement(\"baz\"))", [])
-
+    # Single element
     assert_docs("bool_array contains ({elementFilter:0}sameElement(\"true\"))", [0, 2])
     assert_docs("bool_array contains ({elementFilter:1}sameElement(\"true\"))", [0, 3])
     assert_docs("bool_array contains ({elementFilter:2}sameElement(\"true\"))", [1])
@@ -67,66 +67,7 @@ class ElementFilterAnnotation < IndexedStreamingSearchTest
     assert_docs("bool_array contains ({elementFilter:2}sameElement(\"false\"))", [0, 2, 3, 4])
     assert_docs("bool_array contains ({elementFilter:3}sameElement(\"false\"))", [])
 
-    ["byte_array", "int_array", "long_array"].each do |array_name|
-      assert_docs("#{array_name} contains ({elementFilter:0}sameElement(\"1\"))", [0])
-      assert_docs("#{array_name} contains ({elementFilter:1}sameElement(\"1\"))", [])
-      assert_docs("#{array_name} contains ({elementFilter:2}sameElement(\"1\"))", [])
-      assert_docs("#{array_name} contains ({elementFilter:3}sameElement(\"1\"))", [])
-
-      assert_docs("#{array_name} contains ({elementFilter:0}sameElement(\"9\"))", [3, 4])
-      assert_docs("#{array_name} contains ({elementFilter:1}sameElement(\"9\"))", [4])
-      assert_docs("#{array_name} contains ({elementFilter:2}sameElement(\"9\"))", [2, 4])
-      assert_docs("#{array_name} contains ({elementFilter:3}sameElement(\"9\"))", [])
-    end
-
-    # Does not work, string gets tokenized
-    #["float_array", "double_array"].each do |array_name|
-    #  assert_docs("#{array_name} contains ({elementFilter:0}sameElement(\"1.0\"))", [0])
-    #  assert_docs("#{array_name} contains ({elementFilter:1}sameElement(\"1.0\"))", [])
-    #  assert_docs("#{array_name} contains ({elementFilter:2}sameElement(\"1.0\"))", [])
-    #  assert_docs("#{array_name} contains ({elementFilter:3}sameElement(\"1.0\"))", [])
-    #end
-  end
-
-
-  def test_element_filter_annotation_when_empty
-    set_description('Use elementFilter annotation of sameElement operator for array indexing using multiple indices.')
-    deploy_app(SearchApp.new.sd(selfdir+'arrays.sd'))
-    start
-
-    feed_docs
-
-    # An empty list means "no filtering" and behaves as if no annotation is present.
-    assert_docs("string_array contains sameElement(\"baz\")", [0, 4])
-    assert_docs("string_array contains ({elementFilter:[]}sameElement(\"baz\"))", [0, 4])
-
-    ["byte_array", "int_array", "long_array"].each do |array_name|
-      assert_docs("#{array_name} contains sameElement(\"9\")", [2, 3, 4])
-      assert_docs("#{array_name} contains ({elementFilter:[]}sameElement(\"9\"))", [2, 3, 4])
-    end
-
-  end
-
-
-  def test_element_filter_annotation_with_multiple_ids
-    set_description('Use elementFilter annotation of sameElement operator for array indexing using multiple indices.')
-    deploy_app(SearchApp.new.sd(selfdir+'arrays.sd'))
-    start
-
-    feed_docs
-
-    assert_docs("string_array contains ({elementFilter:[0]}sameElement(\"baz\"))", [4])
-    assert_docs("string_array contains ({elementFilter:[0,1]}sameElement(\"baz\"))", [4])
-    assert_docs("string_array contains ({elementFilter:[0,1,2]}sameElement(\"baz\"))", [0, 4])
-    assert_docs("string_array contains ({elementFilter:[0,1,2,3]}sameElement(\"baz\"))", [0, 4])
-    assert_docs("string_array contains ({elementFilter:[1,2,3]}sameElement(\"baz\"))", [0, 4])
-    assert_docs("string_array contains ({elementFilter:[2,3]}sameElement(\"baz\"))", [0, 4])
-    assert_docs("string_array contains ({elementFilter:[3]}sameElement(\"baz\"))", [])
-
-    # Test wrong order and duplicated elements
-    assert_docs("string_array contains ({elementFilter:[0,0,0,0]}sameElement(\"baz\"))", [4])
-    assert_docs("string_array contains ({elementFilter:[25,25,25,0]}sameElement(\"baz\"))", [4])
-
+    # Multiple elements
     assert_docs("bool_array contains ({elementFilter:[0]}sameElement(\"true\"))", [0, 2])
     assert_docs("bool_array contains ({elementFilter:[0,1]}sameElement(\"true\"))", [0, 2, 3])
     assert_docs("bool_array contains ({elementFilter:[0,1,2]}sameElement(\"true\"))", [0, 1, 2, 3])
@@ -140,6 +81,65 @@ class ElementFilterAnnotation < IndexedStreamingSearchTest
     # Test wrong order and duplicated elements
     assert_docs("bool_array contains ({elementFilter:[0,0,0,0]}sameElement(\"true\"))", [0, 2])
     assert_docs("bool_array contains ({elementFilter:[25,25,25,0]}sameElement(\"true\"))", [0, 2])
+  end
+
+
+  def test_string_array
+    set_description('Use elementFilter annotation of sameElement operator for indexing array of strings.')
+    deploy_app(SearchApp.new.sd(selfdir+'arrays.sd'))
+    start
+
+    feed_docs({"string_array" => [["foo", "bar", "baz"], ["foo", "bar", "bar"], ["foo", "foo", "foo"], ["bar", "bar", "foo"], ["baz", "baz", "baz"]]})
+
+    # An empty list means "no filtering" and behaves as if no annotation is present.
+    assert_docs("string_array contains sameElement(\"baz\")", [0, 4])
+    assert_docs("string_array contains ({elementFilter:[]}sameElement(\"baz\"))", [0, 4])
+
+    assert_docs("string_array contains ({elementFilter:0}sameElement(\"baz\"))", [4])
+    assert_docs("string_array contains ({elementFilter:1}sameElement(\"baz\"))", [4])
+    assert_docs("string_array contains ({elementFilter:2}sameElement(\"baz\"))", [0, 4])
+    assert_docs("string_array contains ({elementFilter:3}sameElement(\"baz\"))", [])
+
+    assert_docs("string_array contains ({elementFilter:[0]}sameElement(\"baz\"))", [4])
+    assert_docs("string_array contains ({elementFilter:[0,1]}sameElement(\"baz\"))", [4])
+    assert_docs("string_array contains ({elementFilter:[0,1,2]}sameElement(\"baz\"))", [0, 4])
+    assert_docs("string_array contains ({elementFilter:[0,1,2,3]}sameElement(\"baz\"))", [0, 4])
+    assert_docs("string_array contains ({elementFilter:[1,2,3]}sameElement(\"baz\"))", [0, 4])
+    assert_docs("string_array contains ({elementFilter:[2,3]}sameElement(\"baz\"))", [0, 4])
+    assert_docs("string_array contains ({elementFilter:[3]}sameElement(\"baz\"))", [])
+
+    # Test wrong order and duplicated elements
+    assert_docs("string_array contains ({elementFilter:[0,0,0,0]}sameElement(\"baz\"))", [4])
+    assert_docs("string_array contains ({elementFilter:[25,25,25,0]}sameElement(\"baz\"))", [4])
+  end
+
+
+  def test_numerical_arrays
+    set_description('Use elementFilter annotation of sameElement operator for indexing arrays of numbers.')
+    deploy_app(SearchApp.new.sd(selfdir+'arrays.sd'))
+    start
+
+    int_arrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [9, 5, 4], [9, 9, 9]]
+    float_arrays = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [9.0, 5.0, 4.0], [9.0, 9.0, 9.0]]
+
+    feed_docs({"byte_array" => int_arrays, "int_array" => int_arrays, "long_array" => int_arrays, "float_array" => float_arrays, "double_array" => float_arrays})
+
+    ["byte_array", "int_array", "long_array"].each do |array_name|
+      assert_docs("#{array_name} contains sameElement(\"9\")", [2, 3, 4])
+      assert_docs("#{array_name} contains ({elementFilter:[]}sameElement(\"9\"))", [2, 3, 4])
+    end
+
+    ["byte_array", "int_array", "long_array"].each do |array_name|
+      assert_docs("#{array_name} contains ({elementFilter:0}sameElement(\"1\"))", [0])
+      assert_docs("#{array_name} contains ({elementFilter:1}sameElement(\"1\"))", [])
+      assert_docs("#{array_name} contains ({elementFilter:2}sameElement(\"1\"))", [])
+      assert_docs("#{array_name} contains ({elementFilter:3}sameElement(\"1\"))", [])
+
+      assert_docs("#{array_name} contains ({elementFilter:0}sameElement(\"9\"))", [3, 4])
+      assert_docs("#{array_name} contains ({elementFilter:1}sameElement(\"9\"))", [4])
+      assert_docs("#{array_name} contains ({elementFilter:2}sameElement(\"9\"))", [2, 4])
+      assert_docs("#{array_name} contains ({elementFilter:3}sameElement(\"9\"))", [])
+    end
 
     ["byte_array", "int_array", "long_array"].each do |array_name|
       assert_docs("#{array_name} contains ({elementFilter:[0,1,2,3]}sameElement(\"1\"))", [0])
@@ -157,7 +157,13 @@ class ElementFilterAnnotation < IndexedStreamingSearchTest
       assert_docs("#{array_name} contains ({elementFilter:[3,2,3,2,1,0]}sameElement(\"9\"))", [2, 3, 4])
     end
 
-    end
+    # Does not work yet, string gets tokenized
+    #["float_array", "double_array"].each do |array_name|
+    #  assert_docs("#{array_name} contains ({elementFilter:0}sameElement(\"1.0\"))", [0])
+    #  assert_docs("#{array_name} contains ({elementFilter:1}sameElement(\"1.0\"))", [])
+    #  assert_docs("#{array_name} contains ({elementFilter:2}sameElement(\"1.0\"))", [])
+    #  assert_docs("#{array_name} contains ({elementFilter:3}sameElement(\"1.0\"))", [])
+    #end
 
-
+  end
 end
