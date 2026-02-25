8b0292620a8ffb261b7df9208f561e0d18a951cc
Håvard Pettersen <3535158+havardpe@users.noreply.github.com>
2026-02-25 14:38:43 +0000

    add test for using array of bool in ranking

--- tests/search/array_of_bool/array_of_bool.rb
+++ tests/search/array_of_bool/array_of_bool.rb
@@ -31,6 +31,13 @@ class ArrayOfBool < IndexedStreamingSearchTest
     assert_summary_flags('tttf', [true, true, true, false])
     assert_summary_flags('empty', nil)
     assert_summary_flags('notset', nil)
+
+    # verify ranking can extract values from bool array
+    assert_ranking_flags('tft',  {0 => 1.0, 1 => 0.0, 2 => 1.0, 'count' => 3.0})
+    assert_ranking_flags('ff',   {0 => 0.0, 1 => 0.0, 2 => 0.0, 'count' => 2.0})
+    assert_ranking_flags('tttf', {0 => 1.0, 1 => 1.0, 2 => 1.0, 'count' => 4.0})
+    assert_ranking_flags('empty',  {0 => 0.0, 1 => 0.0, 2 => 0.0, 'count' => 0.0})
+    assert_ranking_flags('notset', {0 => 0.0, 1 => 0.0, 2 => 0.0, 'count' => 0.0})
   end
 
   def assert_summary_flags(title, expected)
@@ -38,6 +45,16 @@ class ArrayOfBool < IndexedStreamingSearchTest
     puts "title(#{title}) gives result: #{result.xmldata}"
     assert_equal(1, result.hitcount)
     assert_equal(result.hit[0].field['flags'], expected)
+  end
+
+  def assert_ranking_flags(title, expected)
+    result = search({"yql" => "select * from sources * where title contains '#{title}'"})
+    assert_equal(1, result.hitcount)
+    sf = result.hit[0].field['summaryfeatures']
+    expected.each do |key, value|
+      feature = key == 'count' ? 'attribute(flags).count' : "attribute(flags,#{key})"
+      assert_features({feature => value}, sf)
+    end
   end
 
   def teardown
