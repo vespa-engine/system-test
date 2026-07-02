101c8fc92b70be1f1a86fdc177f5452e1c2ec1ee
Jan Böker <boeker@vespa.ai>
2026-07-02 13:29:16 +0000

    Make number of groups and nodes in test configurable

--- tests/search/coverage/coverage.rb
+++ tests/search/coverage/coverage.rb
@@ -3,38 +3,46 @@ require 'indexed_only_search_test'
 
 class Coverage < IndexedOnlySearchTest
 
-  NUM_DOCUMENTS = 200_000
+  NUM_DOCUMENTS = 100_000
+  NUM_GROUPS = 2
+  NUM_NODES_PER_GROUP = 2
 
   def setup
     set_owner("boeker")
   end
 
-  def create_app(schema_file)
+  def create_app(schema_file, num_groups, num_nodes_per_group)
     SearchApp.new.sd(selfdir + "coverage.sd")
 
-    topgroup = NodeGroup.new(0, "mytopgroup").distribution("*") # Use "1|*" for multiple groups
-    nodegroup = NodeGroup.new(0, "mygroup0")
-    nodegroup.node(NodeSpec.new("node1", 0))
-    nodegroup.node(NodeSpec.new("node1", 1))
-    nodegroup.node(NodeSpec.new("node1", 2))
-    topgroup.group(nodegroup)
-    #nodegroup2 = NodeGroup.new(1, "mygroup1")
-    #nodegroup2.node(NodeSpec.new("node1", 2))
-    #nodegroup2.node(NodeSpec.new("node1", 3))
-    #topgroup.group(nodegroup2)
+    if num_groups > 1
+      distribution = "1|*"
+    else
+      distribution = "*"
+    end
+    topgroup = NodeGroup.new(0, "mytopgroup").distribution(distribution)
+    distkey = 0
+    for g in 1..num_groups do
+      nodegroup = NodeGroup.new(g-1, "mygroup#{g-1}")
+      for n in 1..num_nodes_per_group do
+        nodegroup.node(NodeSpec.new("node1", distkey))
+        distkey = distkey + 1
+      end
+      topgroup.group(nodegroup)
+    end
 
     SearchApp.new
              .search_dir(selfdir + "search")
              .cluster(SearchCluster.new("mycluster")
                                        .sd(schema_file)
-                                       .redundancy(2)
-                                       .ready_copies(1)
+                                       .redundancy(num_groups)
+                                       .ready_copies(num_groups)
+                                       .min_active_docs_coverage(90.0)
                                        .group(topgroup))
   end
 
   def test_coverage_when_stopping_node
     set_description("Check reported coverage when a node in a group is stopped")
-    deploy_app(create_app(selfdir + "coverage.sd"))
+    deploy_app(create_app(selfdir + "coverage.sd", NUM_GROUPS, NUM_NODES_PER_GROUP))
 
     @container = get_container
     compile_document_generator
@@ -45,22 +53,24 @@ class Coverage < IndexedOnlySearchTest
     search_and_print
 
     search_cluster = vespa.search.values.first
-    assert_equal(3, search_cluster.searchnode.length)
+    assert_equal(NUM_GROUPS * NUM_NODES_PER_GROUP, search_cluster.searchnode.length)
 
     first_searchnode = search_cluster.searchnode.values.first
     puts "Stopping searchnode #{first_searchnode}"
     first_searchnode.stop
 
-    600.times do
+    300.times do
       search_and_print
+      puts "Searchnode was stopped"
       sleep 0.2
     end
 
     puts "Starting searchnode #{first_searchnode}"
     first_searchnode.start
 
-    600.times do
+    300.times do
       search_and_print
+      puts "Searchnode was started again"
       sleep 0.2
     end
   end
