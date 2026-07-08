099de8ad3b16756665350b743fe066eeed647965
Jan Böker <boeker@vespa.ai>
2026-07-08 08:51:35 +0000

    Introduce wait_for_coverage function

--- tests/search/coverage/coverage.rb
+++ tests/search/coverage/coverage.rb
@@ -5,6 +5,7 @@ class Coverage < IndexedOnlySearchTest
 
   def setup
     set_owner("boeker")
+    add_ignorable_messages([/group \d has reduced coverage/])
     @num_documents = 10_000
     @coverage_query = {"query" => "sddocname:coverage"}
   end
@@ -26,24 +27,10 @@ class Coverage < IndexedOnlySearchTest
   def feed_and_wait_and_stop
     feed_and_wait
     vespa.stop_content_node("mycluster", 0, 120, "d")
-
-    #search_cluster = vespa.search.values.first
-    ##assert_equal(@num_groups * @num_nodes_per_group, search_cluster.searchnode.length)
-    ## Get node from that cluster
-    #first_searchnode = search_cluster.searchnode.values.first
-    ## Stop it
-    #puts "Stopping searchnode #{first_searchnode}"
-    #first_searchnode.stop
   end
 
-  def create_app(schema_file, num_groups, num_nodes_per_group, redundancy, ready_copies)
+  def create_app(schema_file, num_groups, num_nodes_per_group, redundancy, ready_copies, distribution)
     SearchApp.new.sd(selfdir + "coverage.sd")
-
-    if num_groups > 1
-      distribution = "1|*"
-    else
-      distribution = "*"
-    end
 
     topgroup = NodeGroup.new(0, "mytopgroup").distribution(distribution)
     distkey = 0
@@ -56,90 +43,81 @@ class Coverage < IndexedOnlySearchTest
       topgroup.group(nodegroup)
     end
 
-    SearchApp.new
-             .cluster(SearchCluster.new("mycluster")
+    SearchApp.new.cluster(SearchCluster.new("mycluster")
                                        .sd(schema_file)
                                        .redundancy(redundancy)
                                        .ready_copies(ready_copies)
-                                       .min_active_docs_coverage(90.0)
+                                       .min_active_docs_coverage(100.0) # Should make no difference for the test. For toying around with
                                        .group(topgroup))
   end
 
   def test_coverage_when_stopping_node_with_one_group_with_redundancy
     set_description("Check reported coverage when a node in a group is stopped with one groups and redundant documents on the other node")
-    deploy_app(create_app(selfdir + "coverage.sd", 1, 2, 2, 1))
+    deploy_app(create_app(selfdir + "coverage.sd", 1, 2, 2, 1, "*"))
     start
 
     feed_and_wait_and_stop
 
-    wait_for_hitcount_from_group(@coverage_query, 0, @num_documents)
-    verify_coverage(@coverage_query, 0, 100)
+    wait_for_hitcount_from_group(@coverage_query, nil, @num_documents)
+    wait_for_coverage_from_group(@coverage_query, nil, 100)
   end
 
   def test_coverage_when_stopping_node_with_one_group_without_redundancy
     set_description("Check reported coverage when a node in a group is stopped with one groups and no redundant documents on the other node")
-    deploy_app(create_app(selfdir + "coverage.sd", 1, 2, 1, 1))
+    deploy_app(create_app(selfdir + "coverage.sd", 1, 2, 1, 1, "*"))
     start
 
     feed_and_wait_and_stop
 
-    wait_for_hitcount_from_group(@coverage_query, 0, @num_documents)
-    verify_coverage(@coverage_query, 0, 50)
+    # No need to wait for hitcount. Will be around @num_documents / 2 and not change since there is no redundancy.
+    wait_for_coverage_from_group(@coverage_query, nil, 50)
   end
 
   def test_coverage_when_stopping_node_with_two_groups_without_redundancy
     set_description("Check reported coverage when a node in a group is stopped with two groups in total and no inner-group redundancy")
-    deploy_app(create_app(selfdir + "coverage.sd", 2, 2, 2, 2))
+    deploy_app(create_app(selfdir + "coverage.sd", 2, 2, 2, 2, "1|*"))
     start
 
     feed_and_wait_and_stop
 
     wait_for_hitcount_from_group(@coverage_query, 0, @num_documents)
     wait_for_hitcount_from_group(@coverage_query, 1, @num_documents)
-    verify_coverage(@coverage_query, 0, 100)
-    verify_coverage(@coverage_query, 1, 100)
+    wait_for_coverage_from_group(@coverage_query, 0, 100)
+    wait_for_coverage_from_group(@coverage_query, 1, 100)
   end
 
   def test_coverage_when_stopping_node_with_two_groups_with_redundancy
     set_description("Check reported coverage when a node in a group is stopped with two groups in total and inner-group redundancy")
-    deploy_app(create_app(selfdir + "coverage.sd", 2, 2, 4, 2))
+    deploy_app(create_app(selfdir + "coverage.sd", 2, 2, 4, 2, "2|*"))
     start
 
     feed_and_wait_and_stop
 
     wait_for_hitcount_from_group(@coverage_query, 0, @num_documents)
     wait_for_hitcount_from_group(@coverage_query, 1, @num_documents)
-    verify_coverage(@coverage_query, 0, 100)
-    verify_coverage(@coverage_query, 1, 100)
-  end
-
-  def test_foo
-    set_description("Check reported coverage when a node in a group is stopped with two groups in total and no inner-group redundancy")
-    deploy_app(create_app(selfdir + "coverage.sd", 2, 2, 2, 2))
-    start
-
-    #vespa.stop_content_node("mycluster", 0)
+    wait_for_coverage_from_group(@coverage_query, 0, 100)
+    wait_for_coverage_from_group(@coverage_query, 1, 100)
   end
 
   def wait_for_hitcount_from_group(query, wanted_group, wanted_hitcount, timeout_in=60, qrserver_id=0, params={})
     query = query.merge({"hits" => "1", "model.searchGroup" => "#{wanted_group}"})
 
     hitcount = -1
+    group = -1
     timeout = timeout_in
     timeout = calculateQueryTimeout(timeout)
 
-    puts "Waiting for #{wanted_hitcount} hits, timeout: #{timeout}"
+    puts "Waiting for #{wanted_hitcount} hits from group #{wanted_group}, timeout: #{timeout}"
     trynum = 0
     start = Time.now.to_i
 
-    # check that the hitcount is equal to the wanted hitcount
     while Time.now.to_i < start + timeout
       begin
         trynum += 1
         result = search_with_timeout(timeout_in, query, qrserver_id, {}, false, params)
         hitcount = result.hitcount
         group = result.json['root']['fields']['searchGroup']
-        if hitcount == wanted_hitcount && group == wanted_group
+        if hitcount == wanted_hitcount && (wanted_group == nil || group == wanted_group)
           puts "Success on try #{trynum}: Got #{wanted_hitcount} hits from group #{wanted_group}"
           return true
         else
@@ -155,20 +133,38 @@ class Coverage < IndexedOnlySearchTest
     fail("Timeout after #{trynum} tries: Expected #{wanted_hitcount} hits from group #{wanted_group}, got #{hitcount} hits from group #{group}")
   end
 
-  def verify_coverage(query, wanted_group, expected_coverage)
-    puts "Checking for #{expected_coverage} coverage from group #{wanted_group}"
-
+  def wait_for_coverage_from_group(query, wanted_group, wanted_coverage, timeout_in=60, qrserver_id=0, params={})
     query = query.merge({"hits" => "1", "model.searchGroup" => "#{wanted_group}"})
-    puts query
-    result = search_with_timeout(60, query)
-    group = result.json['root']['fields']['searchGroup']
-    coverage = result.json['root']['coverage']['coverage']
 
-    puts JSON.pretty_generate(result.json)
+    coverage = -1
+    group = -1
+    timeout = timeout_in
+    timeout = calculateQueryTimeout(timeout)
 
-    puts "Got #{coverage} coverage from group #{group}"
-    assert_equal(wanted_group, group)
-    assert_equal(expected_coverage, coverage)
+    puts "Waiting for #{wanted_coverage} coverage from group #{wanted_group}, timeout: #{timeout}"
+    trynum = 0
+    start = Time.now.to_i
+
+    while Time.now.to_i < start + timeout
+      begin
+        trynum += 1
+        result = search_with_timeout(timeout_in, query, qrserver_id, {}, false, params)
+        coverage = result.json['root']['coverage']['coverage']
+        group = result.json['root']['fields']['searchGroup']
+        if coverage == wanted_coverage && (wanted_group == nil || group == wanted_group)
+          puts "Success on try #{trynum}: Got #{wanted_coverage} coverage from group #{wanted_group}"
+          return true
+        else
+          puts "Failure on try #{trynum}: Expected #{wanted_coverage} coverage from group #{wanted_group}, got #{coverage} coverage from group #{group}"
+        end
+      rescue StandardError => e
+        puts "error #{e}: #{e.backtrace}"
+      rescue Interrupt
+        puts "low-level timeout, retry"
+      end
+      sleep 1
+    end
+    fail("Timeout after #{trynum} tries: Expected #{wanted_coverage} coverage from group #{wanted_group}, got #{coverage} coverage from group #{group}")
   end
 
   #def get_container
